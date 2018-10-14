556eea6d480d72ca360ddcabfd182a82122c4d1a
JP Dillingham <jpdillingham@gmail.com>
2018-10-14 18:31:27 -0500

    stub Message

---
+++ src/Soulseek.NET/Messaging/Message.cs
@@ -0,0 +1,25 @@
+﻿namespace Soulseek.NET.Messaging
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    public class Message
+    {
+        public int Length { get; private set; }
+        public MessageCode Code { get; private set; }
+        public IReadOnlyCollection<IMessageSegment> Segments => new ReadOnlyCollection<IMessageSegment>(InternalSegments);
+
+        private IList<IMessageSegment> InternalSegments { get; set; } = new List<IMessageSegment>();
+
+        public Message(byte[] message)
+        {
+        }
+
+        public Message(MessageCode code, params IMessageSegment[] segments)
+        {
+            Code = code;
+            InternalSegments = segments.ToList();
+        }
+    }
+}
