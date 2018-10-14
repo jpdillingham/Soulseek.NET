d9309784382562f00c716427517ac3175b25629e
JP Dillingham <jpdillingham@gmail.com>
2018-10-14 18:31:35 -0500

    stub MessageBuilder

---
+++ src/Soulseek.NET/Messaging/MessageBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace Soulseek.NET.Messaging
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MessageBuilder
+    {
+        private MessageCode Code { get; set; }
+        private IList<IMessageSegment> Segments { get; set; } = new List<IMessageSegment>();
+
+        public Message Build()
+        {
+            return new Message(Code, Segments.ToArray());
+        }
+
+        public MessageBuilder MessageCode(MessageCode code)
+        {
+            Code = code;
+            return this;
+        }
+    }
+}
