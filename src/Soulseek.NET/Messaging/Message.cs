53ed4017b8646739cb36c66f14c2d113d46ee6a6
JP Dillingham <jpdillingham@gmail.com>
2018-10-15 08:41:39 -0500

    remove byte array constructor from Message; redundant

--- src/Soulseek.NET/Messaging/Message.cs
+++ src/Soulseek.NET/Messaging/Message.cs
@@ -1,6 +1,5 @@
 ﻿namespace Soulseek.NET.Messaging
 {
-    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
@@ -11,11 +10,6 @@
         public MessageCode Code { get; private set; }
         public IReadOnlyCollection<IMessageSegment> Segments => new ReadOnlyCollection<IMessageSegment>(InternalSegments);
         private IList<IMessageSegment> InternalSegments { get; set; } = new List<IMessageSegment>();
-
-        public Message(byte[] bytes)
-        {
-            var Message = new MessageFactory().GetMessage(bytes);
-        }
 
         public Message(MessageCode code, params IMessageSegment[] segments)
         {
