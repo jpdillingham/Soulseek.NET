07af9c193c3cdde51f96e815d1b32b0dee19959c
JP Dillingham <jpdillingham@gmail.com>
2018-10-15 08:14:37 -0500

    add shared Exceptions class file

---
+++ src/Soulseek.NET/Exceptions.cs
@@ -0,0 +1,25 @@
+﻿namespace Soulseek.NET
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class MessageFormatException : Exception
+    {
+        public MessageFormatException()
+            : base()
+        {
+        }
+
+        public MessageFormatException(string message)
+            : base(message)
+        {
+        }
+
+        public MessageFormatException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
