c9f898ab6369c32f3549a3092545cbb714b5fada
JP Dillingham <jpdillingham@gmail.com>
2018-10-15 08:38:07 -0500

    add Extensions

---
+++ src/Soulseek.NET/Extensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Soulseek.NET
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class Extensions
+    {
+        public static string ToHexString(this IEnumerable<byte> bytes)
+        {
+            StringBuilder Result = new StringBuilder(bytes.Count() * 2);
+            string HexAlphabet = "0123456789ABCDEF";
+
+            foreach (byte B in bytes)
+            {
+                Result.Append(HexAlphabet[(int)(B >> 4)]);
+                Result.Append(HexAlphabet[(int)(B & 0xF)]);
+            }
+
+            return Result.ToString();
+        }
+    }
+}
