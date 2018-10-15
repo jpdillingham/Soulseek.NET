872ba1ab9a725ecd61c87985b8afd5518188d964
JP Dillingham <jpdillingham@gmail.com>
2018-10-15 08:38:18 -0500

    stub MessageFactory

---
+++ src/Soulseek.NET/Messaging/MessageFactory.cs
@@ -0,0 +1,62 @@
+﻿namespace Soulseek.NET.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    public class MessageFactory
+    {
+        public Message GetMessage(byte[] bytes)
+        {
+            var length = GetMessageLength(bytes);
+
+            var builder = new MessageBuilder()
+                .MessageCode(GetMessageCode(bytes));
+        }
+
+        private int GetMessageLength(byte[] bytes)
+        {
+            try
+            {
+                return Convert.ToInt32(bytes.Take(4));
+            }
+            catch (Exception ex)
+            {
+                throw new MessageFormatException($"The Message length is not a valid integer.", ex);
+            }
+        }
+
+        private MessageCode GetMessageCode(byte[] bytes)
+        {
+            IEnumerable<byte> chunk = default(IEnumerable<byte>);
+            var codeNum = 0;
+
+            try
+            {
+                chunk = bytes.Skip(4).Take(4);
+            }
+            catch (Exception ex)
+            {
+                throw new MessageFormatException($"The Message does not contain a Message Code.");
+            }
+
+            try
+            {
+                codeNum = Convert.ToInt32(chunk);
+            }
+            catch (Exception ex)
+            {
+                throw new MessageFormatException($"The Message Code (0x{chunk.ToHexString()}) is not a valid integer.", ex);
+            }
+
+            try
+            {
+                return (MessageCode)codeNum;
+            }
+            catch (Exception)
+            {
+                return MessageCode.Unknown;
+            }
+        }
+    }
+}
