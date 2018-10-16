3cb78a3faa55ac137fd3cde1ed0772597e09b708
JP Dillingham <jpdillingham@gmail.com>
2018-10-15 22:19:33 -0500

    stub MessageReader

---
+++ src/Soulseek.NET/Messaging/MessageReader.cs
@@ -0,0 +1,17 @@
+﻿namespace Soulseek.NET.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MessageReader
+    {
+        private List<byte> Bytes { get; set; }
+
+        public MessageReader(byte[] bytes)
+        {
+            Bytes = bytes.ToList();
+        }
+    }
+}
