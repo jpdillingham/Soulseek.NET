6cfdacf78a34c867c7810b894e861c67efadbf02
JP Dillingham <jpdillingham@gmail.com>
2018-10-16 07:20:28 -0500

    implement MessageReader

--- src/Soulseek.NET/Messaging/MessageReader.cs
+++ src/Soulseek.NET/Messaging/MessageReader.cs
@@ -1,17 +1,84 @@
 ﻿namespace Soulseek.NET.Messaging
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
 
     public class MessageReader
     {
-        private List<byte> Bytes { get; set; }
+        private int Position { get; set; } = 0;
+        private byte[] Bytes { get; set; }
 
         public MessageReader(byte[] bytes)
         {
-            Bytes = bytes.ToList();
+            Bytes = bytes;
+        }
+
+        public MessageReader Reset()
+        {
+            Position = 0;
+            return this;
+        }
+
+        public int ReadByte()
+        {
+            try
+            {
+                var retVal = Bytes[Position];
+                Position += 1;
+                return retVal;
+            }
+            catch (Exception ex)
+            {
+                throw new MessageReadException($"Failed to read a byte from position {Position} of the message.", ex);
+            }
+        }
+
+        public int ReadInteger()
+        {
+            try
+            {
+                var retVal = BitConverter.ToInt32(Bytes, Position);
+                Position += 4;
+                return retVal;
+            }
+            catch (Exception ex)
+            {
+                throw new MessageReadException($"Failed to read an integer (4 bytes) from position {Position} of the message.", ex);
+            }
+        }
+
+        public long ReadLong()
+        {
+            try
+            {
+                var retVal = BitConverter.ToInt64(Bytes, Position);
+                Position += 8;
+                return retVal;
+            }
+            catch (Exception ex)
+            {
+                throw new MessageReadException($"Failed to read a long integer (8 bytes) from position {Position} of the message.", ex);
+            }
+        }
+
+        public string ReadString()
+        {
+            var length = 0;
+
+            try
+            {
+                length = ReadInteger();
+                var retVal = BitConverter.ToString(Bytes, Position, length);
+                Position += length;
+                return retVal;
+            }
+            catch (MessageReadException ex)
+            {
+                throw new MessageReadException($"Failed to read the length of the requested string from position {Position} of the message.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new MessageReadException($"Failed to read a string of length {length} from position {Position} of the message.", ex);
+            }
         }
     }
 }
