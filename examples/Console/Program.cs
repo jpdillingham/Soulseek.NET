9472e01b0ec4bc2459a7c9e2084cc87e1ab23d5e
JP Dillingham <jpdillingham@gmail.com>
2018-10-16 21:26:33 -0500

    remove deprecated methods

--- examples/Console/Program.cs
+++ examples/Console/Program.cs
@@ -86,29 +86,6 @@
             }
         }
 
-        public static byte[] StringToByteArray(string hex)
-        {
-            hex = hex.Replace(" ", string.Empty);
-            return Enumerable.Range(0, hex.Length)
-                             .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                             .ToArray();
-        }
-
-        public static string ByteArrayToHexString(byte[] Bytes)
-        {
-            StringBuilder Result = new StringBuilder(Bytes.Length * 2);
-            string HexAlphabet = "0123456789ABCDEF";
-
-            foreach (byte B in Bytes)
-            {
-                Result.Append(HexAlphabet[(int)(B >> 4)]);
-                Result.Append(HexAlphabet[(int)(B & 0xF)]);
-            }
-
-            return Result.ToString();
-        }
-
         public static int Main(String[] args)
         {
             StartClient();
