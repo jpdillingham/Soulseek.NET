867fc1629bf7fa977878a39325e601156ef5a5d6
JP Dillingham <jpdillingham@gmail.com>
2018-10-16 21:50:26 -0500

    add ServerException

--- src/Soulseek.NET/Exceptions.cs
+++ src/Soulseek.NET/Exceptions.cs
@@ -3,6 +3,25 @@
     using System;
     using System.Diagnostics.CodeAnalysis;
 
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class ServerException : Exception
+    {
+        public ServerException()
+            : base()
+        {
+        }
+
+        public ServerException(string message)
+            : base(message)
+        {
+        }
+
+        public ServerException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
 
     [ExcludeFromCodeCoverage]
     [Serializable]
