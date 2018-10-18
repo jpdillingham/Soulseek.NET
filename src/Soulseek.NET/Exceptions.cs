feeebb5c33beeca48383f68ea5e8012901597861
JP Dillingham <jpdillingham@gmail.com>
2018-10-18 07:06:22 -0500

    move message Exceptions to Messaging namespace

--- src/Soulseek.NET/Exceptions.cs
+++ src/Soulseek.NET/Exceptions.cs
@@ -22,44 +22,4 @@
         {
         }
     }
-
-    [ExcludeFromCodeCoverage]
-    [Serializable]
-    public class MessageException : Exception
-    {
-        public MessageException()
-            : base()
-        {
-        }
-
-        public MessageException(string message)
-            : base(message)
-        {
-        }
-
-        public MessageException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-    }
-
-    [ExcludeFromCodeCoverage]
-    [Serializable]
-    public class MessageReadException : MessageException
-    {
-        public MessageReadException()
-            : base()
-        {
-        }
-
-        public MessageReadException(string message)
-            : base(message)
-        {
-        }
-
-        public MessageReadException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-    }
 }
---
+++ src/Soulseek.NET/Messaging/Exceptions.cs
@@ -0,0 +1,45 @@
+﻿namespace Soulseek.NET.Messaging
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class MessageException : Exception
+    {
+        public MessageException()
+            : base()
+        {
+        }
+
+        public MessageException(string message)
+            : base(message)
+        {
+        }
+
+        public MessageException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class MessageReadException : MessageException
+    {
+        public MessageReadException()
+            : base()
+        {
+        }
+
+        public MessageReadException(string message)
+            : base(message)
+        {
+        }
+
+        public MessageReadException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
