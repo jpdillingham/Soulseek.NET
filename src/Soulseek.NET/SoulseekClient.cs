bfbe801ebde78d281683afbbafec9f27b2feed53
JP Dillingham <jpdillingham@gmail.com>
2018-10-18 08:37:02 -0500

    add RawMessageReceived and MessageReceived events to SoulseekClient

--- src/Soulseek.NET/SoulseekClient.cs
+++ src/Soulseek.NET/SoulseekClient.cs
@@ -1,9 +1,11 @@
 ﻿namespace Soulseek.NET
 {
     using Soulseek.NET.Messaging;
+    using Soulseek.NET.Messaging.Login;
     using Soulseek.NET.Tcp;
     using System;
     using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
 
     public class SoulseekClient
@@ -20,6 +22,8 @@
 
         public event EventHandler<ConnectionStateChangedEventArgs> ConnectionStateChanged;
         public event EventHandler<DataReceivedEventArgs> DataReceived;
+        public event EventHandler<RawMessageReceivedEventArgs> RawMessageReceived;
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
 
         public string Address { get; private set; }
         public Connection Connection { get; private set; }
@@ -28,60 +32,66 @@
         public async Task ConnectAsync()
         {
             await Connection.ConnectAsync();
-            
         }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var request = new LoginRequest(username, password).ToBytes();
+
+            Console.WriteLine($"Logging in as {username}...");
+
+            await Connection.SendAsync(request);
+
+            return true;
+        }
+
+        private void OnConnectionDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine($"Data Received: {e.Data.Length} bytes");
+            DataReceived?.Invoke(this, e);
+
+            OnRawMessageReceived(e.Data);
+        }
+
+        private void OnRawMessageReceived(byte[] message)
+        {
+            var reader = new MessageReader(message);
+            Console.WriteLine($"Raw Message Recieved; Length: {reader.Length}, Code: {reader.Code}, Data ({reader.Payload.Length}): {Encoding.ASCII.GetString(reader.Payload).ToCharArray().Take(30)} ");
+            RawMessageReceived?.Invoke(this, new RawMessageReceivedEventArgs() { Reader = reader });
+
+            OnMessageReceived(reader.Code, reader.RawBytes);
+        }
+
+        private void OnMessageReceived(MessageCode code, byte[] message)
+        {
+            var response = new object();
+
+            switch (code)
+            {
+                case MessageCode.Login:
+                    response = new LoginResponse(message);
+                    break;
+                default:
+                    response = null;
+                    break;
+            }
+
+            MessageReceived?.Invoke(this, new MessageReceivedEventArgs() { Code = code, Response = response });
+
+            if (response is LoginResponse lr)
+            {
+                Console.WriteLine($"Status: {lr.Status}");
+                Console.WriteLine($"Message: {lr.Message}");
+                Console.WriteLine($"IPAddress: {lr.IPAddress}");
+            }
+        }
+
+
 
         private void OnConnectionStateChanged(object sender, ConnectionStateChangedEventArgs e)
         {
             Console.WriteLine($"Connection State Changed: {e.State} ({e.Message ?? "Unknown"})");
             ConnectionStateChanged?.Invoke(this, e);
-        }
-
-        private void OnConnectionDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            Console.WriteLine($"Data Received");
-            var reader = new MessageReader(e.Data);
-            Console.WriteLine($"Length: {reader.Length}");
-            Console.WriteLine($"Code: {reader.Code}");
-
-            DataReceived?.Invoke(this, e);
-        }
-
-        public async Task<bool> LoginAsync(string username, string password)
-        {
-            var request = new MessageBuilder()
-                .Code(MessageCode.Login)
-                .WriteString(username)
-                .WriteString(password)
-                .WriteInteger(181)
-                .WriteString($"{username}{password}".ToMD5Hash())
-                .WriteInteger(1)
-                .Build();
-
-            Console.WriteLine($"Logging in as {username}...");
-
-            await Connection.SendAsync(request);
-            //var responses = await Connection.ReadAsync();
-
-            //foreach (var response in responses)
-            //{
-            //    var rdr = new MessageReader(response);
-            //    Console.WriteLine($"Length: {rdr.Length()}, Code: {rdr.Code()}");
-            //}
-
-            //Console.WriteLine("----------");
-            //var reader = new MessageReader(responses.ToArray()[0]);
-            //Console.WriteLine($"Length: {reader.Length()}");
-            //Console.WriteLine($"Code: {reader.Code()}");
-
-            //var result = reader.ReadByte();
-            //Console.WriteLine($"Result: {result}");
-            //Console.WriteLine($"Message: {reader.ReadString()}");
-
-            //var success = result == 1;
-            //return success;
-
-            return true;
         }
     }
 }
