c2e65717b10cc02832b21442d48a60912c7ef23d
JP Dillingham <jpdillingham@gmail.com>
2018-10-17 21:18:16 -0500

    combine EventArgs into EventArgs.cs

--- src/Soulseek.NET/Tcp/EventArgs.cs
+++ src/Soulseek.NET/Tcp/EventArgs.cs
@@ -2,8 +2,14 @@
 {
     using System;
 
+    public class DataReceivedEventArgs : EventArgs
+    {
+        public byte[] Data;
+    }
+
     public class ConnectionStateChangedEventArgs : EventArgs
     {
         public ConnectionState State { get; set; }
+        public string Message { get; set; }
     }
 }
