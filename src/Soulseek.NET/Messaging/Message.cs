bb2928bd60b199df0f3f1e13173768d7406f8837
JP Dillingham <jpdillingham@gmail.com>
2018-10-18 09:13:25 -0500

    add Message

---
+++ src/Soulseek.NET/Messaging/Message.cs
@@ -0,0 +1,51 @@
+﻿namespace Soulseek.NET.Messaging
+{
+    using System;
+    using System.Linq;
+
+    public class Message
+    {
+        private byte[] Bytes { get; set; }
+
+        public int Length => GetLength();
+        public MessageCode Code => GetCode();
+        public byte[] Payload => GetPayload();
+        public byte[] RawBytes => Bytes;
+
+        public Message(byte[] bytes)
+        {
+            Bytes = bytes;
+        }
+
+        private byte[] GetPayload()
+        {
+            return Bytes.Skip(8).ToArray();
+        }
+
+        private int GetLength()
+        {
+            try
+            {
+                var retVal = BitConverter.ToInt32(Bytes, 0);
+                return retVal;
+            }
+            catch (Exception ex)
+            {
+                throw new MessageReadException($"Failed to read the message length.", ex);
+            }
+        }
+
+        private MessageCode GetCode()
+        {
+            try
+            {
+                var retVal = BitConverter.ToInt32(Bytes, 4);
+                return (MessageCode)retVal;
+            }
+            catch (Exception ex)
+            {
+                throw new MessageReadException($"Failed to read the message code of the message.", ex);
+            }
+        }
+    }
+}
