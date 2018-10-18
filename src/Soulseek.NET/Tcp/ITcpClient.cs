ecf7f51827e1cbffc3396e461c44ccf9fb88b639
JP Dillingham <jpdillingham@gmail.com>
2018-10-17 21:41:40 -0500

    add ITcpClient and TcpClientAdapter

---
+++ src/Soulseek.NET/Tcp/ITcpClient.cs
@@ -0,0 +1,19 @@
+﻿namespace Soulseek.NET.Tcp
+{
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+
+    internal interface ITcpClient
+    {
+        bool Connected { get; }
+
+        void Close();
+
+        Task ConnectAsync(IPAddress ipAddress, int port);
+
+        void Dispose();
+
+        NetworkStream GetStream();
+    }
+}
---
+++ src/Soulseek.NET/Tcp/TcpClientAdapter.cs
@@ -0,0 +1,48 @@
+﻿namespace Soulseek.NET.Tcp
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+
+    public class TcpClientAdapter : ITcpClient, IDisposable
+    {
+        public TcpClientAdapter(TcpClient tcpClient = null)
+        {
+            TcpClient = tcpClient ?? new TcpClient();
+        }
+
+        public bool Connected => TcpClient.Connected;
+
+        private TcpClient TcpClient { get; set; }
+
+        public void Close()
+        {
+            TcpClient.Close();
+        }
+
+        public async Task ConnectAsync(IPAddress ipAddress, int port)
+        {
+            await TcpClient.ConnectAsync(ipAddress, port);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public NetworkStream GetStream()
+        {
+            return TcpClient.GetStream();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                TcpClient.Dispose();
+            }
+        }
+    }
+}
