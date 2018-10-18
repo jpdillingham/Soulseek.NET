05bb1b93a7e409a89b59174e7b8cb64b159e3e33
JP Dillingham <jpdillingham@gmail.com>
2018-10-17 21:18:28 -0500

    add Exceptions for Tcp namespace

---
+++ src/Soulseek.NET/Tcp/Exceptions.cs
@@ -0,0 +1,45 @@
+﻿namespace Soulseek.NET.Tcp
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class ConnectionException : Exception
+    {
+        public ConnectionException()
+            : base()
+        {
+        }
+
+        public ConnectionException(string message)
+            : base(message)
+        {
+        }
+
+        public ConnectionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class ConnectionStateException : Exception
+    {
+        public ConnectionStateException()
+            : base()
+        {
+        }
+
+        public ConnectionStateException(string message)
+            : base(message)
+        {
+        }
+
+        public ConnectionStateException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
