d9f327f4c05d161d6d24901b44355986b7cee314
JP Dillingham <jpdillingham@gmail.com>
2018-10-18 08:36:27 -0500

    add MessageResponseAttribute

---
+++ src/Soulseek.NET/Messaging/MessageResponseAttribute.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Soulseek.NET.Messaging
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class MessageResponseAttribute : Attribute
+    {
+        public MessageResponseAttribute(MessageCode code)
+        {
+            Code = code;
+        }
+
+        public MessageCode Code { get; private set; }
+    }
+}
