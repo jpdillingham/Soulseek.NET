db1082bbfec4612cbe63c312a1c990f2a075b276
JP Dillingham <jpdillingham@gmail.com>
2018-10-17 21:18:38 -0500

    add IConnection interface

---
+++ src/Soulseek.NET/Tcp/IConnection.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Soulseek.NET.Tcp
+{
+    using System;
+    using System.Threading.Tasks;
+
+    interface IConnection
+    {
+        event EventHandler<DataReceivedEventArgs> DataReceived;
+        event EventHandler<ConnectionStateChangedEventArgs> StateChanged;
+
+        Task ConnectAsync();
+        void Disconnect(string message = null);
+        Task SendAsync(byte[] bytes);
+    }
+}
