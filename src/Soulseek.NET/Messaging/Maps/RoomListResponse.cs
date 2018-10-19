8ad5ca038f06682ef5cfa3e0c5713bcd4cf75633
JP Dillingham <jpdillingham@gmail.com>
2018-10-19 17:46:30 -0500

    add RoomListResponse

---
+++ src/Soulseek.NET/Messaging/Maps/RoomListResponse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Soulseek.NET.Messaging.Maps
+{
+    [MessageMap(MessageCode.RoomList)]
+    public class RoomListResponse : IMessageMap<RoomListResponse>
+    {
+        public IEnumerable<Room> Rooms => RoomList;
+
+        private int RoomCount { get; set; }
+        private int UserCountCount { get; set; }
+        private List<Room> RoomList { get; set; } = new List<Room>();
+
+        public RoomListResponse MapFrom(Message message)
+        {
+            var reader = new MessageReader(message);
+
+            if (reader.Code != MessageCode.RoomList)
+            {
+                throw new MessageException($"Message Code mismatch creating Room List response (expected: {(int)MessageCode.RoomList}, received: {(int)reader.Code}");
+            }
+
+            RoomCount = reader.ReadInteger();
+
+            for (int i = 0; i < RoomCount; i++)
+            {
+                RoomList.Add(new Room() { Name = reader.ReadString() });
+            }
+
+            UserCountCount = reader.ReadInteger();
+
+            for (int i = 0; i < UserCountCount; i++)
+            {
+                RoomList[i].UserCount = reader.ReadInteger();
+            }
+
+            return this;
+        }
+    }
+
+    public class Room
+    {
+        public string Name { get; set; }
+        public int UserCount { get; set; }
+    }
+}
