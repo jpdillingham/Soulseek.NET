3cc48060c5153f94c10742d5181827e84a9a01d0
JP Dillingham <jpdillingham@gmail.com>
2018-10-20 16:38:52 -0500

    add MessageWaiter

---
+++ src/Soulseek.NET/Messaging/MessageWaiter.cs
@@ -0,0 +1,86 @@
+﻿namespace Soulseek.NET.Messaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class MessageWaiter
+    {
+        private ReaderWriterLockSlim Lock { get; set; } = new ReaderWriterLockSlim();
+        private Dictionary<object, Queue<TaskCompletionSource<object>>> Waits { get; set; } = new Dictionary<object, Queue<TaskCompletionSource<object>>>();
+
+        public TaskCompletionSource<object> WaitFor(MessageCode code, object token = null)
+        {
+            var key = GetKey(code, token);
+            var wait = new TaskCompletionSource<object>();
+
+            Lock.EnterWriteLock();
+
+            try
+            { 
+                if (Waits.ContainsKey(key))
+                {
+                    Waits[key].Enqueue(wait);
+                }
+                else
+                {
+                    Waits.Add(key, new Queue<TaskCompletionSource<object>>(new[] { wait }));
+                }
+            }
+            finally
+            {
+                Lock.ExitWriteLock();
+            }            
+            
+            return wait;
+        }
+
+        public void Complete(MessageCode code, object result)
+        {
+            Complete(code, null, result);
+        }
+        
+        public void Complete(MessageCode code, object token, object result)
+        {
+            var key = GetKey(code, token);
+
+            Lock.EnterUpgradeableReadLock();
+
+            try
+            {
+                if (Waits.ContainsKey(key))
+                {
+                    var queue = Waits[key];
+
+                    if (queue.Count > 0)
+                    {
+                        Lock.EnterWriteLock();
+
+                        try
+                        {
+                            var wait = queue.Dequeue();
+                            wait.SetResult(result);
+
+                            queue.TrimExcess();
+                        }
+                        finally
+                        {
+                            Lock.ExitWriteLock();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                Lock.ExitUpgradeableReadLock();
+            }
+
+        }
+
+        private object GetKey(MessageCode code, object token)
+        {
+            return token == null ? code : (object)new Tuple<MessageCode, object>(code, token);
+        }
+    }
+}
