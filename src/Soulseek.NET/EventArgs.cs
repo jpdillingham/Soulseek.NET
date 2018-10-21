fda695d022b6be017ded7b535844e9add39eb6b6
JP Dillingham <jpdillingham@gmail.com>
2018-10-21 13:43:24 -0500

    add Response/MessageReceivedEventArgs

--- src/Soulseek.NET/EventArgs.cs
+++ src/Soulseek.NET/EventArgs.cs
@@ -1,11 +1,24 @@
 ﻿
 namespace Soulseek.NET
 {
+    using Soulseek.NET.Messaging;
     using Soulseek.NET.Messaging.Responses;
     using System;
 
     public class SearchResultReceivedEventArgs : EventArgs
     {
         public PeerSearchReplyResponse Response { get; set; }
+    }
+
+    public class ResponseReceivedEventArgs : EventArgs
+    {
+        public Message Message { get; set; }
+        public Type ResponseType { get; set; }
+        public object Response { get; set; }
+    }
+
+    public class MessageReceivedEventArgs
+    {
+        public Message Message { get; set; }
     }
 }
