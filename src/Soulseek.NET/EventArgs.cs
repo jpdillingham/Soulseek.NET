92d3c7aec280ef84889a611dd1613c57a91a51ab
JP Dillingham <jpdillingham@gmail.com>
2018-10-22 08:47:40 -0500

    add context to MessageReceivedEventArgs

--- src/Soulseek.NET/EventArgs.cs
+++ src/Soulseek.NET/EventArgs.cs
@@ -2,23 +2,12 @@
 namespace Soulseek.NET
 {
     using Soulseek.NET.Messaging;
-    using Soulseek.NET.Messaging.Responses;
-    using System;
-
-    public class SearchResultReceivedEventArgs : EventArgs
-    {
-        public PeerSearchReplyResponse Response { get; set; }
-    }
-
-    public class ResponseReceivedEventArgs : EventArgs
-    {
-        public Message Message { get; set; }
-        public Type ResponseType { get; set; }
-        public object Response { get; set; }
-    }
 
     public class MessageReceivedEventArgs
     {
+        public string Address;
+        public string IPAddress;
+        public int Port;
         public Message Message { get; set; }
     }
 }
