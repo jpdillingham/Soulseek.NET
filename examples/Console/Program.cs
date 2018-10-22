faaf2711b3746fc91c003c1660b872b985813995
JP Dillingham <jpdillingham@gmail.com>
2018-10-21 19:29:17 -0500

    wrap example in using

--- examples/Console/Program.cs
+++ examples/Console/Program.cs
@@ -11,39 +11,41 @@
     {
         static async Task Main(string[] args)
         {
-            var client = new SoulseekClient();
-            client.Connection.StateChanged += Client_ServerStateChanged;
-            client.SearchResultReceived += Client_SearchResultReceived;
-
-            await client.ConnectAsync();
-
-            Console.WriteLine("Enter username and password:");
-
-            while (true)
+            using (var client = new SoulseekClient())
             {
-                var cmd = Console.ReadLine();
+                client.Connection.StateChanged += Client_ServerStateChanged;
+                client.SearchResultReceived += Client_SearchResultReceived;
 
-                if (cmd == "stop")
-                {
-                    client.Connection.Disconnect("User requested Disconnect");
-                }
-                if (cmd.StartsWith("search"))
-                {
-                    await client.SearchAsync(string.Join(' ', cmd.Split(' ').Skip(1)));
-                }
-                else
-                {
-                    var r = await client.LoginAsync(cmd.Split(' ')[0], cmd.Split(' ')[1]);
+                await client.ConnectAsync();
 
-                    if (r.Succeeded)
+                Console.WriteLine("Enter username and password:");
+
+                while (true)
+                {
+                    var cmd = Console.ReadLine();
+
+                    if (cmd == "stop")
                     {
-                        Console.WriteLine("Login succeeded");
-                        Console.WriteLine(JsonConvert.SerializeObject(r));
-                        //break;
+                        client.Connection.Disconnect("User requested Disconnect");
+                    }
+                    if (cmd.StartsWith("search"))
+                    {
+                        await client.SearchAsync(string.Join(' ', cmd.Split(' ').Skip(1)));
                     }
                     else
                     {
-                        Console.WriteLine("Login failed");
+                        var r = await client.LoginAsync(cmd.Split(' ')[0], cmd.Split(' ')[1]);
+
+                        if (r.Succeeded)
+                        {
+                            Console.WriteLine("Login succeeded");
+                            Console.WriteLine(JsonConvert.SerializeObject(r));
+                            //break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Login failed");
+                        }
                     }
                 }
             }
