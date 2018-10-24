70de38c907c4227567870feda2f246f4e7230c73
JP Dillingham <jpdillingham@gmail.com>
2018-10-24 07:39:12 -0500

    add private message handling

---
+++ src/Soulseek.NET/Messaging/Requests/AcknowledgePrivateMessageRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace Soulseek.NET.Messaging.Requests
+{ 
+    public class AcknowledgePrivateMessageRequest
+    {
+        public AcknowledgePrivateMessageRequest(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+        public byte[] ToByteArray()
+        {
+            return new MessageBuilder()
+                .Code(23)
+                .WriteInteger(Id)
+                .Build()
+                .ToByteArray();
+        }
+    }
+}
---
+++ src/Soulseek.NET/Messaging/Responses/PrivateMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Soulseek.NET.Messaging.Responses
+{
+    public sealed class PrivateMessage
+    {
+        public int Id { get; private set; }
+        public int Timestamp { get; private set; }
+        public string Username { get; private set; }
+        public string Message { get; private set; }
+        public bool IsAdmin { get; private set; }
+
+        public static PrivateMessage Parse(Message message)
+        {
+            var response = new PrivateMessage();
+
+            var reader = new MessageReader(message);
+
+            response.Id = reader.ReadInteger();
+            response.Timestamp = reader.ReadInteger();
+            response.Username = reader.ReadString();
+            response.Message = reader.ReadString();
+            response.IsAdmin = reader.ReadByte() == 1;
+
+            return response;
+        }
+    }
+}
