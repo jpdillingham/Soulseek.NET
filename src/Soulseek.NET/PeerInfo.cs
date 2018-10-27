0da304e98959377000332a4095ff7a8416496eb8
JP Dillingham <jpdillingham@gmail.com>
2018-10-27 18:26:05 -0500

    add PeerInfo

---
+++ src/Soulseek.NET/PeerInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Soulseek.NET
+{
+    public class PeerInfo
+    {
+        public int Queued { get; internal set; }
+        public int Active { get; internal set; }
+        public int Connected { get; internal set; }
+        public int Connecting { get; internal set; }
+        public int Disconnecting { get; internal set; }
+        public int Disconnected { get; internal set; }
+    }
+}
