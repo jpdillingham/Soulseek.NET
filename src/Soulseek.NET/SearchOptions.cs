226965c0b749f3b5f9609d4286696f42303be6e3
JP Dillingham <jpdillingham@gmail.com>
2018-10-27 22:20:48 -0500

    add SearchOptions

---
+++ src/Soulseek.NET/SearchOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soulseek.NET
+{
+    public class SearchOptions
+    {
+        public int MinimumResultFileCount { get; set; }
+        public int MinimumFreeUploadSlots { get; set; }
+        public int MaximumInQueue { get; set; }
+        public int MinimumUploadSpeed { get; set; }
+        public string FileExtension { get; set; }
+    }
+}
