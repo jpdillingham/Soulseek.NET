e1a0736b6d8e46264d8799c91ca64458029dd0b5
JP Dillingham <jpdillingham@gmail.com>
2018-10-28 13:22:13 -0500

    refactor SearchOptions, add props

--- src/Soulseek.NET/SearchOptions.cs
+++ src/Soulseek.NET/SearchOptions.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Soulseek.NET
+﻿namespace Soulseek.NET
 {
+    using System.Collections.Generic;
+
     public class SearchOptions
     {
-        public int MinimumResultFileCount { get; set; }
-        public int MinimumFreeUploadSlots { get; set; }
-        public int MaximumInQueue { get; set; }
-        public int MinimumUploadSpeed { get; set; }
-        public string FileExtension { get; set; }
+        public int Timeout { get; set; } = 15;
+        public int ResultLimit { get; set; } = 5000;
+
+        public int MinimumFileCount { get; set; } = 1;
+        public int MinimumPeerFreeUploadSlots { get; set; }
+        public int MaximumPeerInQueue { get; set; } = 9999;
+        public int MinimumPeerUploadSpeed { get; set; }
+
+        public IEnumerable<string> IgnoredFileExtensions { get; set; }
+
+        public int MinimumBitRate { get; set; } = 128;
+        public int MinimumSize { get; set; }
+        public int MinimumLength { get; set; }
+        public int MinimumSampleRate { get; set; }
+        public int MinimumBitDepth { get; set; }
     }
 }
