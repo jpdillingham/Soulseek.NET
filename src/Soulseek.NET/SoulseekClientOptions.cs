ecde2780abd1a81063bb24e52ac0808c6a7acae0
JP Dillingham <jpdillingham@gmail.com>
2018-10-27 22:21:35 -0500

    add props to Options

--- src/Soulseek.NET/SoulseekClientOptions.cs
+++ src/Soulseek.NET/SoulseekClientOptions.cs
@@ -3,5 +3,8 @@
     public class SoulseekClientOptions
     {
         public int ConcurrentPeerConnections { get; set; } = 500;
+        public int ConnectionTimeout { get; set; } = 5;
+        public int ReadTimeout { get; set; } = 5;
+        public int BufferSize { get; set; } = 4096;
     }
 }
