08f072d87b8a377546c87dff18890b16cbbdcb08
JP Dillingham <jpdillingham@gmail.com>
2018-10-30 09:25:53 -0500

    add additional props to SearchOptions

--- src/Soulseek.NET/SearchOptions.cs
+++ src/Soulseek.NET/SearchOptions.cs
@@ -7,17 +7,21 @@
         public int Timeout { get; set; } = 15;
         public int ResultLimit { get; set; } = 5000;
 
-        public int MinimumFileCount { get; set; } = 1;
+        public bool FilterResponses { get; set; } = true;
+        public int MinimumResponseFileCount { get; set; } = 1;
         public int MinimumPeerFreeUploadSlots { get; set; }
-        public int MaximumPeerInQueue { get; set; } = 9999;
+        public int MaximumPeerQueueLength { get; set; } = 1000000;
         public int MinimumPeerUploadSpeed { get; set; }
 
         public IEnumerable<string> IgnoredFileExtensions { get; set; }
 
-        public int MinimumBitRate { get; set; } = 128;
-        public int MinimumSize { get; set; }
-        public int MinimumLength { get; set; }
-        public int MinimumSampleRate { get; set; }
-        public int MinimumBitDepth { get; set; }
+        public bool FilterFiles { get; set; } = false;
+        public int MinimumFileBitRate { get; set; }
+        public int MinimumFileSize { get; set; }
+        public int MinimumFileLength { get; set; }
+        public int MinimumFileSampleRate { get; set; }
+        public int MinimumFileBitDepth { get; set; }
+        public bool IncludeConstantBitRate { get; set; } = true;
+        public bool IncludeVariableBitRate { get; set; } = true;
     }
 }
