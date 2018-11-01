23a448912dbd830e9a11059187844f8bfe38d5e2
JP Dillingham <jpdillingham@gmail.com>
2018-10-31 21:40:40 -0500

    split Room into a separate file

---
+++ src/Soulseek.NET/Messaging/Responses/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace Soulseek.NET.Messaging.Responses
+{
+
+    public sealed class Room
+    {
+        public string Name { get; internal set; }
+        public int UserCount { get; internal set; }
+
+        internal Room()
+        {
+        }
+    }
+}
--- src/Soulseek.NET/Messaging/Responses/RoomList.cs
+++ src/Soulseek.NET/Messaging/Responses/RoomList.cs
@@ -31,14 +31,4 @@
             return list.AsReadOnly();
         }
     }
-
-    public sealed class Room
-    {
-        public string Name { get; internal set; }
-        public int UserCount { get; internal set; }
-
-        internal Room()
-        {
-        }
-    }
 }
