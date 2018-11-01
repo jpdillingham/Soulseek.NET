0448c1b35d0b428585162655fb81e9db157e6e64
JP Dillingham <jpdillingham@gmail.com>
2018-11-01 09:17:56 -0500

    implement timeouts

--- src/Soulseek.NET/Messaging/MessageWaiter.cs
+++ src/Soulseek.NET/Messaging/MessageWaiter.cs
@@ -1,12 +1,29 @@
 ﻿namespace Soulseek.NET.Messaging
 {
+    using Soulseek.NET.Common;
     using System;
     using System.Collections.Concurrent;
     using System.Threading.Tasks;
+    using SystemTimer = System.Timers.Timer;
 
     public class MessageWaiter
     {
-        private ConcurrentDictionary<object, ConcurrentQueue<TaskCompletionSource<object>>> Waits { get; set; } = new ConcurrentDictionary<object, ConcurrentQueue<TaskCompletionSource<object>>>();
+        public MessageWaiter(int timeout)
+        {
+            Timeout = timeout;
+            TimeoutTimer = new SystemTimer()
+            {
+                Enabled = true,
+                AutoReset = true,
+                Interval = 500,
+            };
+
+            TimeoutTimer.Elapsed += CompleteExpiredWaits;
+        }
+
+        private int Timeout { get; set; }
+        private SystemTimer TimeoutTimer { get; set; }
+        private ConcurrentDictionary<object, ConcurrentQueue<PendingWait>> Waits { get; set; } = new ConcurrentDictionary<object, ConcurrentQueue<PendingWait>>();
 
         public void Complete(MessageCode code, object result)
         {
@@ -21,7 +38,7 @@
             {
                 if (queue.TryDequeue(out var wait))
                 {
-                    wait.SetResult(result);
+                    wait.TaskCompletionSource.SetResult(result);
                 }
             }
         }
@@ -29,20 +46,45 @@
         public TaskCompletionSource<object> Wait(MessageCode code, object token = null)
         {
             var key = GetKey(code, token);
-            var wait = new TaskCompletionSource<object>();
+            var wait = new PendingWait()
+            {
+                TaskCompletionSource = new TaskCompletionSource<object>(),
+                DateTime = DateTime.UtcNow,
+            };
 
-            Waits.AddOrUpdate(key, new ConcurrentQueue<TaskCompletionSource<object>>(new[] { wait }), (_, queue) =>
+            Waits.AddOrUpdate(key, new ConcurrentQueue<PendingWait>(new[] { wait }), (_, queue) =>
             {
                 queue.Enqueue(wait);
                 return queue;
             });
 
-            return wait;
+            return wait.TaskCompletionSource;
+        }
+
+        private void CompleteExpiredWaits(object sender, object e)
+        {
+            foreach (var queue in Waits)
+            {
+                if (queue.Value.TryPeek(out var nextPendingWait) && nextPendingWait.DateTime.AddSeconds(Timeout) < DateTime.UtcNow)
+                {
+                    if (queue.Value.TryDequeue(out var timedOutWait))
+                    {
+                        var code = ((Tuple<MessageCode, object>)queue.Key).Item1;
+                        timedOutWait.TaskCompletionSource.SetException(new MessageTimeoutException($"Message wait for {code} timed out after {Timeout} seconds."));
+                    }
+                }
+            }
         }
 
         private object GetKey(MessageCode code, object token)
         {
-            return token == null ? code : (object)new Tuple<MessageCode, object>(code, token);
+            return new Tuple<MessageCode, object>(code, token);
+        }
+
+        private class PendingWait
+        {
+            public DateTime DateTime { get; set; }
+            public TaskCompletionSource<object> TaskCompletionSource { get; set; }
         }
     }
 }
