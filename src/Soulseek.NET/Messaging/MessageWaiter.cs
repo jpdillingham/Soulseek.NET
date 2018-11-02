319e0a51cac081c857d5a8688cb1e95fbb3a0489
JP Dillingham <jpdillingham@gmail.com>
2018-11-02 08:36:06 -0500

    begin documenting MessageWaiter

--- src/Soulseek.NET/Messaging/MessageWaiter.cs
+++ src/Soulseek.NET/Messaging/MessageWaiter.cs
@@ -1,4 +1,21 @@
-﻿namespace Soulseek.NET.Messaging
+﻿// <copyright file="SoulseekClient.cs" company="JP Dillingham">
+//     Copyright(C) 2018 JP Dillingham
+//     
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//     
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//     GNU General Public License for more details.
+//     
+//     You should have received a copy of the GNU General Public License
+//     along with this program.If not, see<https://www.gnu.org/licenses/>.
+// </copyright>
+
+namespace Soulseek.NET.Messaging
 {
     using Soulseek.NET.Common;
     using System;
@@ -6,11 +23,29 @@
     using System.Threading.Tasks;
     using SystemTimer = System.Timers.Timer;
 
+    /// <summary>
+    ///     Enables await-able server messages.
+    /// </summary>
     internal class MessageWaiter
     {
+        private const int defaultTimeout = 5;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MessageWaiter"/> class with the default timeout.
+        /// </summary>
+        internal MessageWaiter()
+            : this(defaultTimeout)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MessageWaiter"/> class with the specified <paramref name="defaultTimeout"/>.
+        /// </summary>
+        /// <param name="defaultTimeout">The default timeout duration for message waits.</param>
         internal MessageWaiter(int defaultTimeout)
         {
             DefaultTimeout = defaultTimeout;
+
             TimeoutTimer = new SystemTimer()
             {
                 Enabled = true,
