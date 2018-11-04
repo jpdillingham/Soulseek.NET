1dddab79f465411e477a4a4bfdd7c938adf0a1d5
JP Dillingham <jpdillingham@gmail.com>
2018-11-04 17:33:37 -0600

    add PeerSharesRequest

---
+++ src/Soulseek.NET/Messaging/Requests/PeerSharesRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Soulseek.NET.Messaging.Requests
+{ 
+    public class PeerSharesRequest
+    {
+        public byte[] ToByteArray()
+        {
+            return new MessageBuilder()
+                .Code(MessageCode.PeerSharesRequest)
+                .Build()
+                .ToByteArray();
+        }
+    }
+}
