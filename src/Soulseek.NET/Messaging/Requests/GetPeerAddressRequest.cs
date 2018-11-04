4e544f510a459f54272775609cce4f2bb59fe2eb
JP Dillingham <jpdillingham@gmail.com>
2018-11-04 08:07:42 -0600

    add GetPeerAddressRequest

---
+++ src/Soulseek.NET/Messaging/Requests/GetPeerAddressRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Soulseek.NET.Messaging.Requests
+{
+    public class GetPeerAddressRequest
+    {
+        public string Username { get; set; }
+
+        public Message ToMessage()
+        {
+            return new MessageBuilder()
+                .Code(MessageCode.ServerGetPeerAddress)
+                .WriteString(Username)
+                .Build();
+        }
+    }
+}
