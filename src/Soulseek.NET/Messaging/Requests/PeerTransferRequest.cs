d0526e6dd188900007492c35a7244c63de805428
JP Dillingham <jpdillingham@gmail.com>
2018-11-04 17:33:47 -0600

    add PeerTransferRequest

---
+++ src/Soulseek.NET/Messaging/Requests/PeerTransferRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace Soulseek.NET.Messaging.Requests
+{ 
+    public class PeerTransferRequest
+    {
+        public PeerTransferRequest(int direction, int token, string filename, int size = 0)
+        {
+            Direction = direction;
+            Token = token;
+            Filename = filename;
+            Size = size;
+        }
+
+        public int Direction { get; set; }
+        public int Token { get; set; }
+        public string Filename { get; set; }
+        public int Size { get; set; }
+
+        public Message ToMessage()
+        {
+            return new MessageBuilder()
+                .Code(MessageCode.PeerTransferRequest)
+                .WriteInteger(Direction)
+                .WriteInteger(Token)
+                .WriteString(Filename)
+                .WriteInteger(Size)
+                .Build();
+        }
+    }
+}
