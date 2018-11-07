3636f57186a55e832ea6a86a7670f39149f54486
JP Dillingham <jpdillingham@gmail.com>
2018-11-06 20:25:16 -0600

    add header to and organize various Common classes

--- src/Soulseek.NET/Common/File.cs
+++ src/Soulseek.NET/Common/File.cs
@@ -1,22 +1,33 @@
-﻿namespace Soulseek.NET
+﻿// <copyright file="File.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET
 {
     using System.Collections.Generic;
     using System.Linq;
 
     public sealed class File
     {
-        public int Code { get; internal set; }
-        public string Filename { get; internal set; }
-        public long Size { get; internal set; }
-        public string Extension { get; internal set; }
-        public int AttributeCount { get; internal set; }
-        public IEnumerable<FileAttribute> Attributes => AttributeList.AsReadOnly();
-
-        internal List<FileAttribute> AttributeList { get; set; } = new List<FileAttribute>();
-
         internal File()
         {
         }
+
+        public int AttributeCount { get; internal set; }
+        public IEnumerable<FileAttribute> Attributes => AttributeList.AsReadOnly();
+        public int Code { get; internal set; }
+        public string Extension { get; internal set; }
+        public string Filename { get; internal set; }
+        public long Size { get; internal set; }
+        internal List<FileAttribute> AttributeList { get; set; } = new List<FileAttribute>();
 
         public int? GetAttributeValue(FileAttributeType type)
         {
--- src/Soulseek.NET/Common/PeerInfo.cs
+++ src/Soulseek.NET/Common/PeerInfo.cs
@@ -1,4 +1,16 @@
-﻿namespace Soulseek.NET
+﻿// <copyright file="PeerInfo.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET
 {
     public class PeerInfo
     {
@@ -6,11 +18,11 @@
         {
         }
 
-        public int Queued { get; internal set; }
         public int Active { get; internal set; }
         public int Connected { get; internal set; }
         public int Connecting { get; internal set; }
-        public int Disconnecting { get; internal set; }
         public int Disconnected { get; internal set; }
+        public int Disconnecting { get; internal set; }
+        public int Queued { get; internal set; }
     }
 }
