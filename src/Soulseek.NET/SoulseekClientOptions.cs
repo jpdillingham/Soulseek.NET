008c2f90a97b2b12c40a2c9e70874d2fbdfe4ab5
JP Dillingham <jpdillingham@gmail.com>
2018-11-11 11:59:34 -0600

    inherit ConnectionOptions in SoulseekClientOptions

--- src/Soulseek.NET/SoulseekClientOptions.cs
+++ src/Soulseek.NET/SoulseekClientOptions.cs
@@ -12,30 +12,16 @@
 
 namespace Soulseek.NET
 {
+    using Soulseek.NET.Tcp;
+
     /// <summary>
     ///     Options for SoulseekClient.
     /// </summary>
-    public class SoulseekClientOptions
+    public class SoulseekClientOptions : ConnectionOptions
     {
-        /// <summary>
-        ///     Gets or sets the read and write buffer size for underlying TCP connections.
-        /// </summary>
-        public int BufferSize { get; set; } = 4096;
-
-        /// <summary>
-        ///     Gets or sets the connection timeout for client and peer TCP connections.
-        /// </summary>
-        public int ConnectionTimeout { get; set; } = 5;
-
         /// <summary>
         ///     Gets or sets the message timeout used when waiting for a response from the server.
         /// </summary>
         public int MessageTimeout { get; set; } = 5;
-
-        /// <summary>
-        ///     Gets or sets the read timeout for peer TCP connections. Once connected and after reading data, if a no additional
-        ///     data is read within this threshold the connection will be forcibly disconnected.
-        /// </summary>
-        public int ReadTimeout { get; set; } = 5;
     }
 }
