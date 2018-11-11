71ad5dc82d5198bf1c533303a4cc1be0fb012a73
JP Dillingham <jpdillingham@gmail.com>
2018-11-11 12:04:33 -0600

    stub Download

---
+++ src/Soulseek.NET/Download.cs
@@ -0,0 +1,26 @@
+﻿namespace Soulseek.NET
+{
+    using Soulseek.NET.Tcp;
+
+    public sealed class Download
+    {
+        internal Download(string username, string filename, string ipAddress, int port, DownloadOptions options, IConnection peerConnection = null, IConnection transferConnection = null)
+        {
+            Username = username;
+            Filename = filename;
+            IPAddress = ipAddress;
+            Port = port;
+            Options = options ?? new DownloadOptions();
+            PeerConnection = peerConnection ?? new Connection(ConnectionType.Peer, ipAddress, port, Options);
+            TransferConnection = transferConnection ?? new Connection(ConnectionType.Transfer, ipAddress, port, Options);
+        }
+
+        public string Username { get; private set; }
+        public string IPAddress { get; private set; }
+        public int Port { get; private set; }
+        public string Filename { get; private set; }
+        public DownloadOptions Options { get; private set; }
+        private IConnection PeerConnection { get; set; }
+        private IConnection TransferConnection { get; set; }
+    }
+}
