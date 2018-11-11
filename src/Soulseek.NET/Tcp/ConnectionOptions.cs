d0220df53289cbad2e8548a2a2927e0102b7d3ab
JP Dillingham <jpdillingham@gmail.com>
2018-11-11 11:58:45 -0600

    add ConnectionOptions

---
+++ src/Soulseek.NET/Tcp/ConnectionOptions.cs
@@ -0,0 +1,21 @@
+﻿namespace Soulseek.NET.Tcp
+{
+    public class ConnectionOptions
+    {
+        /// <summary>
+        ///     Gets or sets the read and write buffer size for underlying TCP connections.
+        /// </summary>
+        public int BufferSize { get; set; } = 4096;
+
+        /// <summary>
+        ///     Gets or sets the connection timeout for client and peer TCP connections.
+        /// </summary>
+        public int ConnectionTimeout { get; set; } = 5;
+
+        /// <summary>
+        ///     Gets or sets the read timeout for peer TCP connections. Once connected and after reading data, if a no additional
+        ///     data is read within this threshold the connection will be forcibly disconnected.
+        /// </summary>
+        public int ReadTimeout { get; set; } = 5;
+    }
+}
