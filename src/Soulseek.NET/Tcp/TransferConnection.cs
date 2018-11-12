83b15aa2dfecfe932c8fc278e5eed12a56644cb2
JP Dillingham <jpdillingham@gmail.com>
2018-11-12 09:06:09 -0600

    add I/TransferConnection

---
+++ src/Soulseek.NET/Tcp/TransferConnection.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="TransferConnection.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET.Tcp
+{
+    using System;
+    using System.Threading.Tasks;
+
+    internal class TransferConnection : Connection, ITransferConnection
+    {
+        internal TransferConnection(ConnectionType type, string address, int port, ConnectionOptions options = null, ITcpClient tcpClient = null)
+            : base(type, address, port, options, tcpClient)
+        {
+        }
+
+        public async Task SendAsync(byte[] bytes)
+        {
+            await SendAsync(bytes);
+        }
+
+        public async Task<byte[]> ReadAsync(long count)
+        {
+            try
+            {
+                var intCount = (int)count;
+                return await ReadAsync(intCount);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"adsfasfdsa");
+                throw new NotImplementedException($"File sizes exceeding ~2gb are not yet supported.");
+            }
+        }
+
+        public async Task<byte[]> ReadAsync(int count)
+        {
+            return await ReadAsync(Stream, count);
+        }
+    }
+}
