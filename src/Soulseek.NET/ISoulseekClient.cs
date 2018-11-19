88c84c46c9f44cabfcb9ef37bf1eb91ddc23ee91
JP Dillingham <jpdillingham@gmail.com>
2018-11-18 20:45:08 -0600

    remove timeout from BrowseAsync()

--- src/Soulseek.NET/ISoulseekClient.cs
+++ src/Soulseek.NET/ISoulseekClient.cs
@@ -91,7 +91,7 @@ namespace Soulseek.NET
         /// <param name="timeout">The operation timeout, in seconds.</param>
         /// <param name="cancellationToken">A cancellation token.</param>
         /// <returns>The operation context, including the fetched list of files.</returns>
-        Task<BrowseResponse> BrowseAsync(string username, int timeout = 60, CancellationToken? cancellationToken = null);
+        Task<BrowseResponse> BrowseAsync(string username, CancellationToken? cancellationToken = null);
 
         /// <summary>
         ///     Asynchronously connects the client to the server specified in the <see cref="Address"/> and <see cref="Port"/> properties.
