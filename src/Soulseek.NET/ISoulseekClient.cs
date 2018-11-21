093d90c7bb08a382e495d50b6ad0a341dccbf637
JP Dillingham <jpdillingham@gmail.com>
2018-11-21 17:50:31 -0600

    add token to SearchAsync params

--- src/Soulseek.NET/ISoulseekClient.cs
+++ src/Soulseek.NET/ISoulseekClient.cs
@@ -130,7 +130,7 @@ namespace Soulseek.NET
         /// <param name="options">The operation <see cref="SearchOptions"/>.</param>
         /// <param name="cancellationToken">A cancellation token.</param>
         /// <returns>The operation context, including the search results.</returns>
-        Task<IEnumerable<SearchResponse>> SearchAsync(string searchText, SearchOptions options = null, CancellationToken? cancellationToken = null);
+        Task<IEnumerable<SearchResponse>> SearchAsync(string searchText, int token, SearchOptions options = null, CancellationToken? cancellationToken = null);
 
         Task<byte[]> DownloadAsync(string username, string filename, CancellationToken? cancellationToken = null);
 
