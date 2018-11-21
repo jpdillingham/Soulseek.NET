4d2f8d4f583f28a8737ff5aa6a4422bc3017d241
JP Dillingham <jpdillingham@gmail.com>
2018-11-21 17:50:50 -0600

    cleanup, documentation and refactoring of Search

--- src/Soulseek.NET/Search.cs
+++ src/Soulseek.NET/Search.cs
@@ -17,34 +17,28 @@ namespace Soulseek.NET
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
-    using Soulseek.NET.Messaging;
-    using Soulseek.NET.Messaging.Requests;
     using Soulseek.NET.Messaging.Responses;
-    using Soulseek.NET.Messaging.Tcp;
-    using Soulseek.NET.Tcp;
     using SystemTimer = System.Timers.Timer;
 
     /// <summary>
     ///     A single file search.
     /// </summary>
-    public sealed class Search : IDisposable
+    internal sealed class Search : IDisposable
     {
         private int resultCount = 0;
 
         /// <summary>
-        ///     Initializes a new instance of the <see cref="Search"/> class with the specified <paramref name="searchText"/>,
-        ///     <paramref name="options"/>, and <paramref name="serverConnection"/>.
+        ///     Initializes a new instance of the <see cref="Search"/> class with the specified <paramref name="searchText"/> and
+        ///     optionally specified <paramref name="token"/> and <paramref name="options"/>.
         /// </summary>
         /// <param name="searchText">The text for which to search.</param>
+        /// <param name="token">The unique search token.</param>
         /// <param name="options">The options for the search.</param>
-        /// <param name="serverConnection">The connection to use when searching.</param>
-        internal Search(string searchText, int token, SearchOptions options, IMessageConnection serverConnection)
+        public Search(string searchText, int token, SearchOptions options)
         {
             SearchText = searchText;
             Token = token;
             Options = options;
-            ServerConnection = serverConnection;
-            SearchFilters = new SearchFilters(Options);
 
             SearchTimeoutTimer = new SystemTimer()
             {
@@ -53,13 +47,9 @@ namespace Soulseek.NET
                 AutoReset = false,
             };
 
-            SearchTimeoutTimer.Elapsed += (sender, e) => { End($"The search completed after {options.SearchTimeout} seconds of inactivity."); };
+            SearchTimeoutTimer.Elapsed += (sender, e) => { Complete($"The search completed after {options.SearchTimeout} seconds of inactivity."); };
             SearchTimeoutTimer.Reset();
         }
-
-        internal Action<Search, SearchResponse> ResponseHandler { get; set; } = (search, response) => { };
-        internal Action<Search, string> EndHandler { get; set; } = (search, message) => { };
-        internal Action<Search> TimeoutHandler { get; set; } = (search) => { };
 
         /// <summary>
         ///     Gets the options for the search.
@@ -81,12 +71,19 @@ namespace Soulseek.NET
         /// </summary>
         public int Token { get; private set; }
 
-        private SearchFilters SearchFilters { get; set; }
+        /// <summary>
+        ///     Gets or sets the action invoked upon completion of the search.
+        /// </summary>
+        public Action<Search, string> CompleteHandler { get; set; } = (search, message) => { };
+
+        /// <summary>
+        ///     Gets or sets the action invoked upon receipt of a search response.
+        /// </summary>
+        public Action<Search, SearchResponse> ResponseHandler { get; set; } = (search, response) => { };
+
         private bool Disposed { get; set; } = false;
         private List<SearchResponse> ResponseList { get; set; } = new List<SearchResponse>();
         private SystemTimer SearchTimeoutTimer { get; set; }
-        private IMessageConnection ServerConnection { get; set; }
-        private MessageWaiter MessageWaiter { get; set; } = new MessageWaiter();
 
         /// <summary>
         ///     Disposes this instance.
@@ -96,23 +93,26 @@ namespace Soulseek.NET
             Dispose(true);
         }
 
-        internal void AddResponse(IMessageConnection connection, SearchResponse response)
+        /// <summary>
+        ///     Adds the specified <paramref name="response"/> to the list of responses after applying the filters specified in the search options.
+        /// </summary>
+        /// <param name="response">The response to add.</param>
+        public void AddResponse(SearchResponse response)
         {
-            Console.WriteLine($"[Adding]");
-            if (response.Token == Token && SearchFilters.ResponseMeetsOptionCriteria(response))
+            if (response.Token == Token && ResponseMeetsOptionCriteria(response))
             {
                 response.ParseFiles();
 
                 if (Options.FilterFiles)
                 {
-                    response.Files = response.Files.Where(f => SearchFilters.FileMeetsOptionCriteria(f));
+                    response.Files = response.Files.Where(f => FileMeetsOptionCriteria(f));
                 }
 
                 Interlocked.Add(ref resultCount, response.Files.Count());
 
                 if (resultCount >= Options.FileLimit)
                 {
-                    End($"The search completed after receiving {Options.FileLimit} results.");
+                    Complete($"The search completed after receiving {Options.FileLimit} results.");
                     return;
                 }
 
@@ -125,43 +125,14 @@ namespace Soulseek.NET
         }
 
         /// <summary>
-        ///     Ends the search with the specified <paramref name="state"/>.
+        ///     Completes the search with the specified <paramref name="message"/>.
         /// </summary>
-        /// <remarks>
-        ///     A state of <see cref="SearchState.Completed"/> indicates that the search completed normally by timeout or after
-        ///     having reached the result limit, while <see cref="SearchState.Stopped"/> indicates that the search was stopped
-        ///     prematurely, e.g., by error or user request.
-        /// </remarks>
-        /// <param name="state">The desired state of the search.</param>
-        internal void End(string message)
+        /// <param name="message">A message indicating how the search was completed.</param>
+        internal void Complete(string message)
         {
             SearchTimeoutTimer.Stop();
-            EndHandler(this, message);
+            CompleteHandler(this, message);
         }
-
-        ///// <summary>
-        /////     Asynchronously starts the search.
-        ///// </summary>
-        ///// <returns>This search.</returns>
-        //internal async Task<Search> SearchAsync(CancellationToken? cancellationToken)
-        //{
-        //    if (State != SearchState.Pending)
-        //    {
-        //        throw new SearchException($"The Search is already in progress or has completed.");
-        //    }
-
-        //    State = SearchState.InProgress;
-
-        //    var request = new SearchRequest(SearchText, Ticket);
-
-        //    Console.WriteLine($"Searching for {SearchText}...");
-        //    await ServerConnection.SendMessageAsync(request.ToMessage());
-
-        //    SearchTimeoutTimer.Reset();
-        //    SearchTimeoutTimer.Elapsed += (sender, e) => End(SearchState.Completed);
-
-        //    return await MessageWaiter.WaitIndefinitely<Search>(MessageCode.ServerFileSearch, Ticket.ToString(), cancellationToken);
-        //}
 
         private void Dispose(bool disposing)
         {
@@ -175,6 +146,62 @@ namespace Soulseek.NET
 
                 Disposed = true;
             }
+        }
+
+        private bool FileMeetsOptionCriteria(File file)
+        {
+            if (!Options.FilterFiles)
+            {
+                return true;
+            }
+
+            bool fileHasIgnoredExtension(File f)
+            {
+                return Options.IgnoredFileExtensions == null ? false :
+                    Options.IgnoredFileExtensions.Any(e => e == System.IO.Path.GetExtension(f.Filename));
+            }
+
+            if (file.Size < Options.MinimumFileSize || fileHasIgnoredExtension(file))
+            {
+                return false;
+            }
+
+            var bitRate = file.GetAttributeValue(FileAttributeType.BitRate);
+            var length = file.GetAttributeValue(FileAttributeType.Length);
+            var bitDepth = file.GetAttributeValue(FileAttributeType.BitDepth);
+            var sampleRate = file.GetAttributeValue(FileAttributeType.SampleRate);
+
+            if ((bitRate != null && bitRate < Options.MinimumFileBitRate) ||
+                (length != null && length < Options.MinimumFileLength) ||
+                (bitDepth != null && bitDepth < Options.MinimumFileBitDepth) ||
+                (sampleRate != null && sampleRate < Options.MinimumFileSampleRate))
+            {
+                return false;
+            }
+
+            var constantBitRates = new[] { 32, 64, 128, 192, 256, 320 };
+            var isConstant = constantBitRates.Any(b => b == bitRate);
+
+            if (bitRate != null && ((!Options.IncludeConstantBitRate && isConstant) || (!Options.IncludeVariableBitRate && !isConstant)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ResponseMeetsOptionCriteria(SearchResponse response)
+        {
+            if (Options.FilterResponses && (
+                    response.FileCount < Options.MinimumResponseFileCount ||
+                    response.FreeUploadSlots < Options.MinimumPeerFreeUploadSlots ||
+                    response.UploadSpeed < Options.MinimumPeerUploadSpeed ||
+                    response.QueueLength > Options.MaximumPeerQueueLength))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
