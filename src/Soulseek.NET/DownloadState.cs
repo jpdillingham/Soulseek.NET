a86ed360eb7d7da02227d8f825749d3e0746ab8f
JP Dillingham <jpdillingham@gmail.com>
2018-11-25 16:43:27 -0600

    add DownloadState

---
+++ src/Soulseek.NET/DownloadState.cs
@@ -0,0 +1,14 @@
+﻿namespace Soulseek.NET
+{
+    using System;
+
+    [Flags]
+    public enum DownloadState
+    {
+        Queued = 0,
+        InProgress = 1,
+        Completed = 2,
+        Cancelled = 4,
+        TimedOut = 8,
+    }
+}
