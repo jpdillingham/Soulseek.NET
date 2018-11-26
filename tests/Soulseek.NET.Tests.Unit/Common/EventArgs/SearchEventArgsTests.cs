680432bec4a7fa7289186cb8d996ab42ae366be1
JP Dillingham <jpdillingham@gmail.com>
2018-11-25 21:12:17 -0600

    add SearchEventArgsTests

---
+++ tests/Soulseek.NET.Tests.Unit/Common/EventArgs/SearchEventArgsTests.cs
@@ -0,0 +1,62 @@
+﻿using Soulseek.NET.Messaging.Responses;
+using System;
+using Xunit;
+
+namespace Soulseek.NET.Tests.Unit.Common.EventArgs
+{
+    public class SearchEventArgsTests
+    {
+        [Trait("Category", "Instantiation")]
+        [Trait("Class", "SearchEventArgs")]
+        [Fact(DisplayName = "Instantiates with valid Search")]
+        public void SearchEventArgs_Instantiates_With_Valid_Search()
+        {
+            var searchText = Guid.NewGuid().ToString();
+            var token = new Random().Next();
+
+            var search = new Search(searchText, token, new SearchOptions());
+
+            var e = new SearchEventArgs(search);
+
+            Assert.Equal(searchText, e.SearchText);
+            Assert.Equal(token, e.Token);
+        }
+
+        [Trait("Category", "Instantiation")]
+        [Trait("Class", "SearchResponseReceivedEventArgs")]
+        [Fact(DisplayName = "Instantiates with valid Search and SearchResponse")]
+        public void SearchResponseReceivedEventArgs_Instantiates_With_Valid_Search_And_SearchResponse()
+        {
+            var searchText = Guid.NewGuid().ToString();
+            var token = new Random().Next();
+
+            var search = new Search(searchText, token, new SearchOptions());
+
+            var response = new SearchResponse();
+
+            var e = new SearchResponseReceivedEventArgs(search, response);
+
+            Assert.Equal(searchText, e.SearchText);
+            Assert.Equal(token, e.Token);
+            Assert.Equal(response, e.Response);
+        }
+
+        [Trait("Category", "Instantiation")]
+        [Trait("Class", "SearchStateChangedEventArgs")]
+        [Fact(DisplayName = "Instantiates with valid Search")]
+        public void SearchStateChangedEventArgs_Instantiates_With_Valid_Search()
+        {
+            var searchText = Guid.NewGuid().ToString();
+            var token = new Random().Next();
+
+            var search = new Search(searchText, token, new SearchOptions());
+            search.SetProperty("State", SearchState.Completed);
+
+            var e = new SearchStateChangedEventArgs(search);
+
+            Assert.Equal(searchText, e.SearchText);
+            Assert.Equal(token, e.Token);
+            Assert.Equal(SearchState.Completed, e.State);
+        }
+    }
+}
