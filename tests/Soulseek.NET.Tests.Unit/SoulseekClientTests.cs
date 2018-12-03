074ebc9e230ede07b3f05c31c3f8f4bacbac1818
JP Dillingham <jpdillingham@gmail.com>
2018-12-03 08:15:55 -0600

    add first SoulseekClient tests

---
+++ tests/Soulseek.NET.Tests.Unit/SoulseekClientTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Soulseek.NET.Tests.Unit
+{
+    using Xunit;
+
+    public class SoulseekClientTests
+    {
+        [Trait("Category", "Instantiation")]
+        [Fact(DisplayName = "Uses defaults for minimal constructor")]
+        public void Uses_Defaults_For_Minimal_Constructor()
+        {
+            var s = new SoulseekClient();
+
+            var defaultServer = s.GetField<string>("DefaultAddress");
+            var defaultPort = s.GetField<int>("DefaultPort");
+            var defaultOptions = new SoulseekClientOptions();
+
+            Assert.Equal(defaultServer, s.Address);
+            Assert.Equal(defaultPort, s.Port);
+            Assert.Equal(defaultOptions, s.Options);
+        }
+
+        [Trait("Category", "Instantiation")]
+        [Fact(DisplayName = "Instantiates without exception")]
+        public void Instantiates_Without_Exception()
+        {
+            SoulseekClient s = null;
+
+            var ex = Record.Exception(() => s = new SoulseekClient());
+
+            Assert.Null(ex);
+            Assert.NotNull(s);
+        }
+    }
+}
