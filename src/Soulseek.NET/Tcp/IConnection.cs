3f9b54d6999c0790dc8cd3b541eae188a06acc53
JP Dillingham <jpdillingham@gmail.com>
2018-12-19 18:54:36 -0600

    document IConnection, use IEnumerable<byte> over byte[], rename Send to Write

--- src/Soulseek.NET/Tcp/IConnection.cs
+++ src/Soulseek.NET/Tcp/IConnection.cs
@@ -13,39 +13,96 @@
 namespace Soulseek.NET.Tcp
 {
     using System;
+    using System.Collections.Generic;
     using System.Net;
     using System.Threading.Tasks;
 
+    /// <summary>
+    ///     Provides client connections for TCP network services.
+    /// </summary>
     internal interface IConnection : IDisposable
     {
-        event EventHandler<ConnectionStateChangedEventArgs> StateChanged;
+        /// <summary>
+        ///     Occurs when the connection is connected.
+        /// </summary>
         event EventHandler Connected;
-        event EventHandler<string> Disconnected;
+
+        /// <summary>
+        ///     Occurs when data is ready from the connection.
+        /// </summary>
         event EventHandler<ConnectionDataEventArgs> DataRead;
 
-        #region Public Properties
+        /// <summary>
+        ///     Occurs when the connection is disconnected.
+        /// </summary>
+        event EventHandler<string> Disconnected;
 
+        /// <summary>
+        ///     Occurs when the connection state changes.
+        /// </summary>
+        event EventHandler<ConnectionStateChangedEventArgs> StateChanged;
+
+        /// <summary>
+        ///     Gets or sets the generic connection context.
+        /// </summary>
         object Context { get; set; }
+
+        /// <summary>
+        ///     Gets the remote IP address of the connection.
+        /// </summary>
         IPAddress IPAddress { get; }
+
+        /// <summary>
+        ///     Gets the unique identifier of the connection.
+        /// </summary>
         ConnectionKey Key { get; }
+
+        /// <summary>
+        ///     Gets the options for the connection.
+        /// </summary>
         ConnectionOptions Options { get; }
+
+        /// <summary>
+        ///     Gets the remote port of the connection.
+        /// </summary>
         int Port { get; }
+
+        /// <summary>
+        ///     Gets the current connection state.
+        /// </summary>
         ConnectionState State { get; }
 
-        #endregion Public Properties
-
-        #region Public Methods
-
+        /// <summary>
+        ///     Asynchronously connects the client to the configured <see cref="IPAddress"/> and <see cref="Port"/>.
+        /// </summary>
+        /// <returns>A Task representing the asynchronous operation.</returns>
         Task ConnectAsync();
 
+        /// <summary>
+        ///     Disconnects the client.
+        /// </summary>
+        /// <param name="message">The optional message or reason for the disconnect.</param>
         void Disconnect(string message = null);
 
-        Task<byte[]> ReadAsync(int count);
+        /// <summary>
+        ///     Asynchronously reads the specified number of bytes from the connection.
+        /// </summary>
+        /// <param name="count">The number of bytes to read.</param>
+        /// <returns>The read bytes.</returns>
+        Task<IEnumerable<byte>> ReadAsync(int count);
 
-        Task<byte[]> ReadAsync(long count);
+        /// <summary>
+        ///     Asynchronously reads the specified number of bytes from the connection.
+        /// </summary>
+        /// <param name="count">The number of bytes to read.</param>
+        /// <returns>The read bytes.</returns>
+        Task<IEnumerable<byte>> ReadAsync(long count);
 
-        Task SendAsync(byte[] bytes);
-
-        #endregion Public Methods
+        /// <summary>
+        ///     Asynchronously writes the specified bytes to the connection.
+        /// </summary>
+        /// <param name="bytes">The bytes to write.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
+        Task WriteAsync(IEnumerable<byte> bytes);
     }
 }
