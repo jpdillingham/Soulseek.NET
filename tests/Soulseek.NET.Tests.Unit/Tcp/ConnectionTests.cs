7c527b68d05505040c9fe746bf104a026b2a3cbf
JP Dillingham <jpdillingham@gmail.com>
2018-12-21 13:13:02 -0600

    add instantiation tests for Connection

--- tests/Soulseek.NET.Tests.Unit/Tcp/ConnectionTests.cs
+++ tests/Soulseek.NET.Tests.Unit/Tcp/ConnectionTests.cs
@@ -12,9 +12,59 @@
 
 namespace Soulseek.NET.Tests.Unit.Tcp
 {
+    using Moq;
+    using Soulseek.NET.Tcp;
+    using System.Net;
     using Xunit;
 
     public class ConnectionTests
     {
+        [Trait("Category", "Instantiation")]
+        [Fact(DisplayName = "Instantiates properly")]
+        public void Instantiates_Properly()
+        {
+            Connection c = null;
+
+            var ip = new IPAddress(0x0);
+            var port = 1;
+
+            var ex = Record.Exception(() => c = new Connection(ip, port));
+
+            Assert.Null(ex);
+            Assert.NotNull(c);
+
+            Assert.Equal(ip, c.IPAddress);
+            Assert.Equal(port, c.Port);
+        }
+
+        [Trait("Category", "Instantiation")]
+        [Fact(DisplayName = "Instantiates with given options")]
+        public void Instantiates_With_Given_Options()
+        {
+            var ip = new IPAddress(0x0);
+            var port = 1;
+
+            var options = new ConnectionOptions(1, 1, 1);
+
+            var c = new Connection(ip, port, options);
+
+            Assert.Equal(options, c.Options);
+        }
+
+        [Trait("Category", "Instantiation")]
+        [Fact(DisplayName = "Instantiates with given TcpClient")]
+        public void Instantiates_With_Given_TcpClient()
+        {
+            var ip = new IPAddress(0x0);
+            var port = 1;
+
+            var t = new Mock<ITcpClient>();
+
+            var c = new Connection(ip, port, tcpClient: t.Object);
+
+            var ct = c.GetProperty<ITcpClient>("TcpClient");
+
+            Assert.Equal(t.Object, ct);
+        }
     }
 }
