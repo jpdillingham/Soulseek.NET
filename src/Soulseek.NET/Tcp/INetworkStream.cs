eabdc414b96228825156e9d7d3c48172cced2a2f
JP Dillingham <jpdillingham@gmail.com>
2018-12-26 16:55:11 -0600

    add INetworkStream

---
+++ src/Soulseek.NET/Tcp/INetworkStream.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soulseek.NET.Tcp
+{
+    interface INetworkStream : IDisposable
+    {
+        Task WriteAsync(byte[] buffer, int offset, int count);
+        Task<int> ReadAsync(byte[] buffer, int offset, int count);
+        void Close();
+    }
+}
