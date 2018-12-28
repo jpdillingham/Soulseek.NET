ed92fa21980e37364e54ccda1e7ea05924975d0e
JP Dillingham <jpdillingham@gmail.com>
2018-12-28 10:59:56 -0600

    add RequestsTests, add tests for AcknowledgePrivateMessageRequest

---
+++ tests/Soulseek.NET.Tests.Unit/Messaging/RequestsTests.cs
@@ -0,0 +1,49 @@
+﻿using Soulseek.NET.Messaging;
+// <copyright file="RequestsTests.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET.Tests.Unit.Messaging
+{
+    using Soulseek.NET.Messaging.Requests;
+    using System;
+    using Xunit;
+
+    public class RequestsTests
+    {
+        [Trait("Category", "Instantiation")]
+        [Trait("Request", "AcknowledgePrivateMessageRequest")]
+        [Fact(DisplayName = "AcknowledgePrivateMessageRequest instantiates properly")]
+        public void AcknowledgePrivateMessageRequest_Instantiates_Properly()
+        {
+            var num = new Random().Next();
+            var a = new AcknowledgePrivateMessageRequest(num);
+
+            Assert.Equal(num, a.Id);
+        }
+
+        [Trait("Category", "ToMessage")]
+        [Trait("Request", "AcknowledgePrivateMessageRequest")]
+        [Fact(DisplayName = "AcknowledgePrivateMessageRequest constructs the correct Message")]
+        public void AcknowledgePrivateMessageRequest_Constructs_The_Correct_Message()
+        {
+            var num = new Random().Next();
+            var msg = new AcknowledgePrivateMessageRequest(num).ToMessage();
+
+            Assert.Equal(MessageCode.ServerAcknowledgePrivateMessage, msg.Code);
+            Assert.Equal(8, msg.Length);
+
+            var reader = new MessageReader(msg);
+
+            Assert.Equal(num, reader.ReadInteger());
+        }
+    }
+}
