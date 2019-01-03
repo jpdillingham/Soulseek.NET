9045206948751cb57f1c81dd22d14fc523f9e7a4
JP Dillingham <jpdillingham@gmail.com>
2019-01-03 08:06:27 -0600

    add instantiation test, add traits, displaynames and copyright header

--- tests/Soulseek.NET.Tests.Unit/Messaging/MessageBuilderTests.cs
+++ tests/Soulseek.NET.Tests.Unit/Messaging/MessageBuilderTests.cs
@@ -1,14 +1,35 @@
-﻿namespace Soulseek.NET.Tests.Unit.Messaging
+﻿// <copyright file="MessageBuilderTests.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET.Tests.Unit.Messaging
 {
     using Soulseek.NET.Messaging;
     using System;
-    using System.Collections.Generic;
-    using System.Text;
     using Xunit;
 
     public class MessageBuilderTests
     {
-        [Fact]
+        [Trait("Category", "Instantiation")]
+        [Fact(DisplayName = "Instantiates without exception")]
+        public void Instantiates_Without_Exception()
+        {
+            MessageBuilder builder = null;
+            var ex = Record.Exception(() => builder = new MessageBuilder());
+
+            Assert.Null(ex);
+        }
+
+        [Trait("Category", "Compress")]
+        [Fact(DisplayName = "Compress produces valid data")]
         public void Compression_Works()
         {
             var txt = Guid.NewGuid().ToString();
