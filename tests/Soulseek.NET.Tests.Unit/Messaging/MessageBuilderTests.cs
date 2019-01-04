3c3e41024881dc97caf21af5eb955b882939a8f6
JP Dillingham <jpdillingham@gmail.com>
2019-01-04 07:07:03 -0600

    add tests for Code() and Compress()

--- tests/Soulseek.NET.Tests.Unit/Messaging/MessageBuilderTests.cs
+++ tests/Soulseek.NET.Tests.Unit/Messaging/MessageBuilderTests.cs
@@ -12,8 +12,12 @@
 
 namespace Soulseek.NET.Tests.Unit.Messaging
 {
+    using Soulseek.NET.Exceptions;
     using Soulseek.NET.Messaging;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
     using Xunit;
 
     public class MessageBuilderTests
@@ -26,6 +30,37 @@ namespace Soulseek.NET.Tests.Unit.Messaging
             var ex = Record.Exception(() => builder = new MessageBuilder());
 
             Assert.Null(ex);
+        }
+
+        [Trait("Category", "Code")]
+        [Fact(DisplayName = "Code sets code bytes")]
+        public void Code_Sets_Code_Bytes()
+        {
+            var builder = new MessageBuilder();
+
+            builder.Code(MessageCode.PeerBrowseRequest);
+
+            var code = builder.GetProperty<List<byte>>("CodeBytes");
+
+            Assert.Equal(BitConverter.GetBytes((int)MessageCode.PeerBrowseRequest), code);
+        }
+
+        [Trait("Category", "Code")]
+        [Fact(DisplayName = "Code resets code bytes")]
+        public void Code_Resets_Code_Bytes()
+        {
+            var builder = new MessageBuilder();
+
+            builder.Code(MessageCode.PeerBrowseRequest);
+
+            var code1 = builder.GetProperty<List<byte>>("CodeBytes");
+
+            builder.Code(MessageCode.PeerBrowseResponse);
+
+            var code2 = builder.GetProperty<List<byte>>("CodeBytes");
+
+            Assert.Equal(BitConverter.GetBytes((int)MessageCode.PeerBrowseRequest).ToList(), code1);
+            Assert.Equal(BitConverter.GetBytes((int)MessageCode.PeerBrowseResponse).ToList(), code2);
         }
 
         [Trait("Category", "Build")]
@@ -55,7 +90,7 @@ namespace Soulseek.NET.Tests.Unit.Messaging
 
         [Trait("Category", "Compress")]
         [Fact(DisplayName = "Compress produces valid data")]
-        public void Compression_Works()
+        public void Compress_Produces_Valid_Data()
         {
             var txt = Guid.NewGuid().ToString();
             var num = new Random().Next();
@@ -76,6 +111,46 @@ namespace Soulseek.NET.Tests.Unit.Messaging
             Assert.Equal(txt, reader.ReadString());
             Assert.Equal(num, reader.ReadInteger());
             Assert.Equal(txt2, reader.ReadString());
+        }
+
+        [Trait("Category", "Compress")]
+        [Fact(DisplayName = "Compress throws when payload is empty")]
+        public void Compress_Throws_When_Payload_Is_Empty()
+        {
+            var ex = Record.Exception(() => new MessageBuilder()
+                .Code(MessageCode.PeerInfoRequest)
+                .Compress());
+
+            Assert.NotNull(ex);
+            Assert.IsType<InvalidOperationException>(ex);
+        }
+
+        [Trait("Category", "Compress")]
+        [Fact(DisplayName = "Compress throws when already compressed")]
+        public void Compress_Throws_When_Already_Compressed()
+        {
+            var ex = Record.Exception(() => new MessageBuilder()
+                .Code(MessageCode.PeerInfoRequest)
+                .WriteString("foo")
+                .Compress()
+                .Compress());
+
+            Assert.NotNull(ex);
+            Assert.IsType<InvalidOperationException>(ex);
+            Assert.Contains("already", ex.Message);
+        }
+
+        [Trait("Category", "Compress")]
+        [Fact(DisplayName = "Compress throws MessageCompressionException on compression exception")]
+        public void Compress_Throws_MessageCompressionException_On_Zlib_Exception()
+        {
+            var builder = new MessageBuilder();
+            var ex = Record.Exception(() => builder.InvokeMethod("Compress", BindingFlags.NonPublic | BindingFlags.Instance, null, null));
+
+            Assert.NotNull(ex);
+            Assert.NotNull(ex.InnerException);
+            Assert.NotNull(ex.InnerException.InnerException);
+            Assert.IsType<MessageCompressionException>(ex.InnerException.InnerException);
         }
     }
 }
