7ac58dc11316057d83415e90e0b84e7c878c8f48
JP Dillingham <jpdillingham@gmail.com>
2019-01-17 07:34:51 -0600

    remove unused GetStableHashCode() extension

--- src/Soulseek.NET/Common/Extensions.cs
+++ src/Soulseek.NET/Common/Extensions.cs
@@ -48,29 +48,6 @@ namespace Soulseek.NET
             task.ContinueWith(t => { Debug.WriteLine($"Forgotten Thread Error: {t.Exception.Message}", t.Exception); }, TaskContinuationOptions.OnlyOnFaulted);
         }
 
-        public static int GetStableHashCode(this string str)
-        {
-            unchecked
-            {
-                int hash1 = 5381;
-                int hash2 = hash1;
-
-                for (int i = 0; i < str.Length && str[i] != '\0'; i += 2)
-                {
-                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
-
-                    if (i == str.Length - 1 || str[i + 1] == '\0')
-                    {
-                        break;
-                    }
-
-                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
-                }
-
-                return hash1 + (hash2 * 1566083941);
-            }
-        }
-
         public static void RemoveAll<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> concurrentDictionary)
         {
             while (!concurrentDictionary.IsEmpty)
