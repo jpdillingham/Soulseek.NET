de0bd7a7e3b703040e0ff830993438e966355c85
JP Dillingham <jpdillingham@gmail.com>
2019-01-20 19:04:03 -0600

    fix header and sort order of usings

--- src/Soulseek.NET/SoulseekClient.cs
+++ src/Soulseek.NET/SoulseekClient.cs
@@ -1,19 +1,17 @@
 ﻿// <copyright file="SoulseekClient.cs" company="JP Dillingham">
-//     Copyright (c) JP Dillingham. All rights reserved. // This program is free software: you can redistribute it and/or modify it
-//     under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the
-//     License, or (at your option) any later version. // This program is distributed in the hope that it will be useful, but
-//     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU
-//     General Public License for more details. // You should have received a copy of the GNU General Public License along with
-//     this program. If not, see https://www.gnu.org/licenses/.
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
 // </copyright>
 
 namespace Soulseek.NET
 {
-    using Soulseek.NET.Exceptions;
-    using Soulseek.NET.Messaging;
-    using Soulseek.NET.Messaging.Messages;
-    using Soulseek.NET.Messaging.Tcp;
-    using Soulseek.NET.Tcp;
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
@@ -21,6 +19,11 @@ namespace Soulseek.NET
     using System.Net;
     using System.Threading;
     using System.Threading.Tasks;
+    using Soulseek.NET.Exceptions;
+    using Soulseek.NET.Messaging;
+    using Soulseek.NET.Messaging.Messages;
+    using Soulseek.NET.Messaging.Tcp;
+    using Soulseek.NET.Tcp;
 
     /// <summary>
     ///     A client for the Soulseek file sharing network.
