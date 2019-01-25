22d6692789d0b66c8b87811a05b6d98e56b74703
JP Dillingham <jpdillingham@gmail.com>
2019-01-25 08:48:14 -0600

    add DiagnosticEventArgs

---
+++ src/Soulseek.NET/Common/DiagnosticMessageLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace Soulseek.NET.Common
+{
+    public enum DiagnosticMessageLevel
+    {
+        Debug = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3,
+    }
+}
---
+++ src/Soulseek.NET/Common/EventArgs/DiagnosticEventArgs.cs
@@ -0,0 +1,25 @@
+﻿namespace Soulseek.NET.Common.EventArgs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class DiagnosticEventArgs : EventArgs
+    {
+    }
+
+    public class DiagnosticMessageGeneratedEventArgs : DiagnosticEventArgs
+    {
+        public DiagnosticMessageGeneratedEventArgs(DiagnosticMessageLevel level, string message, Exception exception = null)
+        {
+            Level = level;
+            Message = message;
+            Exception = exception;
+        }
+
+        public DiagnosticMessageLevel Level { get; }
+        public string Message { get; }
+        public bool IncludesException => Exception == null;
+        public Exception Exception { get; }
+    }
+}
