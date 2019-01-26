0535b620c29c3d67ff4568d1e0826dc1f20c5b03
JP Dillingham <jpdillingham@gmail.com>
2019-01-26 12:21:44 -0600

    clean up and document IDiagnosticMessageFactory

--- src/Soulseek.NET/Common/IDiagnosticMessageFactory.cs
+++ src/Soulseek.NET/Common/IDiagnosticMessageFactory.cs
@@ -1,14 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="IDiagnosticMessageFactory.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
 
 namespace Soulseek.NET
 {
+    using System;
+
+    /// <summary>
+    ///     Creates diagnostic messages.
+    /// </summary>
     internal interface IDiagnosticMessageFactory
     {
+        /// <summary>
+        ///     Create an <see cref="DiagnosticMessageLevel.Error"/> diagnostic message.
+        /// </summary>
+        /// <param name="message">The desired message.</param>
+        /// <param name="exception">An optional Exception.</param>
         void Error(string message, Exception exception = null);
+
+        /// <summary>
+        ///     Create a <see cref="DiagnosticMessageLevel.Warning"/> diagnostic message.
+        /// </summary>
+        /// <param name="message">The desired message.</param>
+        /// <param name="exception">An optional Exception.</param>
         void Warning(string message, Exception exception = null);
+
+        /// <summary>
+        ///     Create an <see cref="DiagnosticMessageLevel.Info"/> diagnostic message.
+        /// </summary>
+        /// <param name="message">The desired message.</param>
         void Info(string message);
+
+        /// <summary>
+        ///     Create a <see cref="DiagnosticMessageLevel.Debug"/> diagnostic message.
+        /// </summary>
+        /// <param name="message">The desired message.</param>
         void Debug(string message);
     }
 }
