ac79ade560c8ad73e6fe06ee0e78468e3de411e9
JP Dillingham <jpdillingham@gmail.com>
2019-01-28 08:08:27 -0600

    add remaining DiagnosticFactory tests

--- tests/Soulseek.NET.Tests.Unit/Common/DiagnosticFactoryTests.cs
+++ tests/Soulseek.NET.Tests.Unit/Common/DiagnosticFactoryTests.cs
@@ -63,5 +63,93 @@ namespace Soulseek.NET.Tests.Unit
 
             Assert.Null(e);
         }
+
+        [Trait("Category", "Info")]
+        [Theory(DisplayName = "Raises event on info"), AutoData]
+        public void Raises_Event_On_Info(string message)
+        {
+            DiagnosticGeneratedEventArgs e = null;
+
+            var d = new DiagnosticFactory(this, DiagnosticLevel.Info, (sender, args) =>
+            {
+                e = args;
+            });
+
+            d.Info(message);
+
+            Assert.Equal(message, e.Message);
+            Assert.Equal(DiagnosticLevel.Info, e.Level);
+            Assert.False(e.IncludesException);
+            Assert.Null(e.Exception);
+        }
+
+        [Trait("Category", "Info")]
+        [Theory(DisplayName = "Does not raise event on info when level is > Info"), AutoData]
+        public void Does_Not_Raise_Event_On_Info_When_Level_Is_Gt_Info(string message)
+        {
+            DiagnosticGeneratedEventArgs e = null;
+
+            var d = new DiagnosticFactory(this, DiagnosticLevel.Warning, (sender, args) =>
+            {
+                e = args;
+            });
+
+            d.Info(message);
+
+            Assert.Null(e);
+        }
+
+        [Trait("Category", "Warning")]
+        [Theory(DisplayName = "Raises event on warning"), AutoData]
+        public void Raises_Event_On_Warning(string message)
+        {
+            DiagnosticGeneratedEventArgs e = null;
+
+            var d = new DiagnosticFactory(this, DiagnosticLevel.Warning, (sender, args) =>
+            {
+                e = args;
+            });
+
+            d.Warning(message);
+
+            Assert.Equal(message, e.Message);
+            Assert.Equal(DiagnosticLevel.Warning, e.Level);
+            Assert.False(e.IncludesException);
+            Assert.Null(e.Exception);
+        }
+
+        [Trait("Category", "Warning")]
+        [Theory(DisplayName = "Does not raise event on warning when level is > Warning"), AutoData]
+        public void Does_Not_Raise_Event_On_Warning_When_Level_Is_Gt_Warning(string message)
+        {
+            DiagnosticGeneratedEventArgs e = null;
+
+            var d = new DiagnosticFactory(this, DiagnosticLevel.None, (sender, args) =>
+            {
+                e = args;
+            });
+
+            d.Warning(message);
+
+            Assert.Null(e);
+        }
+
+        [Trait("Category", "None")]
+        [Theory(DisplayName = "Does not raise event when level is None"), AutoData]
+        public void Does_Not_Raise_Event_When_Level_Is_None(string message)
+        {
+            DiagnosticGeneratedEventArgs e = null;
+
+            var d = new DiagnosticFactory(this, DiagnosticLevel.None, (sender, args) =>
+            {
+                e = args;
+            });
+
+            d.Debug(message);
+            d.Info(message);
+            d.Warning(message);
+
+            Assert.Null(e);
+        }
     }
 }
