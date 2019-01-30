9d7d752e56975e247b9ac577e5439da47c46f943
JP Dillingham <jpdillingham@gmail.com>
2019-01-30 07:52:20 -0600

    add tests for guard clauses in BrowseAsync

---
+++ tests/Soulseek.NET.Tests.Unit/Client/BrowseAsyncTests.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="BrowseAsyncTests.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET.Tests.Unit.Client
+{
+    using System;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class BrowseAsyncTests
+    {
+        [Trait("Category", "BrowseAsync")]
+        [Theory(DisplayName = "BrowseAsync throws ArgumentException given bad username")]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task BrowseAsync_Throws_ArgumentException_Given_Bad_Username(string username)
+        {
+            var s = new SoulseekClient();
+
+            var ex = await Record.ExceptionAsync(async () => await s.BrowseAsync(username));
+
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentException>(ex);
+        }
+
+        [Trait("Category", "BrowseAsync")]
+        [Fact(DisplayName = "BrowseAsync throws InvalidOperationException when not connected")]
+        public async Task BrowseAsync_Throws_InvalidOperationException_When_Not_Connected()
+        {
+            var s = new SoulseekClient();
+
+            var ex = await Record.ExceptionAsync(async () => await s.BrowseAsync("foo"));
+
+            Assert.NotNull(ex);
+            Assert.IsType<InvalidOperationException>(ex);
+            Assert.Contains("Connected", ex.Message);
+        }
+
+        [Trait("Category", "BrowseAsync")]
+        [Fact(DisplayName = "BrowseAsync throws InvalidOperationException when not logged in")]
+        public async Task BrowseAsync_Throws_InvalidOperationException_When_Not_Logged_In()
+        {
+            var s = new SoulseekClient();
+            s.SetProperty("State", SoulseekClientStates.Connected);
+
+            var ex = await Record.ExceptionAsync(async () => await s.BrowseAsync("foo"));
+
+            Assert.NotNull(ex);
+            Assert.IsType<InvalidOperationException>(ex);
+            Assert.Contains("logged in", ex.Message);
+        }
+    }
+}
