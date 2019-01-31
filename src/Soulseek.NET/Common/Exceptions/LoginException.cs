a80d5cd8a52108341ced2d0a2a92e2c64d18e4c2
JP Dillingham <jpdillingham@gmail.com>
2019-01-31 07:55:44 -0600

    document DownloadException and LoginException

--- src/Soulseek.NET/Common/Exceptions/LoginException.cs
+++ src/Soulseek.NET/Common/Exceptions/LoginException.cs
@@ -16,25 +16,49 @@ namespace Soulseek.NET.Exceptions
     using System.Diagnostics.CodeAnalysis;
     using System.Runtime.Serialization;
 
+    /// <summary>
+    ///     Represents errors that occur while logging in to the Soulseek network.
+    /// </summary>
     [ExcludeFromCodeCoverage]
     [Serializable]
     public class LoginException : SoulseekClientException
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="LoginException"/> class.
+        /// </summary>
         public LoginException()
             : base()
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="LoginException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public LoginException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="LoginException"/> class with a specified error message and a
+        ///     reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner
+        ///     exception is specified.
+        /// </param>
         public LoginException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="LoginException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
         protected LoginException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
