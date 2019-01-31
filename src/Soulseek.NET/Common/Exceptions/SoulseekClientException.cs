4f3c7756030224fcb7e5612d2440a69bfdc95667
JP Dillingham <jpdillingham@gmail.com>
2019-01-31 07:46:33 -0600

    add standard documentation to SoulseekClientException

--- src/Soulseek.NET/Common/Exceptions/SoulseekClientException.cs
+++ src/Soulseek.NET/Common/Exceptions/SoulseekClientException.cs
@@ -15,27 +15,50 @@ namespace Soulseek.NET.Exceptions
     using System;
     using System.Diagnostics.CodeAnalysis;
     using System.Runtime.Serialization;
-    using System.Security.Permissions;
 
+    /// <summary>
+    ///     Represents errors that occur during execution of <see cref="SoulseekClient"/>.
+    /// </summary>
     [ExcludeFromCodeCoverage]
     [Serializable]
     public class SoulseekClientException : Exception
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SoulseekClientException"/> class.
+        /// </summary>
         public SoulseekClientException()
             : base()
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SoulseekClientException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public SoulseekClientException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SoulseekClientException"/> class with a specified error message and a
+        ///     reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner
+        ///     exception is specified.
+        /// </param>
         public SoulseekClientException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SoulseekClientException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
         protected SoulseekClientException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
