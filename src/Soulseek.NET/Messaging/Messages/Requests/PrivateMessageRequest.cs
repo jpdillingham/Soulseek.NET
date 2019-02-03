58b3df4b46111d6dc8806ffe48f3d59c5364a1b9
JP Dillingham <jpdillingham@gmail.com>
2019-02-02 20:55:35 -0600

    add PrivateMessageRequest

---
+++ src/Soulseek.NET/Messaging/Messages/Requests/PrivateMessageRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace Soulseek.NET.Messaging.Messages
+{
+    internal class PrivateMessageRequest
+    {
+        public PrivateMessageRequest(string username, string message)
+        {
+            Username = username;
+            Message = message;
+        }
+
+        public string Username { get; }
+        public string Message { get; }
+
+        internal Message ToMessage()
+        {
+            return new MessageBuilder()
+                .Code(MessageCode.ServerPrivateMessage)
+                .WriteString(Username)
+                .WriteString(Message)
+                .Build();
+        }
+    }
+}
