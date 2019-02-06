a75640ebb0f17432c4cbaff474758a0ff295bc39
JP Dillingham <jpdillingham@gmail.com>
2019-02-06 08:53:06 -0600

    start tests for SearchAsync

---
+++ tests/Soulseek.NET.Tests.Unit/Client/SearchAsyncTests.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="SearchAsyncTests.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET.Tests.Unit.Client
+{
+    using System;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class SearchAsyncTests
+    {
+        [Trait("Category", "SearchAsync")]
+        [Fact(DisplayName = "SearchAsync throws InvalidOperationException when not connected")]
+        public async Task SearchAsync_Throws_InvalidOperationException_When_Not_Connected()
+        {
+            var s = new SoulseekClient();
+
+            var ex = await Record.ExceptionAsync(async () => await s.SearchAsync("foo", 0));
+
+            Assert.NotNull(ex);
+            Assert.IsType<InvalidOperationException>(ex);
+            Assert.Contains("Connected", ex.Message, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Trait("Category", "SearchAsync")]
+        [Fact(DisplayName = "SearchAsync throws InvalidOperationException when not logged in")]
+        public async Task SearchAsync_Throws_InvalidOperationException_When_Not_Logged_In()
+        {
+            var s = new SoulseekClient();
+            s.SetProperty("State", SoulseekClientStates.Connected);
+
+            var ex = await Record.ExceptionAsync(async () => await s.SearchAsync("foo", 0));
+
+            Assert.NotNull(ex);
+            Assert.IsType<InvalidOperationException>(ex);
+            Assert.Contains("logged in", ex.Message, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
