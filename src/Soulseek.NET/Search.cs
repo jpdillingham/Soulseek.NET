e6c0cfba4e9dd9f31ba05d3e740fb79c66622935
JP Dillingham <jpdillingham@gmail.com>
2019-02-11 09:05:42 -0600

    remove delegates from Search

--- src/Soulseek.NET/Search.cs
+++ src/Soulseek.NET/Search.cs
@@ -33,25 +33,9 @@ namespace Soulseek.NET
         /// <param name="token">The unique search token.</param>
         /// <param name="options">The options for the search.</param>
         public Search(string searchText, int token, SearchOptions options = null)
-            : this(searchText, token, (search, response) => { }, (search, state) => { }, options)
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="Search"/> class.
-        /// </summary>
-        /// <param name="searchText">The text for which to search.</param>
-        /// <param name="token">The unique search token.</param>
-        /// <param name="responseHandler">The action invoked upon receipt of a search response.</param>
-        /// <param name="completeHandler">The action invoked upon completion of the search.</param>
-        /// <param name="options">The options for the search.</param>
-        public Search(string searchText, int token, Action<Search, SearchResponse> responseHandler, Action<Search, SearchStates> completeHandler, SearchOptions options = null)
         {
             SearchText = searchText;
             Token = token;
-
-            ResponseHandler = responseHandler ?? ((search, response) => { });
-            CompleteHandler = completeHandler ?? ((search, state) => { });
 
             Options = options ?? new SearchOptions();
 
@@ -94,9 +78,7 @@ namespace Soulseek.NET
         /// </summary>
         public int Token { get; }
 
-        private Action<Search, SearchStates> CompleteHandler { get; }
         private bool Disposed { get; set; } = false;
-        private Action<Search, SearchResponse> ResponseHandler { get; }
         private List<SearchResponse> ResponseList { get; set; } = new List<SearchResponse>();
         private SystemTimer SearchTimeoutTimer { get; set; }
 
@@ -128,7 +110,6 @@ namespace Soulseek.NET
 
                 ResponseList.Add(fullResponse);
 
-                ResponseHandler(this, fullResponse);
                 ResponseRecieved?.Invoke(this, fullResponse);
                 SearchTimeoutTimer.Reset();
 
@@ -147,7 +128,6 @@ namespace Soulseek.NET
         {
             SearchTimeoutTimer.Stop();
             State = SearchStates.Completed | state;
-            CompleteHandler(this, State);
             Completed?.Invoke(this, State);
         }
 
