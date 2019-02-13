02c0270da4d8b788ab01752adde3b45d6e5428bb
JP <jpdillingham@gmail.com>
2019-02-12 20:04:11 -0800

    add ConnectionFactory test

---
+++ tests/Soulseek.NET.Tests.Unit/Tcp/ConnectionFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Soulseek.NET.Tests.Unit.Tcp
+{
+    using System.Net;
+    using AutoFixture.Xunit2;
+    using Soulseek.NET.Tcp;
+    using Xunit;
+
+    public class ConnectionFactoryTests
+    {
+        [Trait("Category", "GetConnection")]
+        [Theory(DisplayName = "GetConnection returns expected IConnection"), AutoData]
+        public void GetConnection_Returns_Expected_IConnection(IPAddress ip, int port, ConnectionOptions options)
+        {
+            var f = new ConnectionFactory();
+
+            var c = f.GetConnection(ip, port, options);
+
+            Assert.Equal(ip, c.IPAddress);
+            Assert.Equal(port, c.Port);
+            Assert.Equal(options, c.Options);
+        }
+    }
+}
