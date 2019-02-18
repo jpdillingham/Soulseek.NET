a6527ec48fa75b1258083b3911b0cbea177d94ff
JP Dillingham <jpdillingham@gmail.com>
2019-02-17 18:59:13 -0600

    add test for ServerGetPeerAddress response handling

--- tests/Soulseek.NET.Tests.Unit/Client/ServerConnection_MessageReadTests.cs
+++ tests/Soulseek.NET.Tests.Unit/Client/ServerConnection_MessageReadTests.cs
@@ -12,9 +12,12 @@
 
 namespace Soulseek.NET.Tests.Unit.Client
 {
+    using AutoFixture.Xunit2;
     using Moq;
     using Soulseek.NET.Messaging;
+    using Soulseek.NET.Messaging.Messages;
     using System;
+    using System.Net;
     using Xunit;
 
     public class ServerConnection_MessageReadTests
@@ -57,6 +60,35 @@ namespace Soulseek.NET.Tests.Unit.Client
             diagnostic.Verify(m => m.Debug(It.IsAny<string>()), Times.Exactly(2));
 
             Assert.Contains("Unhandled", msg, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Trait("Category", "Message")]
+        [Theory(DisplayName = "Handles ServerGetPeerAddress"), AutoData]
+        public void Handles_ServerGetPeerAddress(string username, IPAddress ip, int port)
+        {
+            GetPeerAddressResponse result = null;
+
+            var waiter = new Mock<IWaiter>();
+            waiter.Setup(m => m.Complete(It.IsAny<WaitKey>(), It.IsAny<GetPeerAddressResponse>()))
+                .Callback<WaitKey, GetPeerAddressResponse>((key, response) => result = response);
+
+            var ipBytes = ip.GetAddressBytes();
+            Array.Reverse(ipBytes);
+
+            var message = new MessageBuilder()
+                .Code(MessageCode.ServerGetPeerAddress)
+                .WriteString(username)
+                .WriteBytes(ipBytes)
+                .WriteInteger(port)
+                .Build();
+
+            var s = new SoulseekClient("127.0.0.1", 1, messageWaiter: waiter.Object);
+
+            s.InvokeMethod("ServerConnection_MessageRead", null, message);
+
+            Assert.Equal(username, result.Username);
+            Assert.Equal(ip, result.IPAddress);
+            Assert.Equal(port, result.Port);
         }
     }
 }
