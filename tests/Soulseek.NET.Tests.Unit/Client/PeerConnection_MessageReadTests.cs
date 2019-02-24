79d107ca7b7013e206ccd55855636c3adca826f8
JP Dillingham <jpdillingham@gmail.com>
2019-02-23 21:51:35 -0600

    add peer connection handler tests

---
+++ tests/Soulseek.NET.Tests.Unit/Client/PeerConnection_MessageReadTests.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="PeerConnection_MessageReadTests.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET.Tests.Unit.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using AutoFixture.Xunit2;
+    using Moq;
+    using Soulseek.NET.Messaging;
+    using Soulseek.NET.Messaging.Tcp;
+    using Xunit;
+
+    public class PeerConnection_MessageReadTests
+    {
+        [Trait("Category", "Diagnostic")]
+        [Theory(DisplayName = "Creates diagnostic on message"), AutoData]
+        public void Creates_Diagnostic_On_Message(string username, IPAddress ip, int port)
+        {
+            var conn = new Mock<IMessageConnection>();
+            conn.Setup(m => m.Username)
+                .Returns(username);
+            conn.Setup(m => m.IPAddress)
+                .Returns(ip);
+            conn.Setup(m => m.Port)
+                .Returns(port);
+
+            List<string> messages = new List<string>();
+
+            var diagnostic = new Mock<IDiagnosticFactory>();
+            diagnostic.Setup(m => m.Debug(It.IsAny<string>()))
+                .Callback<string>(msg => messages.Add(msg));
+
+            var message = new MessageBuilder()
+                .Code(MessageCode.ServerParentMinSpeed)
+                .WriteInteger(1)
+                .Build();
+
+            var s = new SoulseekClient("127.0.0.1", 1, diagnosticFactory: diagnostic.Object);
+
+            s.InvokeMethod("PeerConnection_MessageRead", conn.Object, message);
+
+            Assert.Contains(messages, m => m.IndexOf("peer message received", StringComparison.InvariantCultureIgnoreCase) > -1);
+        }
+    }
+}
