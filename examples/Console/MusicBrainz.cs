1d23c2ad4de000db09577e7f273bdbfce99dfb0c
JP Dillingham <jpdillingham@gmail.com>
2019-04-04 08:31:43 -0500

    add MusicBrainz lib

---
+++ examples/Console/MusicBrainz.cs
@@ -0,0 +1,15 @@
+﻿namespace Console
+{
+    using System;
+
+    public class MusicBrainz
+    {
+        private static readonly Uri API_ROOT = new Uri("https://musicbrainz.org/ws/2");
+
+        private static Uri GetArtistSearchRequestUri(string query) => new Uri($"{API_ROOT}/artist/?query={Uri.EscapeDataString(query)}&fmt=json");
+
+        private static Uri GetReleaseGroupRequestUri(Guid artistMbid, int offset, int limit) => new Uri($"{API_ROOT}/release-group?artist={artistMbid}&type=album|ep&offset={offset}&limit={limit}&fmt=json");
+
+        private static Uri GetReleaseRequestUri(Guid releaseGroupMbid, int offset, int limit) => new Uri($"{API_ROOT}/release?release-group={releaseGroupMbid}&offset={offset}&limit={limit}&inc=media+recordings&fmt=json");
+    }
+}
