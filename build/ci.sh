03c950d5e59a93a27df4b6af33c0551c4d2f116e
JP Dillingham <jpdillingham@gmail.com>
2019-04-05 12:19:30 -0500

    tweak ci script and travis config

--- build/ci.sh
+++ build/ci.sh
@@ -2,9 +2,9 @@
 set -e
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-dotnet sonarscanner begin /key:"jpdillingham_Soulseek.NET" /o:jpdillingham-github /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${SONARCLOUD_TOKEN}" /d:sonar.cs.opencover.reportsPaths="tests/opencover.xml"
+dotnet sonarscanner begin /key:"jpdillingham_Soulseek.NET" /o:jpdillingham-github /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${SONARCLOUD_TOKEN}" /d:sonar.cs.opencover.reportsPaths="tests/opencover.xml" || true
 
 source ${__dir}/build.sh
 source ${__dir}/test.sh
 
-dotnet sonarscanner end /d:sonar.login="${SONARCLOUD_TOKEN}"
+dotnet sonarscanner end /d:sonar.login="${SONARCLOUD_TOKEN}" || true
