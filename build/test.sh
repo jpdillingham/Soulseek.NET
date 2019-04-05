871b29dbe2e2dc5264abd4e8952eccf6aa12e367
JP Dillingham <jpdillingham@gmail.com>
2019-04-04 22:43:32 -0500

    set test configuration to Release

--- build/test.sh
+++ build/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-dotnet test --no-build -p:CollectCoverage=true -p:CoverletOutput="../coverage.json" tests/Soulseek.NET.Tests.Unit -p:Include="[Soulseek.NET*]*" -p:Exclude="[*.Tests.*]*"
-dotnet test --no-build -p:CollectCoverage=true -p:MergeWith="../coverage.json" -p:CoverletOutput="../opencover.xml" -p:CoverletOutputFormat=opencover tests/Soulseek.NET.Tests.Integration -p:Include="[Soulseek.NET*]*" -p:Exclude="[*.Tests.*]*"
+dotnet test --no-build --configuration Release -p:CollectCoverage=true -p:CoverletOutput="../coverage.json" tests/Soulseek.NET.Tests.Unit -p:Include="[Soulseek.NET*]*" -p:Exclude="[*.Tests.*]*"
+dotnet test --no-build --configuration Release -p:CollectCoverage=true -p:MergeWith="../coverage.json" -p:CoverletOutput="../opencover.xml" -p:CoverletOutputFormat=opencover tests/Soulseek.NET.Tests.Integration -p:Include="[Soulseek.NET*]*" -p:Exclude="[*.Tests.*]*"
