dd2579c5aa6bfb1e7073a64129cce702e058c681
JP Dillingham <jpdillingham@gmail.com>
2019-04-04 22:08:43 -0500

    add test script

---
+++ build/test.sh
@@ -0,0 +1,2 @@
+dotnet test --no-build -p:CollectCoverage=true -p:CoverletOutput="../coverage.json" tests/Soulseek.NET.Tests.Unit -p:Include="[Soulseek.NET*]*" -p:Exclude="[*.Tests.*]*"
+dotnet test --no-build -p:CollectCoverage=true -p:MergeWith="../coverage.json" -p:CoverletOutput="../opencover.xml" -p:CoverletOutputFormat=opencover tests/Soulseek.NET.Tests.Integration -p:Include="[Soulseek.NET*]*" -p:Exclude="[*.Tests.*]*"
