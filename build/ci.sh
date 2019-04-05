d2c2a9d9ee84cc72c505aaaaa488bbf7cb3abdbd
JP Dillingham <jpdillingham@gmail.com>
2019-04-05 09:31:55 -0500

    add ci.sh

---
+++ build/ci.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+dotnet sonarscanner begin /key:"jpdillingham_Soulseek.NET" /o:jpdillingham-github /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${SONARCLOUD_TOKEN}" /d:sonar.cs.opencover.reportsPaths="tests/opencover.xml"
+source ${__dir}/build.sh
+source ${__dir}/test.sh
+dotnet sonarscanner end /d:sonar.login="${SONARCLOUD_TOKEN}"
