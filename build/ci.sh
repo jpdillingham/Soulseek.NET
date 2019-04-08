f270e60862a7a7d264a80c3a92ee2b4b2c99a376
JP Dillingham <jpdillingham@gmail.com>
2019-04-07 21:46:40 -0500

    tweak ci script

--- build/ci.sh
+++ build/ci.sh
@@ -3,6 +3,10 @@ set -e
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __branch=$(git branch --no-color | grep -E '^\*' | awk '{print $2}')
 
+if [ "${CIRCLECI}" = "true" ]; then
+    __branch="${CIRCLE_BRANCH}"
+fi
+
 dotnet-sonarscanner begin /key:"jpdillingham_Soulseek.NET" /o:jpdillingham-github /d:sonar.host.url="https://sonarcloud.io" /d:sonar.exclusions="**/*examples*/**" /d:sonar.branch.name=${__branch} /d:sonar.login="${SONARCLOUD_TOKEN}" /d:sonar.cs.opencover.reportsPaths="tests/opencover.xml"
 
 . "${__dir}/build.sh"
