b87054c6e8163495c4d49000786fe1698e7b14ca
JP Dillingham <jpdillingham@gmail.com>
2019-04-09 21:00:58 -0500

    add Levenshtein extension methods

--- examples/Console/Utility.cs
+++ examples/Console/Utility.cs
@@ -4,8 +4,61 @@ using System.Text;
 
 namespace Console
 {
-    class Utility
+    public static class Utility
     {
+        public static double Similarity(this string s, string t)
+        {
+            return (1.0 - ((double)s.LevenshteinDistance(t) / (double)Math.Max(s.Length, t.Length)));
+        }
+
+        public static double SimilarityCaseInsensitive(this string s, string t)
+        {
+            return (1.0 - ((double)s.LevenshteinDistanceCaseInsensitive(t) / (double)Math.Max(s.Length, t.Length)));
+        }
+
+        public static int LevenshteinDistanceCaseInsensitive(this string s, string t)
+        {
+            return s.ToLower().LevenshteinDistance(t.ToLower());
+        }
+
+        public static int LevenshteinDistance(this string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return d[n, m];
+        }
     }
 
     public class SemiNumericComparer : IComparer<string>
