9d1dd8e2a624b2cafff9a4001edf7ccf73001172
JP Dillingham <jpdillingham@gmail.com>
2019-04-09 20:39:12 -0500

    add Utility.cs

---
+++ examples/Console/Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console
+{
+    class Utility
+    {
+    }
+
+    public class SemiNumericComparer : IComparer<string>
+    {
+        public int Compare(string s1, string s2)
+        {
+            if (IsNumeric(s1) && IsNumeric(s2))
+            {
+                if (Convert.ToInt32(s1) > Convert.ToInt32(s2)) return 1;
+                if (Convert.ToInt32(s1) < Convert.ToInt32(s2)) return -1;
+                if (Convert.ToInt32(s1) == Convert.ToInt32(s2)) return 0;
+            }
+
+            if (IsNumeric(s1) && !IsNumeric(s2))
+                return -1;
+
+            if (!IsNumeric(s1) && IsNumeric(s2))
+                return 1;
+
+            return string.Compare(s1, s2, true);
+        }
+
+        public static bool IsNumeric(object value)
+        {
+            try
+            {
+                int i = Convert.ToInt32(value.ToString());
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
