826a852cc429a29a9f77643f0b893f98736c334c
JP Dillingham <jpdillingham@gmail.com>
2019-04-19 17:59:49 -0500

    add release script

---
+++ examples/Console/build/release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+dir="bin/Release/netcoreapp2.1/packed"
+mkdir -p "$dir"
+
+dotnet publish -c Release -r win-x64 --self-contained true
+warp-packer --arch windows-x64 --input_dir bin/Release/netcoreapp2.1/win-x64/publish --exec slsk-ex.exe --output "$dir"/slsk-ex.win-x64.exe
+
+dotnet publish -c Release -r linux-x64 --self-contained true
+warp-packer --arch linux-x64 --input_dir bin/Release/netcoreapp2.1/linux-x64/publish --exec slsk-ex --output "$dir"/slsk-ex
