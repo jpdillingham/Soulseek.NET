3561b1b1941aace0ad693f44e21830cfc69f848a
JP Dillingham <jpdillingham@gmail.com>
2019-04-25 21:38:07 -0500

    add tests to cover additional Download props

--- tests/Soulseek.NET.Tests.Unit/DownloadTests.cs
+++ tests/Soulseek.NET.Tests.Unit/DownloadTests.cs
@@ -12,6 +12,7 @@
 
 namespace Soulseek.NET.Tests.Unit
 {
+    using System;
     using System.Net;
     using AutoFixture.Xunit2;
     using Moq;
@@ -44,6 +45,15 @@ namespace Soulseek.NET.Tests.Unit
             Assert.Equal(0, d.RemoteToken);
             Assert.Equal(0, d.Size);
             Assert.Equal(DownloadStates.None, d.State);
+            Assert.Equal(0, d.AverageSpeed);
+            Assert.Equal(0, d.BytesDownloaded);
+            Assert.Equal(0, d.BytesRemaining);
+            Assert.Equal(default(TimeSpan), d.ElapsedTime);
+            Assert.Equal(default(TimeSpan), d.RemainingTime);
+            Assert.Null(d.StartTime);
+            Assert.Null(d.EndTime);
+            Assert.Equal(0, d.PercentComplete);
+            Assert.Equal(0, d.RemoteToken);
         }
 
         [Trait("Category", "Properties")]
@@ -71,6 +81,153 @@ namespace Soulseek.NET.Tests.Unit
             var d = new Download(username, filename, token);
 
             Assert.Equal(new WaitKey(username, filename, token), d.WaitKey);
+        }
+
+        [Trait("Category", "State")]
+        [Fact(DisplayName = "State transitions set time props")]
+        internal void State_Transitions_Set_Time_Props()
+        {
+            var d = new Download(string.Empty, string.Empty, 0);
+
+            var s1 = d.StartTime;
+            var e1 = d.EndTime;
+            var et1 = d.ElapsedTime;
+            var rt1 = d.RemainingTime;
+
+            d.State = DownloadStates.InProgress;
+
+            var s2 = d.StartTime;
+            var e2 = d.EndTime;
+            var et2 = d.ElapsedTime;
+            var rt2 = d.RemainingTime;
+
+            d.State = DownloadStates.Completed;
+
+            Assert.Null(s1);
+            Assert.Null(e1);
+            Assert.Equal(default(TimeSpan), et1);
+            Assert.Equal(default(TimeSpan), rt1);
+
+            Assert.NotNull(s2);
+            Assert.Null(e2);
+            Assert.NotEqual(default(TimeSpan), et2);
+            Assert.Equal(default(TimeSpan), rt2);
+
+            Assert.NotNull(d.StartTime);
+            Assert.NotNull(d.EndTime);
+            Assert.NotEqual(default(TimeSpan), d.ElapsedTime);
+            Assert.Equal(default(TimeSpan), d.RemainingTime);
+        }
+
+        [Trait("Category", "State")]
+        [Fact(DisplayName = "ElapsedTime works")]
+        internal void ElapsedTime_Works()
+        {
+            var d = new Download(string.Empty, string.Empty, 0);
+
+            var s = new DateTime(2019, 4, 25);
+            var e = new DateTime(2019, 4, 26);
+
+            Assert.Equal(default(TimeSpan), d.ElapsedTime);
+
+            d.SetProperty("StartTime", s);
+
+            Assert.NotEqual(default(TimeSpan), d.ElapsedTime);
+
+            d.SetProperty("EndTime", e);
+
+            Assert.Equal(e - s, d.ElapsedTime);
+        }
+
+        [Trait("Category", "State")]
+        [Fact(DisplayName = "RemainingTime works")]
+        internal void RemainingTime_Works()
+        {
+            var d = new Download(string.Empty, string.Empty, 0);
+
+            Assert.Equal(default(TimeSpan), d.RemainingTime);
+
+            d.SetProperty("AverageSpeed", 1);
+            d.SetProperty("Size", 2);
+            d.SetProperty("BytesDownloaded", 1);
+
+            Assert.Equal(TimeSpan.FromSeconds(1), d.RemainingTime);
+        }
+
+        [Trait("Category", "State")]
+        [Fact(DisplayName = "PercentComplete works")]
+        internal void PercentComplete_Works()
+        {
+            var d = new Download(string.Empty, string.Empty, 0);
+
+            Assert.Equal(0, d.PercentComplete);
+
+            d.SetProperty("Size", 100);
+            d.SetProperty("BytesDownloaded", 50);
+
+            Assert.Equal(50, d.PercentComplete);
+        }
+
+        [Trait("Category", "UpdateProgress")]
+        [Fact(DisplayName = "UpdateProgress sets AverageSpeed initial value")]
+        internal void UpdateProgress_Sets_AverageSpeed_To_Initial_Value()
+        {
+            var d = new Download(string.Empty, string.Empty, 0);
+
+            Assert.Equal(0, d.AverageSpeed);
+
+            d.SetField("progressUpdateLimit", 0);
+            d.SetProperty("State", DownloadStates.InProgress);
+            d.InvokeMethod("UpdateProgress", 100000);
+
+            Assert.NotEqual(0, d.AverageSpeed);
+        }
+
+        [Trait("Category", "UpdateProgress")]
+        [Fact(DisplayName = "UpdateProgress updates AverageSpeed on subsequent call")]
+        internal void UpdateProgress_Updates_AverageSpeed_On_Subsequent_Call()
+        {
+            var d = new Download(string.Empty, string.Empty, 0);
+
+            Assert.Equal(0, d.AverageSpeed);
+
+            d.SetField("progressUpdateLimit", 0);
+            d.SetProperty("State", DownloadStates.InProgress);
+            d.InvokeMethod("UpdateProgress", 100000);
+
+            var v1 = d.AverageSpeed;
+            Assert.NotEqual(0, v1);
+
+            d.InvokeMethod("UpdateProgress", 10);
+
+            Assert.NotEqual(v1, d.AverageSpeed);
+        }
+
+        [Trait("Category", "UpdateProgress")]
+        [Fact(DisplayName = "UpdateProgress ignores AverageSpeed if window less than progressUpdateLimit")]
+        internal void UpdateProgress_Ignores_AverageSpeed_If_Window_Less_Than_progressUpdateLimit()
+        {
+            var d = new Download(string.Empty, string.Empty, 0);
+
+            Assert.Equal(0, d.AverageSpeed);
+
+            d.SetProperty("State", DownloadStates.InProgress);
+            d.InvokeMethod("UpdateProgress", 100000);
+
+            Assert.Equal(0, d.AverageSpeed);
+        }
+
+        [Trait("Category", "UpdateProgress")]
+        [Fact(DisplayName = "UpdateProgress ignores AverageSpeed if StartTime and lastProgressTime is null")]
+        internal void UpdateProgress_Ignores_AverageSpeed_If_StarTime_And_lastProgressTime_Is_Null()
+        {
+            var d = new Download(string.Empty, string.Empty, 0);
+
+            Assert.Equal(0, d.AverageSpeed);
+
+            d.InvokeMethod("UpdateProgress", 100000);
+
+            Assert.Equal(0, d.AverageSpeed);
         }
     }
 }
