c03d8f71f60eb99df734a891513ccc12c7f2903d
JP Dillingham <jpdillingham@gmail.com>
2019-04-26 20:59:17 -0500

    add I/ConnectionFactory

---
+++ src/Soulseek.NET/Common/ConnectionFactory.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="ConnectionFactory.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.NET
+{
+    using System.Net;
+    using Soulseek.NET.Messaging.Tcp;
+    using Soulseek.NET.Tcp;
+
+    /// <summary>
+    ///     Creates connections.
+    /// </summary>
+    internal class ConnectionFactory : IConnectionFactory
+    {
+        /// <summary>
+        ///     Gets a <see cref="IConnection"/> with the specified parameters.
+        /// </summary>
+        /// <param name="ipAddress">The remote IP address of the connection.</param>
+        /// <param name="port">The remote port of the connection.</param>
+        /// <param name="options">The optional options for the connection.</param>
+        /// <returns>The created connection.</returns>
+        public IConnection GetConnection(IPAddress ipAddress, int port, ConnectionOptions options) => new Connection(ipAddress, port, options);
+
+        /// <summary>
+        ///     Gets a <see cref="IMessageConnection"/> with the specified parameters.
+        /// </summary>
+        /// <param name="type">The connection type (Peer, Server)</param>
+        /// <param name="username">The username of the peer associated with the connection, if applicable.</param>
+        /// <param name="ipAddress">The remote IP address of the connection.</param>
+        /// <param name="port">The remote port of the connection.</param>
+        /// <param name="options">The optional options for the connection.</param>
+        /// <returns>The created connection.</returns>
+        public IMessageConnection GetMessageConnection(MessageConnectionType type, string username, IPAddress ipAddress, int port, ConnectionOptions options) => new MessageConnection(type, username, ipAddress, port, options);
+    }
+}
