1b5ad704baf687c66664fe46d03736b3b360570c
JP Dillingham <jpdillingham@gmail.com>
2019-05-01 21:03:33 -0500

    rough in swagger config

--- examples/WebAPI/Controllers/ValuesController.cs
+++ examples/WebAPI/Controllers/ValuesController.cs
@@ -1,45 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    /// <summary>
+    ///     Values
+    /// </summary>
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1")]
     [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        /// <summary>
+        ///     Gets a list of values
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        /// <summary>
+        ///     Gets a specific value
+        /// </summary>
+        /// <param name="index">The index to get</param>
+        /// <param name="foo">foo</param>
+        /// <returns></returns>
+        [HttpGet("{index}")]
+        public ActionResult<string> GetOne(int index = 1, string foo = null)
         {
-            return "value";
+            var arr = new string[] { "value1", "value2" };
+
+            return arr[index];
+        }
+    }
+
+    /// <summary>
+    ///     Values
+    /// </summary>
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("2")]
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class ValuesController2 : ControllerBase
+    {
+        /// <summary>
+        ///     Gets a list of values
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value3", "value4" };
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        /// <summary>
+        ///     Gets a specific value
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{index}")]
+        public ActionResult<string> GetOne(int index)
         {
-        }
+            var arr = new string[] { "value3", "value4" };
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return arr[index];
         }
     }
 }
--- examples/WebAPI/Startup.cs
+++ examples/WebAPI/Startup.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace WebAPI
+﻿namespace WebAPI
 {
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ApiExplorer;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Swashbuckle.AspNetCore.Swagger;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -22,14 +23,46 @@ namespace WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(options => options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials()));
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Latest)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
+
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                var provider = services.BuildServiceProvider()
+                       .GetRequiredService<IApiVersionDescriptionProvider>();
+
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    c.SwaggerDoc(description.GroupName, new Info { Title = "Soulseek.NET Example API", Version = description.GroupName });
+                }
+
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                var basePath = AppContext.BaseDirectory;
+                c.IncludeXmlComments(Path.Combine(basePath, fileName));
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -40,8 +73,34 @@ namespace WebAPI
                 app.UseHsts();
             }
 
+            app.UseCors("AllowAll");
+
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger(c => 
+            {
+                string camelCase(string key) =>
+                    string.Join('/', key.Split('/').Select(x => x.Contains("{") || x.Length < 2 ? x : char.ToLowerInvariant(x[0]) + x.Substring(1)));
+
+                c.PreSerializeFilters.Add((document, request) =>
+                {
+                    document.Paths = document.Paths.ToDictionary(p => camelCase(p.Key), p => p.Value);
+
+                    document.Paths.ToList()
+                        .ForEach(path => typeof(PathItem).GetProperties().Where(p => p.PropertyType == typeof(Operation)).ToList()
+                            .ForEach(operation => ((Operation)operation.GetValue(path.Value, null))?.Parameters.ToList()
+                                .ForEach(prop => prop.Name = camelCase(prop.Name))));
+                });
+            });
+
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName);
+                }
+            });
         }
     }
 }
