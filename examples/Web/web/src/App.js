7fe32eec29fae39265349f262a3d310efe8af9b2
JP Dillingham <jpdillingham@gmail.com>
2019-05-09 23:05:02 -0500

    add axios, implement rough search and download

--- examples/Web/web/src/App.js
+++ examples/Web/web/src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Input, Button } from 'semantic-ui-react';
+import data from './data'
 
-function App() {
-    return (
-        <div className="App"></div>
-    );
+const BASE_URL = "http://localhost:60084/api/v1";
+
+class App extends Component {
+    state = { searchPhrase: '', results: [] }
+
+    search = () => {
+        axios.get(BASE_URL + '/search/' + this.state.searchPhrase)
+        .then(response => this.setState({ results: response.data }))
+        //this.setState({ results: data })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Input 
+                    placeholder="Enter search phrase..."
+                    onChange={(event, data) => this.setState({ searchPhrase: data.value })}
+                />
+                <Button
+                    onClick={this.search}
+                >
+                    Search
+                </Button>
+                <div>
+                    {this.state.results.filter(r => r.freeUploadSlots > 0).map(r =>
+                        <ul>
+                            <li>{r.username}; {r.freeUploadSlots}, {r.queueLength}</li>
+                            <ul>
+                                {r.files.map(f => 
+                                    <li><a href={BASE_URL + '/download/' + r.username + '/' + encodeURIComponent(f.filename)}>{f.filename}</a></li>
+                                )}
+                            </ul>
+                        </ul>
+                    )}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
