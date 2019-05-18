1a499fe463b4f0fb93bd1c5a52fad582aed1a86b
JP Dillingham <jpdillingham@gmail.com>
2019-05-18 07:43:39 -0500

    refactor search input, switch to post

--- examples/Web/web/src/Search.js
+++ examples/Web/web/src/Search.js
@@ -14,9 +14,13 @@ import {
 class Search extends Component {
     state = { searchPhrase: '', searchState: 'complete', results: data }
 
-    search = (searchPhrase) => {
+    search = () => {
+        let searchPhrase = this.inputtext.inputRef.current.value;
+
         this.setState({ searchState: 'pending' }, () => {
-            axios.get(BASE_URL + '/search/' + searchPhrase)
+            axios.post(BASE_URL + '/search', JSON.stringify(searchPhrase), { 
+                headers: {'Content-Type': 'application/json; charset=utf-8'} 
+            })
             .then(response => this.setState({ results: response.data }))
             .then(() => this.setState({ searchState: 'complete' }))
         });
@@ -27,19 +31,19 @@ class Search extends Component {
     }
     
     render = () => {
-        let { searchPhrase, searchState, results } = this.state;
+        let { searchState, results } = this.state;
         let pending = searchState === 'pending';
 
         return (
             <div>
                 <Segment className='search-segment'>
                     <Input 
+                        ref={input => this.inputtext = input}
                         loading={pending}
                         disabled={pending}
                         className='search-input'
                         placeholder="Enter search phrase..."
-                        onChange={this.onSearchPhraseChange}
-                        action={!pending && { content: 'Search', onClick: () => this.search(searchPhrase) }}
+                        action={!pending && { content: 'Search', onClick: this.search}}
                     />
                 </Segment>
                 {searchState === 'complete' && <div>
