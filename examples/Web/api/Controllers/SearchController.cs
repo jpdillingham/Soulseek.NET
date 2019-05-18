684c386274aed748ad17b7a4bd84e3c1073b4519
JP Dillingham <jpdillingham@gmail.com>
2019-05-18 07:43:28 -0500

    change search to post, add get for list

--- examples/Web/api/Controllers/SearchController.cs
+++ examples/Web/api/Controllers/SearchController.cs
@@ -31,11 +31,17 @@
         /// <param name="searchText">The search phrase.</param>
         /// <param name="token">The optional search token.</param>
         /// <returns></returns>
-        [HttpGet("{searchText}")]
-        public async Task<ActionResult<IEnumerable<SearchResponse>>> Get([FromRoute, Required]string searchText, [FromQuery]int? token = null)
+        [HttpPost("")]
+        public async Task<ActionResult<IEnumerable<SearchResponse>>> Post([FromBody]string searchText, [FromQuery]int? token = null)
         {
             var results = await Client.SearchAsync(searchText, token);
             return results.ToList();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Search>> Get()
+        {
+            return Ok(Client.Searches);
         }
     }
 }
