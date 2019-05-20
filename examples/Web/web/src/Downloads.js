f11dcd92dd85e7a744996d6c533f4635fea3bcfa
JP Dillingham <jpdillingham@gmail.com>
2019-05-19 21:11:31 -0500

    map downloads

--- examples/Web/web/src/Downloads.js
+++ examples/Web/web/src/Downloads.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import { BASE_URL } from './constants';
+import { formatBytes, getDirectoryName, getFileName } from './util';
+
+const buildTree = (files) => {
+    return files.reduce((dict, file) => {
+        let dir = getDirectoryName(file.filename);
+        let selectable = { selected: false, ...file };
+        dict[dir] = dict[dir] === undefined ? [ selectable ] : dict[dir].concat(selectable);
+        return dict;
+    }, {});
+}
 
 class Downloads extends Component {
-    state = { }
+    state = { fetchState: '', downloads: [] }
+
+    componentDidMount = () => {
+        this.fetch();
+    }
+
+    fetch = () => {
+        this.setState({ fetchState: 'pending' }, () => {
+            axios.get(BASE_URL + '/files')
+            .then(response => this.setState({ 
+                fetchState: 'complete', downloads: response.data.map(u => [{ username: u.username, files: buildTree(u.files) }])
+            }))
+            .catch(err => this.setState({ fetchState: 'failed' }))
+        })
+    }
     
-    render = () => (
-        <div>
-            Hello, World!
+    render = () => {
+        let { fetchState, downloads } = this.state;
 
+        console.log(JSON.stringify(downloads));
 
-            Hello, World!
-            Hello, World!
-            v
-            Hello, World!
-            Hello, World!
-            Hello, World!
-        </div>
-    )
+        return (
+            fetchState === 'complete' && downloads && <div>
+                {Object.keys(downloads).map((user, index) => 
+                    Object.keys(downloads[user]).map((file, index) => 
+                        JSON.stringify(downloads[user][file])
+                    )
+                )}
+            </div>
+        );
+    }
 }
 
 export default Downloads;
