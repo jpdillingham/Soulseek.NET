0acb11e03366c255c0cdc2e738d5fa5bd34e0a67
JP Dillingham <jpdillingham@gmail.com>
2019-05-26 14:12:38 -0500

    tweak build script

--- examples/Web/build/build.sh
+++ examples/Web/build/build.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 set -e
 
+# build web
 cd web
 pwd
 npm run build
 
+# remove old build, but keep .gitkeep
 rm -rf ../api/wwwroot
+touch ../api/wwwroot/.gitkeep
+
+# copy new files
 mkdir ../api/wwwroot
 cp -r build/* ../api/wwwroot/
 
+# publish api + web
 cd ../api
 pwd
 dotnet publish --configuration Release
+
+# build docker
+cd ..
+pwd
+docker build -t slsk-web .
