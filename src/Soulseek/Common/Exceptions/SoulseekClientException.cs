f4911094c71603e31aa7fb71ba723bf2966e514f
JP Dillingham <jpdillingham@gmail.com>
2019-05-29 21:14:38 -0500

    add PeerOfflineException

--- src/Soulseek/Common/Exceptions/SoulseekClientException.cs
+++ src/Soulseek/Common/Exceptions/SoulseekClientException.cs
@@ -17,7 +17,7 @@ namespace Soulseek.Exceptions
     using System.Runtime.Serialization;
 
     /// <summary>
-    ///     Represents errors that occur during execution of <see cref="SoulseekClient"/>.
+    ///     Represents errors that occur during execution of <see cref="SoulseekClient"/> operations.
     /// </summary>
     [ExcludeFromCodeCoverage]
     [Serializable]
@@ -60,6 +60,55 @@ namespace Soulseek.Exceptions
         /// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
         /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
         protected SoulseekClientException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+
+    /// <summary>
+    ///     Represents an error connecting to a peer due to the peer being offline.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class PeerOfflineException : SoulseekClientException
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PeerOfflineException"/> class.
+        /// </summary>
+        public PeerOfflineException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PeerOfflineException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public PeerOfflineException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PeerOfflineException"/> class with a specified error message and a
+        ///     reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner
+        ///     exception is specified.
+        /// </param>
+        public PeerOfflineException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PeerOfflineException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
+        protected PeerOfflineException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
         }
