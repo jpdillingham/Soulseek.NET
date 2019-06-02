e35ae7fe105230ce138b3a84c4e6cbf39841f7dc
JP Dillingham <jpdillingham@gmail.com>
2019-06-02 17:26:57 -0500

    reduce test verbosity, force dotnet 2.2.204

--- build/test.sh
+++ build/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-dotnet test --verbosity normal --no-build --configuration Release --blame -p:CollectCoverage=true -p:CoverletOutput="../coverage.json" tests/Soulseek.Tests.Unit -p:Include="[Soulseek]*"
-dotnet test --verbosity normal --no-build --configuration Release --blame -p:CollectCoverage=true -p:MergeWith="../coverage.json" -p:CoverletOutput="../opencover.xml" -p:CoverletOutputFormat=opencover tests/Soulseek.Tests.Integration -p:Include="[Soulseek]*"
+dotnet test --verbosity minimal --no-build --configuration Release --blame -p:CollectCoverage=true -p:CoverletOutput="../coverage.json" tests/Soulseek.Tests.Unit -p:Include="[Soulseek]*"
+dotnet test --verbosity minimal --no-build --configuration Release --blame -p:CollectCoverage=true -p:MergeWith="../coverage.json" -p:CoverletOutput="../opencover.xml" -p:CoverletOutputFormat=opencover tests/Soulseek.Tests.Integration -p:Include="[Soulseek]*"
