ae0c0feca37dceb335f9464c23714555182c9b00
JP Dillingham <jpdillingham@gmail.com>
2019-06-13 15:16:25 -0500

    change TransferType to ConnectionType, add SolicitedConnection

--- src/Soulseek/Common/Constants.cs
+++ src/Soulseek/Common/Constants.cs
@@ -1,4 +1,16 @@
-﻿namespace Soulseek
+﻿// <copyright file="Constants.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek
 {
     internal static class Constants
     {
@@ -6,9 +18,10 @@
         {
             public const string Download = "Download";
             public const string DirectTransfer = "DirectTransfer";
+            public const string SolicitedConnection = "SolicitedConnection";
         }
 
-        internal static class TransferType
+        internal static class ConnectionType
         {
             public const string Peer = "P";
             public const string Tranfer = "F";
