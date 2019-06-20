e272d44864637114ba7d509101058cf2359a6e93
JP Dillingham <jpdillingham@gmail.com>
2019-06-20 08:49:04 -0500

    update download wait key test

--- tests/Soulseek.Tests.Unit/DownloadTests.cs
+++ tests/Soulseek.Tests.Unit/DownloadTests.cs
@@ -81,7 +81,7 @@ namespace Soulseek.Tests.Unit
         {
             var d = new Download(username, filename, token);
 
-            Assert.Equal(new WaitKey(Constants.WaitKey.Download, username), d.WaitKey);
+            Assert.Equal(new WaitKey(Constants.WaitKey.Download, username, filename, token), d.WaitKey);
         }
 
         [Trait("Category", "State")]
