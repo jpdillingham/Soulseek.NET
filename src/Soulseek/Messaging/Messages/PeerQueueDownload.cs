070ca4bf058008b3023b738c84f0d4c0f9d8aead
JP Dillingham <jpdillingham@gmail.com>
2019-06-25 19:52:56 -0500

    add PeerQueueDownload

---
+++ src/Soulseek/Messaging/Messages/PeerQueueDownload.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="PeerQueueDownload.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.Messaging.Messages
+{
+    using Soulseek.Exceptions;
+
+    internal sealed class PeerQueueDownload
+    {
+        internal PeerQueueDownload(string filename)
+        {
+            Filename = filename;
+        }
+
+        public string Filename { get; }
+
+        public static PeerQueueDownload Parse(Message message)
+        {
+            var reader = new MessageReader(message);
+
+            if (reader.Code != MessageCode.PeerQueueDownload)
+            {
+                throw new MessageException($"Message Code mismatch creating Peer Queue Download (expected: {(int)MessageCode.PeerQueueDownload}, received: {(int)reader.Code}.");
+            }
+
+            var filename = reader.ReadString();
+            return new PeerQueueDownload(filename);
+        }
+    }
+}
