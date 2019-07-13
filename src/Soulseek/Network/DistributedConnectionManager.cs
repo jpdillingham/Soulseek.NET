d6c094235243ceebf606066c63bec4cbe1a2254f
JP Dillingham <jpdillingham@gmail.com>
2019-07-13 18:44:01 -0500

    move *ConnectionManager classes to the Network namespace

--- src/Soulseek/Network/DistributedConnectionManager.cs
+++ src/Soulseek/Network/DistributedConnectionManager.cs
@@ -10,7 +10,7 @@
 //     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
 // </copyright>
 
-namespace Soulseek
+namespace Soulseek.Network
 {
     using System;
     using System.Collections.Concurrent;
