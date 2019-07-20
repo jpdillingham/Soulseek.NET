bc35df69e6f410b4c303ed1bdd6a8f2fa0748a95
JP Dillingham <jpdillingham@gmail.com>
2019-07-20 17:26:39 -0500

    rename PeerConnectionManager tests

--- tests/Soulseek.Tests.Unit/Network/PeerConnectionManagerTests.cs
+++ tests/Soulseek.Tests.Unit/Network/PeerConnectionManagerTests.cs
@@ -1,40 +1,39 @@
-﻿//// <copyright file="ConnectionManagerTests.cs" company="JP Dillingham">
-////     Copyright (c) JP Dillingham. All rights reserved.
-////
-////     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
-////     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-////
-////     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-////     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
-////
-////     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
-//// </copyright>
+﻿// <copyright file="ConnectionManagerTests.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
 
-//namespace Soulseek.Tests.Unit.Network
-//{
-//    using System;
-//    using System.Collections.Concurrent;
-//    using System.Linq;
-//    using System.Net;
-//    using System.Threading;
-//    using System.Threading.Tasks;
-//    using AutoFixture.Xunit2;
-//    using Moq;
-//    using Soulseek.Messaging;
-//    using Soulseek.Messaging.Messages;
-//    using Soulseek.Messaging.Tcp;
-//    using Soulseek.Network;
-//    using Xunit;
+namespace Soulseek.Tests.Unit.Network
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Linq;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AutoFixture.Xunit2;
+    using Moq;
+    using Soulseek.Messaging;
+    using Soulseek.Messaging.Messages;
+    using Soulseek.Network;
+    using Xunit;
 
-//    public class ConnectionManagerTests
-//    {
+    public class PeerConnectionManagerTests
+    {
 //        [Trait("Category", "Instantiation")]
 //        [Fact(DisplayName = "Instantiates properly")]
 //        public void Instantiates_Properly()
 //        {
-//            ConnectionManager c = null;
+//            PeerConnectionManager c = null;
 
-//            var ex = Record.Exception(() => c = new ConnectionManager(1000));
+//            var ex = Record.Exception(() => c = new PeerConnectionManager(1000));
 
 //            Assert.Null(ex);
 //            Assert.NotNull(c);
@@ -354,5 +353,5 @@
 //            Assert.Equal(0, c.ActivePeerConnections);
 //            Assert.Equal(0, c.WaitingPeerConnections);
 //        }
-//    }
-//}
+    }
+}
