76f4cd5e380c30e5047075e414a55e531bc5dee2
JP Dillingham <jpdillingham@gmail.com>
2019-08-15 09:02:36 -0500

    stub ListenerHandler tests

---
+++ tests/Soulseek.Tests.Unit/Network/ListenerHandlerTests.cs
@@ -0,0 +1,79 @@
+﻿// <copyright file="ListenerHandlerTests.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.Tests.Unit.Network
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AutoFixture.Xunit2;
+    using Moq;
+    using Soulseek.Messaging;
+    using Soulseek.Messaging.Handlers;
+    using Soulseek.Messaging.Messages;
+    using Soulseek.Network;
+    using Soulseek.Network.Tcp;
+    using Xunit;
+    public class ListenerHandlerTests
+    {
+        [Trait("Category", "Diagnostic")]
+        [Theory(DisplayName = "Creates diagnostic on message"), AutoData]
+        public void Creates_Diagnostic_On_Message(IPAddress ip)
+        {
+            var (handler, mocks) = GetFixture(ip);
+
+            mocks.Diagnostic.Setup(m => m.Debug(It.IsAny<string>()));
+
+            handler.HandleConnection(null, mocks.Connection.Object);
+
+            mocks.Diagnostic.Verify(m => m.Info(It.Is<string>(s => s.Contains("Accepted incoming connection"))), Times.Once);
+        }
+        private (ListenerHandler Handler, Mocks Mocks) GetFixture(IPAddress ip, ClientOptions clientOptions = null)
+        {
+            var mocks = new Mocks(clientOptions);
+
+            mocks.Connection.Setup(m => m.IPAddress).Returns(ip);
+
+            var handler = new ListenerHandler(
+                mocks.Client.Object,
+                mocks.Diagnostic.Object);
+
+            return (handler, mocks);
+        }
+
+        private class Mocks
+        {
+            public Mocks(ClientOptions clientOptions = null)
+            {
+                Client = new Mock<SoulseekClient>(clientOptions)
+                {
+                    CallBase = true,
+                };
+
+                Client.Setup(m => m.PeerConnectionManager).Returns(PeerConnectionManager.Object);
+                Client.Setup(m => m.DistributedConnectionManager).Returns(DistributedConnectionManager.Object);
+                Client.Setup(m => m.State).Returns(SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
+                Client.Setup(m => m.Options).Returns(clientOptions ?? new ClientOptions());
+            }
+
+            public Mock<SoulseekClient> Client { get; }
+            public Mock<IPeerConnectionManager> PeerConnectionManager { get; } = new Mock<IPeerConnectionManager>();
+            public Mock<IDistributedConnectionManager> DistributedConnectionManager { get; } = new Mock<IDistributedConnectionManager>();
+            public Mock<IDiagnosticFactory> Diagnostic { get; } = new Mock<IDiagnosticFactory>();
+            public Mock<IConnection> Connection { get; } = new Mock<IConnection>();
+        }
+    }
+}
