d27051369b658009743a6a9ef38494c0fb052f6e
JP Dillingham <jpdillingham@gmail.com>
2019-08-25 08:16:52 -0500

    add Matches() byte[] extension

---
+++ tests/Soulseek.Tests.Unit/Extensions.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Extensions.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.Tests.Unit
+{
+    using System.Text;
+
+    public static class Extensions
+    {
+        public static bool Matches(this byte[] arr1, byte[] arr2)
+        {
+            return Encoding.UTF8.GetString(arr1) == Encoding.UTF8.GetString(arr2);
+        }
+    }
+}
