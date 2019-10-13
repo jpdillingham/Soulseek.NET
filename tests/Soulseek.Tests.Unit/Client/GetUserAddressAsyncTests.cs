7fba1a058a12ebfc8a188ef4f1d814076a25c352
JP Dillingham <jpdillingham@gmail.com>
2019-10-13 13:09:47 -0500

    add tests for GetUserAddressAsync()

---
+++ tests/Soulseek.Tests.Unit/Client/GetUserAddressAsyncTests.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="GetUserAddressAsyncTests.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.Tests.Unit.Client
+{
+    using System;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AutoFixture.Xunit2;
+    using Moq;
+    using Soulseek.Exceptions;
+    using Soulseek.Messaging.Messages;
+    using Soulseek.Network;
+    using Xunit;
+
+    public class GetUserAddressAsyncTests
+    {
+        [Trait("Category", "GetUserAddressAsync")]
+        [Theory(DisplayName = "GetUserAddressAsync throws ArgumentException on bad username")]
+        [InlineData(null)]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        [InlineData("")]
+        public async Task GetUserAddressAsync_Throws_ArgumentException_On_Null_Username(string username)
+        {
+            using (var s = new SoulseekClient())
+            {
+                s.SetProperty("State", SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
+
+                var ex = await Record.ExceptionAsync(async () => await s.GetUserAddressAsync(username));
+
+                Assert.NotNull(ex);
+                Assert.IsType<ArgumentException>(ex);
+            }
+        }
+
+        [Trait("Category", "GetUserAddressAsync")]
+        [Theory(DisplayName = "GetUserAddressAsync throws InvalidOperationException if not connected and logged in")]
+        [InlineData(SoulseekClientStates.None)]
+        [InlineData(SoulseekClientStates.Disconnected)]
+        [InlineData(SoulseekClientStates.Connected)]
+        [InlineData(SoulseekClientStates.LoggedIn)]
+        public async Task GetUserAddressAsync_Throws_InvalidOperationException_If_Logged_In(SoulseekClientStates state)
+        {
+            using (var s = new SoulseekClient())
+            {
+                s.SetProperty("State", state);
+
+                var ex = await Record.ExceptionAsync(async () => await s.GetUserAddressAsync("a"));
+
+                Assert.NotNull(ex);
+                Assert.IsType<InvalidOperationException>(ex);
+            }
+        }
+    }
+}
