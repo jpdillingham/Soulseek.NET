ec370054b2e2525e7081681cb85ae0eb9001d6ee
JP Dillingham <jpdillingham@gmail.com>
2019-10-24 08:55:09 -0500

    add additional tests for AddUserAsync

--- tests/Soulseek.Tests.Unit/Client/AddUserAsyncTests.cs
+++ tests/Soulseek.Tests.Unit/Client/AddUserAsyncTests.cs
@@ -93,9 +93,9 @@ namespace Soulseek.Tests.Unit.Client
             }
         }
 
-        [Trait("Category", "GetUserStatusAsync")]
-        [Theory(DisplayName = "GetUserStatusAsync throws UserStatusException on throw"), AutoData]
-        public async Task GetUserStatusAsync_Throws_UserStatusException_On_Throw(string username, bool exists, UserStatus status, int averageSpeed, int downloadCount, int fileCount, int directoryCount, string countryCode)
+        [Trait("Category", "AddUserAsyncAsync")]
+        [Theory(DisplayName = "AddUserAsyncAsync throws UserStatusException on throw"), AutoData]
+        public async Task AddUserAsyncAsync_Throws_UserStatusException_On_Throw(string username, bool exists, UserStatus status, int averageSpeed, int downloadCount, int fileCount, int directoryCount, string countryCode)
         {
             var result = new AddUserResponse(username, exists, status, averageSpeed, downloadCount, fileCount, directoryCount, countryCode);
 
@@ -117,6 +117,58 @@ namespace Soulseek.Tests.Unit.Client
                 Assert.NotNull(ex);
                 Assert.IsType<AddUserException>(ex);
                 Assert.IsType<ConnectionException>(ex.InnerException);
+            }
+        }
+
+        [Trait("Category", "AddUserAsyncAsync")]
+        [Theory(DisplayName = "AddUserAsync throws TimeoutException on timeout"), AutoData]
+        public async Task AddUserAsyncAsync_Throws_TimeoutException_On_Timeout(string username, bool exists, UserStatus status, int averageSpeed, int downloadCount, int fileCount, int directoryCount, string countryCode)
+        {
+            var result = new AddUserResponse(username, exists, status, averageSpeed, downloadCount, fileCount, directoryCount, countryCode);
+
+            var waiter = new Mock<IWaiter>();
+            waiter.Setup(m => m.Wait<AddUserResponse>(It.IsAny<WaitKey>(), null, It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(result));
+
+            var serverConn = new Mock<IMessageConnection>();
+            serverConn.Setup(m => m.WriteAsync(It.IsAny<byte[]>(), It.IsAny<CancellationToken>()))
+                .Throws(new TimeoutException());
+
+            using (var s = new SoulseekClient("127.0.0.1", 1, waiter: waiter.Object, serverConnection: serverConn.Object))
+            {
+                s.SetProperty("State", SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
+
+                AddUserResponse r = null;
+                var ex = await Record.ExceptionAsync(async () => r = await s.AddUserAsync(username));
+
+                Assert.NotNull(ex);
+                Assert.IsType<TimeoutException>(ex);
+            }
+        }
+
+        [Trait("Category", "AddUserAsync")]
+        [Theory(DisplayName = "AddUserAsync throws OperationCanceledException on cancel"), AutoData]
+        public async Task AddUserAsync_Throws_OperationCanceledException_On_Cancel(string username, bool exists, UserStatus status, int averageSpeed, int downloadCount, int fileCount, int directoryCount, string countryCode)
+        {
+            var result = new AddUserResponse(username, exists, status, averageSpeed, downloadCount, fileCount, directoryCount, countryCode);
+
+            var waiter = new Mock<IWaiter>();
+            waiter.Setup(m => m.Wait<AddUserResponse>(It.IsAny<WaitKey>(), null, It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(result));
+
+            var serverConn = new Mock<IMessageConnection>();
+            serverConn.Setup(m => m.WriteAsync(It.IsAny<byte[]>(), It.IsAny<CancellationToken>()))
+                .Throws(new OperationCanceledException());
+
+            using (var s = new SoulseekClient("127.0.0.1", 1, waiter: waiter.Object, serverConnection: serverConn.Object))
+            {
+                s.SetProperty("State", SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
+
+                AddUserResponse r = null;
+                var ex = await Record.ExceptionAsync(async () => r = await s.AddUserAsync(username));
+
+                Assert.NotNull(ex);
+                Assert.IsType<OperationCanceledException>(ex);
             }
         }
     }
