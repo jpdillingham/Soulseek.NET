e2d1982870964e42d00a1cda217ccf29d5fd8419
JP Dillingham <jpdillingham@gmail.com>
2019-10-27 07:32:37 -0500

    timeout and cancellation tests for GetUserInfoAsync

--- tests/Soulseek.Tests.Unit/Client/GetUserInfoAsyncTests.cs
+++ tests/Soulseek.Tests.Unit/Client/GetUserInfoAsyncTests.cs
@@ -103,8 +103,80 @@ namespace Soulseek.Tests.Unit.Client
         }
 
         [Trait("Category", "GetUserInfoAsync")]
-        [Theory(DisplayName = "GetUserInfoAsync throws PeerInfoException on throw"), AutoData]
-        public async Task GetUserInfoAsync_Throws_PeerInfoException_On_Throw(string username, string description, byte[] picture, int uploadSlots, int queueLength, bool hasFreeSlot)
+        [Theory(DisplayName = "GetUserInfoAsync throws TimeoutException on timeout"), AutoData]
+        public async Task GetUserInfoAsync_Throws_TimeoutException_On_Timeout(string username, string description, byte[] picture, int uploadSlots, int queueLength, bool hasFreeSlot)
+        {
+            var result = new UserInfoResponse(description, picture, uploadSlots, queueLength, hasFreeSlot);
+
+            var waiter = new Mock<IWaiter>();
+            waiter.Setup(m => m.Wait<UserInfoResponse>(It.IsAny<WaitKey>(), null, It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(result));
+            waiter.Setup(m => m.Wait<UserAddressResponse>(It.IsAny<WaitKey>(), null, It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(new UserAddressResponse(username, IPAddress.Parse("127.0.0.1"), 1)));
+
+            var serverConn = new Mock<IMessageConnection>();
+            serverConn.Setup(m => m.WriteAsync(It.IsAny<byte[]>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            var conn = new Mock<IMessageConnection>();
+            conn.Setup(m => m.WriteAsync(It.IsAny<byte[]>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromException(new TimeoutException()));
+
+            var connManager = new Mock<IPeerConnectionManager>();
+            connManager.Setup(m => m.GetOrAddMessageConnectionAsync(username, It.IsAny<IPAddress>(), It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(conn.Object));
+
+            using (var s = new SoulseekClient("127.0.0.1", 1, waiter: waiter.Object, serverConnection: serverConn.Object, peerConnectionManager: connManager.Object))
+            {
+                s.SetProperty("State", SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
+
+                UserInfoResponse info = null;
+                var ex = await Record.ExceptionAsync(async () => info = await s.GetUserInfoAsync(username));
+
+                Assert.NotNull(ex);
+                Assert.IsType<TimeoutException>(ex);
+            }
+        }
+
+        [Trait("Category", "GetUserInfoAsync")]
+        [Theory(DisplayName = "GetUserInfoAsync throws OperationCanceledException on cancellation"), AutoData]
+        public async Task GetUserInfoAsync_Throws_OperationCanceledException_On_Cancellation(string username, string description, byte[] picture, int uploadSlots, int queueLength, bool hasFreeSlot)
+        {
+            var result = new UserInfoResponse(description, picture, uploadSlots, queueLength, hasFreeSlot);
+
+            var waiter = new Mock<IWaiter>();
+            waiter.Setup(m => m.Wait<UserInfoResponse>(It.IsAny<WaitKey>(), null, It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(result));
+            waiter.Setup(m => m.Wait<UserAddressResponse>(It.IsAny<WaitKey>(), null, It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(new UserAddressResponse(username, IPAddress.Parse("127.0.0.1"), 1)));
+
+            var serverConn = new Mock<IMessageConnection>();
+            serverConn.Setup(m => m.WriteAsync(It.IsAny<byte[]>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            var conn = new Mock<IMessageConnection>();
+            conn.Setup(m => m.WriteAsync(It.IsAny<byte[]>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromException(new OperationCanceledException()));
+
+            var connManager = new Mock<IPeerConnectionManager>();
+            connManager.Setup(m => m.GetOrAddMessageConnectionAsync(username, It.IsAny<IPAddress>(), It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(conn.Object));
+
+            using (var s = new SoulseekClient("127.0.0.1", 1, waiter: waiter.Object, serverConnection: serverConn.Object, peerConnectionManager: connManager.Object))
+            {
+                s.SetProperty("State", SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
+
+                UserInfoResponse info = null;
+                var ex = await Record.ExceptionAsync(async () => info = await s.GetUserInfoAsync(username));
+
+                Assert.NotNull(ex);
+                Assert.IsType<OperationCanceledException>(ex);
+            }
+        }
+
+        [Trait("Category", "GetUserInfoAsync")]
+        [Theory(DisplayName = "GetUserInfoAsync throws UserInfoException on throw"), AutoData]
+        public async Task GetUserInfoAsync_Throws_UserInfoException_On_Throw(string username, string description, byte[] picture, int uploadSlots, int queueLength, bool hasFreeSlot)
         {
             var result = new UserInfoResponse(description, picture, uploadSlots, queueLength, hasFreeSlot);
 
