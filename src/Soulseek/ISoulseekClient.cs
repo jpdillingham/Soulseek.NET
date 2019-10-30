0cf515d25b350a204d375e8a1adc92078e57a496
JP Dillingham <jpdillingham@gmail.com>
2019-10-30 08:54:57 -0500

    rename OnlineStatus -> UserStatus

--- src/Soulseek/ISoulseekClient.cs
+++ src/Soulseek/ISoulseekClient.cs
@@ -320,8 +320,8 @@ namespace Soulseek
         /// <exception cref="InvalidOperationException">Thrown when the client is not connected or logged in.</exception>
         /// <exception cref="TimeoutException">Thrown when the operation has timed out.</exception>
         /// <exception cref="OperationCanceledException">Thrown when the operation has been cancelled.</exception>
-        /// <exception cref="OnlineStatusException">Thrown when an exception is encountered during the operation.</exception>
-        Task SetOnlineStatusAsync(UserStatus status, CancellationToken? cancellationToken = null);
+        /// <exception cref="UserStatusException">Thrown when an exception is encountered during the operation.</exception>
+        Task SetUserStatusAsync(UserStatus status, CancellationToken? cancellationToken = null);
 
         /// <summary>
         ///     Asynchronously informs the server of the number of shared <paramref name="directories"/> and <paramref name="files"/>.
--- src/Soulseek/SoulseekClient.cs
+++ src/Soulseek/SoulseekClient.cs
@@ -713,8 +713,8 @@ namespace Soulseek
         /// <exception cref="InvalidOperationException">Thrown when the client is not connected or logged in.</exception>
         /// <exception cref="TimeoutException">Thrown when the operation has timed out.</exception>
         /// <exception cref="OperationCanceledException">Thrown when the operation has been cancelled.</exception>
-        /// <exception cref="OnlineStatusException">Thrown when an exception is encountered during the operation.</exception>
-        public Task SetOnlineStatusAsync(UserStatus status, CancellationToken? cancellationToken = null)
+        /// <exception cref="UserStatusException">Thrown when an exception is encountered during the operation.</exception>
+        public Task SetUserStatusAsync(UserStatus status, CancellationToken? cancellationToken = null)
         {
             if (!State.HasFlag(SoulseekClientStates.Connected) || !State.HasFlag(SoulseekClientStates.LoggedIn))
             {
@@ -727,7 +727,7 @@ namespace Soulseek
             }
             catch (Exception ex) when (!(ex is OperationCanceledException) && !(ex is TimeoutException))
             {
-                throw new OnlineStatusException($"Failed to set online status to {status}: {ex.Message}", ex);
+                throw new UserStatusException($"Failed to set user status to {status}: {ex.Message}", ex);
             }
         }
 
