c136b73dd3b2bd3acab1f036fcdc297f3f2cd301
JP Dillingham <jpdillingham@gmail.com>
2019-10-31 07:37:48 -0500

    add SharedCountsException

--- src/Soulseek/Common/Exceptions/SoulseekClientException.cs
+++ src/Soulseek/Common/Exceptions/SoulseekClientException.cs
@@ -456,4 +456,53 @@ namespace Soulseek.Exceptions
         {
         }
     }
+
+    /// <summary>
+    ///     Represents errors that occur when attempting to inform the server of the number of shared directores and files.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class SharedCountsException : SoulseekClientException
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SharedCountsException"/> class.
+        /// </summary>
+        public SharedCountsException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SharedCountsException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public SharedCountsException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SharedCountsException"/> class with a specified error message and a
+        ///     reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner
+        ///     exception is specified.
+        /// </param>
+        public SharedCountsException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SharedCountsException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
+        protected SharedCountsException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
 }
