5ec881dc6d27a86820944e418979d750ebe98401
JP Dillingham <jpdillingham@gmail.com>
2019-11-11 20:56:04 -0600

    add RoomListException

--- src/Soulseek/Common/Exceptions/SoulseekClientException.cs
+++ src/Soulseek/Common/Exceptions/SoulseekClientException.cs
@@ -505,4 +505,53 @@ namespace Soulseek.Exceptions
         {
         }
     }
+
+    /// <summary>
+    ///     Represents errors that occur when fetching the list of chat rooms from the server.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class RoomListException : SoulseekClientException
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoomListException"/> class.
+        /// </summary>
+        public RoomListException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoomListException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public RoomListException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoomListException"/> class with a specified error message and a
+        ///     reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner
+        ///     exception is specified.
+        /// </param>
+        public RoomListException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoomListException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
+        protected RoomListException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
 }
