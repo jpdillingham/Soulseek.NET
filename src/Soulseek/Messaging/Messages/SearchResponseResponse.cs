3d6ff0e7bff2c94cd3f92a12290796589593a761
JP Dillingham <jpdillingham@gmail.com>
2019-11-22 17:14:18 -0600

    refactor two-way external classes

---
+++ src/Soulseek/Messaging/Messages/SearchResponseResponse.cs
@@ -0,0 +1,130 @@
+﻿// <copyright file="SearchResponseResponse.cs" company="JP Dillingham">
+//     Copyright (c) JP Dillingham. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+//     as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace Soulseek.Messaging.Messages
+{
+    using System.Collections.Generic;
+    using Soulseek.Messaging;
+
+    /// <summary>
+    ///     The response to a file search request.
+    /// </summary>
+    internal static class SearchResponseResponse
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="SearchResponse"/> from the specified <paramref name="bytes"/>.
+        /// </summary>
+        /// <param name="bytes">The byte array from which to parse.</param>
+        /// <returns>The parsed instance.</returns>
+        internal static SearchResponse FromByteArray(byte[] bytes)
+        {
+            var slim = SearchResponseSlim.FromByteArray(bytes);
+            return FromSlimResponse(slim);
+        }
+
+        /// <summary>
+        ///     Creates a new instance of <see cref="SearchResponse"/> from the specified <paramref name="slimResponse"/>.
+        /// </summary>
+        /// <param name="slimResponse">The slim response from which to parse.</param>
+        /// <returns>The parsed instance.</returns>
+        internal static SearchResponse FromSlimResponse(SearchResponseSlim slimResponse)
+        {
+            var files = ParseFiles(slimResponse.MessageReader, slimResponse.FileCount);
+            return new SearchResponse(slimResponse.Username, slimResponse.Token, files.Count, slimResponse.FreeUploadSlots, slimResponse.UploadSpeed, slimResponse.QueueLength, files);
+        }
+
+        /// <summary>
+        ///     Constructs a <see cref="byte"/> array from this message.
+        /// </summary>
+        /// <param name="searchResponse">The instance from which to construct the byte array.</param>
+        /// <returns>The constructed byte array.</returns>
+        internal static byte[] ToByteArray(this SearchResponse searchResponse)
+        {
+            var builder = new MessageBuilder()
+                .WriteCode(MessageCode.Peer.SearchResponse)
+                .WriteString(searchResponse.Username)
+                .WriteInteger(searchResponse.Token)
+                .WriteInteger(searchResponse.FileCount);
+
+            foreach (var file in searchResponse.Files)
+            {
+                builder
+                    .WriteByte((byte)file.Code)
+                    .WriteString(file.Filename)
+                    .WriteLong(file.Size)
+                    .WriteString(file.Extension)
+                    .WriteInteger(file.AttributeCount);
+
+                foreach (var attribute in file.Attributes)
+                {
+                    builder
+                        .WriteInteger((int)attribute.Type)
+                        .WriteInteger(attribute.Value);
+                }
+            }
+
+            builder
+                .WriteByte((byte)searchResponse.FreeUploadSlots)
+                .WriteInteger(searchResponse.UploadSpeed)
+                .WriteLong(searchResponse.QueueLength);
+
+            builder.Compress();
+            return builder.Build();
+        }
+
+        /// <summary>
+        ///     Parses the list of files contained within the <paramref name="reader"/>.
+        /// </summary>
+        /// <remarks>
+        ///     Requires that the provided MessageReader has been "rewound" to the start of the file list, which is equal to the
+        ///     length of the username plus 12.
+        /// </remarks>
+        /// <param name="reader">The reader from which to parse the file list.</param>
+        /// <param name="count">The expected number of files.</param>
+        /// <returns>The list of parsed files.</returns>
+        private static IReadOnlyCollection<File> ParseFiles(MessageReader<MessageCode.Peer> reader, int count)
+        {
+            var files = new List<File>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var file = new File(
+                    code: reader.ReadByte(),
+                    filename: reader.ReadString(),
+                    size: reader.ReadLong(),
+                    extension: reader.ReadString(),
+                    attributeCount: reader.ReadInteger());
+
+                var attributeList = new List<FileAttribute>();
+
+                for (int j = 0; j < file.AttributeCount; j++)
+                {
+                    var attribute = new FileAttribute(
+                        type: (FileAttributeType)reader.ReadInteger(),
+                        value: reader.ReadInteger());
+
+                    attributeList.Add(attribute);
+                }
+
+                files.Add(new File(
+                    code: file.Code,
+                    filename: file.Filename,
+                    size: file.Size,
+                    extension: file.Extension,
+                    attributeCount: file.AttributeCount,
+                    attributeList: attributeList));
+            }
+
+            return files.AsReadOnly();
+        }
+    }
+}
--- src/Soulseek/Messaging/Messages/UserInfoResponse.cs
+++ src/Soulseek/Messaging/Messages/UserInfoResponse.cs
@@ -10,95 +10,22 @@
 //     You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
 // </copyright>
 
-namespace Soulseek.Messaging.Messages
+namespace Soulseek
 {
     using Soulseek.Exceptions;
+    using Soulseek.Messaging;
 
     /// <summary>
     ///     The response to a user info request.
     /// </summary>
-    public sealed class UserInfoResponse
+    internal static class UserInfoResponse
     {
         /// <summary>
-        ///     Initializes a new instance of the <see cref="UserInfoResponse"/> class.
-        /// </summary>
-        /// <param name="description">The peer's description.</param>
-        /// <param name="uploadSlots">The number of configured upload slots.</param>
-        /// <param name="queueLength">The current queue length.</param>
-        /// <param name="hasFreeUploadSlot">A value indicating whether an upload slot is free.</param>
-        public UserInfoResponse(string description, int uploadSlots, int queueLength, bool hasFreeUploadSlot)
-            : this(description, false, null, uploadSlots, queueLength, hasFreeUploadSlot)
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="UserInfoResponse"/> class.
-        /// </summary>
-        /// <param name="description">The peer's description.</param>
-        /// <param name="picture">If configured, the picture data.</param>
-        /// <param name="uploadSlots">The number of configured upload slots.</param>
-        /// <param name="queueLength">The current queue length.</param>
-        /// <param name="hasFreeUploadSlot">A value indicating whether an upload slot is free.</param>
-        public UserInfoResponse(string description, byte[] picture, int uploadSlots, int queueLength, bool hasFreeUploadSlot)
-            : this(description, picture != null, picture, uploadSlots, queueLength, hasFreeUploadSlot)
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="UserInfoResponse"/> class.
-        /// </summary>
-        /// <param name="description">The peer's description.</param>
-        /// <param name="hasPicture">A value indicating whether a picture has been configured.</param>
-        /// <param name="picture">If configured, the picture data.</param>
-        /// <param name="uploadSlots">The number of configured upload slots.</param>
-        /// <param name="queueLength">The current queue length.</param>
-        /// <param name="hasFreeUploadSlot">A value indicating whether an upload slot is free.</param>
-        private UserInfoResponse(string description, bool hasPicture, byte[] picture, int uploadSlots, int queueLength, bool hasFreeUploadSlot)
-        {
-            Description = description;
-            HasPicture = hasPicture;
-            Picture = picture;
-            UploadSlots = uploadSlots;
-            QueueLength = queueLength;
-            HasFreeUploadSlot = hasFreeUploadSlot;
-        }
-
-        /// <summary>
-        ///     Gets the user's description.
-        /// </summary>
-        public string Description { get; }
-
-        /// <summary>
-        ///     Gets a value indicating whether an upload slot is free.
-        /// </summary>
-        public bool HasFreeUploadSlot { get; }
-
-        /// <summary>
-        ///     Gets a value indicating whether a picture has been configured.
-        /// </summary>
-        public bool HasPicture { get; }
-
-        /// <summary>
-        ///     Gets the picture data, if configured.
-        /// </summary>
-        public byte[] Picture { get; }
-
-        /// <summary>
-        ///     Gets the current queue length.
-        /// </summary>
-        public int QueueLength { get; }
-
-        /// <summary>
-        ///     Gets the number of configured upload slots.
-        /// </summary>
-        public int UploadSlots { get; }
-
-        /// <summary>
-        ///     Creates a new instance of <see cref="UserInfoResponse"/> from the specified <paramref name="bytes"/>.
+        ///     Creates a new instance of <see cref="UserInfo"/> from the specified <paramref name="bytes"/>.
         /// </summary>
         /// <param name="bytes">The byte array from which to parse.</param>
         /// <returns>The parsed instance.</returns>
-        internal static UserInfoResponse FromByteArray(byte[] bytes)
+        internal static UserInfo FromByteArray(byte[] bytes)
         {
             var reader = new MessageReader<MessageCode.Peer>(bytes);
             var code = reader.ReadCode();
@@ -122,31 +49,32 @@ namespace Soulseek.Messaging.Messages
             var queueLength = reader.ReadInteger();
             var hasFreeUploadSlot = reader.ReadByte() > 0;
 
-            return new UserInfoResponse(description, hasPicture, picture, uploadSlots, queueLength, hasFreeUploadSlot);
+            return new UserInfo(description, hasPicture, picture, uploadSlots, queueLength, hasFreeUploadSlot);
         }
 
         /// <summary>
         ///     Constructs a <see cref="byte"/> array from this message.
         /// </summary>
+        /// <param name="userInfo">The instance from which to construct the byte array.</param>
         /// <returns>The constructed byte array.</returns>
-        internal byte[] ToByteArray()
+        internal static byte[] ToByteArray(this UserInfo userInfo)
         {
             var builder = new MessageBuilder()
                 .WriteCode(MessageCode.Peer.InfoResponse)
-                .WriteString(Description)
-                .WriteByte((byte)(HasPicture ? 1 : 0));
+                .WriteString(userInfo.Description)
+                .WriteByte((byte)(userInfo.HasPicture ? 1 : 0));
 
-            if (HasPicture)
+            if (userInfo.HasPicture)
             {
                 builder
-                    .WriteInteger(Picture.Length)
-                    .WriteBytes(Picture);
+                    .WriteInteger(userInfo.Picture.Length)
+                    .WriteBytes(userInfo.Picture);
             }
 
             builder
-                .WriteInteger(UploadSlots)
-                .WriteInteger(QueueLength)
-                .WriteByte((byte)(HasFreeUploadSlot ? 1 : 0));
+                .WriteInteger(userInfo.UploadSlots)
+                .WriteInteger(userInfo.QueueLength)
+                .WriteByte((byte)(userInfo.HasFreeUploadSlot ? 1 : 0));
 
             return builder.Build();
         }
