e379e917460290dc487079e8f0e58ba2130c8094
JP Dillingham <jpdillingham@gmail.com>
2019-12-03 07:56:52 -0600

    add tests to cover Transfer

--- tests/Soulseek.Tests.Unit/TransferTests.cs
+++ tests/Soulseek.Tests.Unit/TransferTests.cs
@@ -15,205 +15,76 @@ namespace Soulseek.Tests.Unit
     using System;
     using System.Net;
     using AutoFixture.Xunit2;
-    using Moq;
-    using Soulseek.Network.Tcp;
     using Xunit;
 
     public class TransferTests
     {
         [Trait("Category", "Instantiation")]
-        [Theory(DisplayName = "Instantiates with the given data"), AutoData]
-        internal void Instantiates_With_The_Given_Data(string username, string filename, int token)
+        [Theory(DisplayName = "Instantiates with expected data"), AutoData]
+        public void Instantiates_With_Expected_Data(
+            TransferDirection direction,
+            string username,
+            string filename,
+            int token,
+            TransferStates state,
+            long size,
+            long bytesTransferred,
+            double averageSpeed,
+            DateTime? startTime,
+            DateTime? endTime,
+            int? remoteToken,
+            IPAddress ipAddress,
+            int? port)
         {
-            var d = new TransferInternal(TransferDirection.Download, username, filename, token);
+            var t = new Transfer(
+                direction,
+                username,
+                filename,
+                token,
+                state,
+                size,
+                bytesTransferred,
+                averageSpeed,
+                startTime,
+                endTime,
+                remoteToken,
+                ipAddress,
+                port);
 
-            Assert.Equal(username, d.Username);
-            Assert.Equal(filename, d.Filename);
-            Assert.Equal(token, d.Token);
+            Assert.Equal(direction, t.Direction);
+            Assert.Equal(username, t.Username);
+            Assert.Equal(filename, t.Filename);
+            Assert.Equal(token, t.Token);
+            Assert.Equal(state, t.State);
+            Assert.Equal(size, t.Size);
+            Assert.Equal(bytesTransferred, t.BytesTransferred);
+            Assert.Equal(averageSpeed, t.AverageSpeed);
+            Assert.Equal(startTime, t.StartTime);
+            Assert.Equal(endTime, t.EndTime);
+            Assert.Equal(remoteToken, t.RemoteToken);
+            Assert.Equal(ipAddress, t.IPAddress);
+            Assert.Equal(port, t.Port);
         }
 
-        [Trait("Category", "Properties")]
-        [Theory(DisplayName = "Properties default to expected values"), AutoData]
-        internal void Properties_Default_To_Expected_Values(string username, string filename, int token, TransferOptions options)
+        [Trait("Category", "Instantiation")]
+        [Theory(DisplayName = "Instantiates with expected data given TransferInternal"), AutoData]
+        internal void Instantiates_With_Expected_Data_Given_TransferInternal(TransferInternal i)
         {
-            var d = new TransferInternal(TransferDirection.Download, username, filename, token, options);
+            var t = new Transfer(i);
 
-            Assert.Null(d.Connection);
-            Assert.Null(d.IPAddress);
-            Assert.Null(d.Port);
-            Assert.Null(d.RemoteToken);
-            Assert.Equal(0, d.Size);
-            Assert.Equal(TransferStates.None, d.State);
-            Assert.Equal(0, d.AverageSpeed);
-            Assert.Equal(0, d.BytesTransferred);
-            Assert.Equal(0, d.BytesRemaining);
-            Assert.Equal(default(TimeSpan), d.ElapsedTime);
-            Assert.Equal(default(TimeSpan), d.RemainingTime);
-            Assert.Null(d.StartTime);
-            Assert.Null(d.EndTime);
-            Assert.Equal(0, d.PercentComplete);
-            Assert.Null(d.RemoteToken);
-            Assert.Equal(options, d.Options);
-        }
-
-        [Trait("Category", "Properties")]
-        [Theory(DisplayName = "IPAddress and Port props return Connection props"), AutoData]
-        internal void IPAddress_And_Port_Props_Return_Connection_Props(string username, string filename, int token, IPAddress ipAddress, int port)
-        {
-            var d = new TransferInternal(TransferDirection.Download, username, filename, token);
-
-            var c = new Mock<IConnection>();
-            c.Setup(m => m.IPAddress)
-                .Returns(ipAddress);
-            c.Setup(m => m.Port)
-                .Returns(port);
-
-            d.Connection = c.Object;
-
-            Assert.Equal(ipAddress, d.IPAddress);
-            Assert.Equal(port, (int)d.Port);
-        }
-
-        [Trait("Category", "Wait Key")]
-        [Theory(DisplayName = "Wait key is expected value"), AutoData]
-        internal void Wait_Key_Is_Expected_Value(string username, string filename, int token, TransferDirection direction)
-        {
-            var d = new TransferInternal(direction, username, filename, token);
-
-            Assert.Equal(new WaitKey(Constants.WaitKey.Transfer, direction, username, filename, token), d.WaitKey);
-        }
-
-        [Trait("Category", "State")]
-        [Fact(DisplayName = "State transitions set time props")]
-        internal void State_Transitions_Set_Time_Props()
-        {
-            var d = new TransferInternal(TransferDirection.Download, string.Empty, string.Empty, 0);
-
-            var s1 = d.StartTime;
-            var e1 = d.EndTime;
-            var et1 = d.ElapsedTime;
-            var rt1 = d.RemainingTime;
-
-            d.State = TransferStates.InProgress;
-
-            var s2 = d.StartTime;
-            var e2 = d.EndTime;
-            var et2 = d.ElapsedTime;
-            var rt2 = d.RemainingTime;
-
-            d.State = TransferStates.Completed;
-
-            Assert.Null(s1);
-            Assert.Null(e1);
-            Assert.Equal(default(TimeSpan), et1);
-            Assert.Equal(default(TimeSpan), rt1);
-
-            Assert.NotNull(s2);
-            Assert.Null(e2);
-            Assert.NotEqual(default(TimeSpan), et2);
-            Assert.Equal(default(TimeSpan), rt2);
-
-            Assert.NotNull(d.StartTime);
-            Assert.NotNull(d.EndTime);
-            Assert.NotEqual(default(TimeSpan), d.ElapsedTime);
-            Assert.Equal(default(TimeSpan), d.RemainingTime);
-        }
-
-        [Trait("Category", "State")]
-        [Fact(DisplayName = "ElapsedTime works")]
-        internal void ElapsedTime_Works()
-        {
-            var d = new TransferInternal(TransferDirection.Download, string.Empty, string.Empty, 0);
-
-            var s = new DateTime(2019, 4, 25);
-            var e = new DateTime(2019, 4, 26);
-
-            Assert.Equal(default(TimeSpan), d.ElapsedTime);
-
-            d.SetProperty("StartTime", s);
-
-            Assert.NotEqual(default(TimeSpan), d.ElapsedTime);
-
-            d.SetProperty("EndTime", e);
-
-            Assert.Equal(e - s, d.ElapsedTime);
-        }
-
-        [Trait("Category", "State")]
-        [Fact(DisplayName = "RemainingTime works")]
-        internal void RemainingTime_Works()
-        {
-            var d = new TransferInternal(TransferDirection.Download, string.Empty, string.Empty, 0);
-
-            Assert.Equal(default(TimeSpan), d.RemainingTime);
-
-            d.SetProperty("AverageSpeed", 1);
-            d.SetProperty("Size", 2);
-            d.SetProperty("BytesTransferred", 1);
-
-            Assert.Equal(TimeSpan.FromSeconds(1), d.RemainingTime);
-        }
-
-        [Trait("Category", "State")]
-        [Fact(DisplayName = "PercentComplete works")]
-        internal void PercentComplete_Works()
-        {
-            var d = new TransferInternal(TransferDirection.Download, string.Empty, string.Empty, 0);
-
-            Assert.Equal(0, d.PercentComplete);
-
-            d.SetProperty("Size", 100);
-            d.SetProperty("BytesTransferred", 50);
-
-            Assert.Equal(50, d.PercentComplete);
-        }
-
-        [Trait("Category", "UpdateProgress")]
-        [Fact(DisplayName = "UpdateProgress sets AverageSpeed initial value")]
-        internal void UpdateProgress_Sets_AverageSpeed_To_Initial_Value()
-        {
-            var d = new TransferInternal(TransferDirection.Download, string.Empty, string.Empty, 0);
-
-            Assert.Equal(0, d.AverageSpeed);
-
-            d.SetField("progressUpdateLimit", 0);
-            d.SetProperty("State", TransferStates.InProgress);
-            d.InvokeMethod("UpdateProgress", 100000);
-
-            Assert.NotEqual(0, d.AverageSpeed);
-        }
-
-        [Trait("Category", "UpdateProgress")]
-        [Fact(DisplayName = "UpdateProgress updates AverageSpeed on subsequent call")]
-        internal void UpdateProgress_Updates_AverageSpeed_On_Subsequent_Call()
-        {
-            var d = new TransferInternal(TransferDirection.Download, string.Empty, string.Empty, 0);
-
-            Assert.Equal(0, d.AverageSpeed);
-
-            d.SetField("progressUpdateLimit", 0);
-            d.SetProperty("State", TransferStates.InProgress);
-            d.InvokeMethod("UpdateProgress", 100000);
-
-            var v1 = d.AverageSpeed;
-            Assert.NotEqual(0, v1);
-
-            d.InvokeMethod("UpdateProgress", 10);
-
-            Assert.NotEqual(v1, d.AverageSpeed);
-        }
-
-        [Trait("Category", "UpdateProgress")]
-        [Fact(DisplayName = "UpdateProgress ignores AverageSpeed if StartTime and lastProgressTime is null")]
-        internal void UpdateProgress_Ignores_AverageSpeed_If_StarTime_And_lastProgressTime_Is_Null()
-        {
-            var d = new TransferInternal(TransferDirection.Download, string.Empty, string.Empty, 0);
-
-            Assert.Equal(0, d.AverageSpeed);
-
-            d.InvokeMethod("UpdateProgress", 100000);
-
-            Assert.Equal(0, d.AverageSpeed);
+            Assert.Equal(i.Direction, t.Direction);
+            Assert.Equal(i.Username, t.Username);
+            Assert.Equal(i.Filename, t.Filename);
+            Assert.Equal(i.Token, t.Token);
+            Assert.Equal(i.State, t.State);
+            Assert.Equal(i.Size, t.Size);
+            Assert.Equal(i.BytesTransferred, t.BytesTransferred);
+            Assert.Equal(i.AverageSpeed, t.AverageSpeed);
+            Assert.Equal(i.StartTime, t.StartTime);
+            Assert.Equal(i.EndTime, t.EndTime);
+            Assert.Equal(i.RemoteToken, t.RemoteToken);
+            Assert.Equal(i.IPAddress, t.IPAddress);
+            Assert.Equal(i.Port, t.Port);
         }
     }
 }
