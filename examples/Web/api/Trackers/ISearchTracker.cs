8d1a1fbdeddaa121d551c09633ff9c6b098c7195
JP Dillingham <jpdillingham@gmail.com>
2019-12-04 08:26:17 -0600

    docs and cleanup of SearchTracker

--- examples/Web/api/Trackers/ISearchTracker.cs
+++ examples/Web/api/Trackers/ISearchTracker.cs
@@ -3,12 +3,37 @@
     using Soulseek;
     using System.Collections.Concurrent;
 
+    /// <summary>
+    ///     Tracks active searches.
+    /// </summary>
     public interface ISearchTracker
     {
+        /// <summary>
+        ///     Gets active searches.
+        /// </summary>
         ConcurrentDictionary<string, Search> Searches { get; }
+
+        /// <summary>
+        ///     Adds or updates a tracked search.
+        /// </summary>
+        /// <param name="args"></param>
         void AddOrUpdate(SearchResponseReceivedEventArgs args);
+
+        /// <summary>
+        ///     Adds or updates a tracked search.
+        /// </summary>
+        /// <param name="args"></param>
         void AddOrUpdate(SearchStateChangedEventArgs args);
-        void TryRemove(string searchText);
+
+        /// <summary>
+        ///     Removes all tracked searches.
+        /// </summary>
         void Clear();
+
+        /// <summary>
+        ///     Removes a tracked search.
+        /// </summary>
+        /// <param name="searchText"></param>
+        void TryRemove(string searchText);
     }
 }
--- examples/Web/api/Trackers/SearchTracker.cs
+++ examples/Web/api/Trackers/SearchTracker.cs
@@ -3,26 +3,47 @@
     using Soulseek;
     using System.Collections.Concurrent;
 
+    /// <summary>
+    ///     Tracks active searches.
+    /// </summary>
     public class SearchTracker : ISearchTracker
     {
-        public ConcurrentDictionary<string, Search> Searches { get; private set; } = 
+        /// <summary>
+        ///     Gets active searches.
+        /// </summary>
+        public ConcurrentDictionary<string, Search> Searches { get; private set; } =
             new ConcurrentDictionary<string, Search>();
 
-        public void Clear()
-        {
-            Searches.Clear();
-        }
-
+        /// <summary>
+        ///     Adds or updates a tracked search.
+        /// </summary>
+        /// <param name="args"></param>
         public void AddOrUpdate(SearchResponseReceivedEventArgs args)
         {
             Searches.AddOrUpdate(args.Search.SearchText, args.Search, (searchText, search) => args.Search);
         }
 
+        /// <summary>
+        ///     Adds or updates a tracked search.
+        /// </summary>
+        /// <param name="args"></param>
         public void AddOrUpdate(SearchStateChangedEventArgs args)
         {
             Searches.AddOrUpdate(args.Search.SearchText, args.Search, (searchText, search) => args.Search);
         }
 
+        /// <summary>
+        ///     Removes all tracked searches.
+        /// </summary>
+        public void Clear()
+        {
+            Searches.Clear();
+        }
+
+        /// <summary>
+        ///     Removes a tracked search.
+        /// </summary>
+        /// <param name="searchText"></param>
         public void TryRemove(string searchText)
         {
             Searches.TryRemove(searchText, out _);
