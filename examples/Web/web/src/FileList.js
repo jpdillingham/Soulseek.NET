4da981a14b10c9ec108bcb16f761443bae1729f1
JP Dillingham <jpdillingham@gmail.com>
2019-12-10 21:50:43 -0600

    add accordion

--- examples/Web/web/src/FileList.js
+++ examples/Web/web/src/FileList.js
@@ -7,7 +7,8 @@ import {
     Table, 
     Icon, 
     List, 
-    Checkbox 
+    Checkbox,
+    Accordion
 } from 'semantic-ui-react';
 
 const FileList = ({ directoryName, files, onSelectionChange, disabled }) => (
@@ -18,46 +19,54 @@ const FileList = ({ directoryName, files, onSelectionChange, disabled }) => (
         >
             <Icon name='folder'/>{directoryName}
         </Header>
-        <List>
-            <List.Item>
-            <Table>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell className='filelist-selector'>
-                            <Checkbox 
-                                fitted
-                                onChange={(event, data) => files.map(f => onSelectionChange(f, data.checked))}
-                                checked={files.filter(f => !f.selected).length === 0}
-                                disabled={disabled}
-                            />
-                        </Table.HeaderCell>
-                        <Table.HeaderCell className='filelist-filename'>File</Table.HeaderCell>
-                        <Table.HeaderCell className='filelist-size'>Size</Table.HeaderCell>
-                        <Table.HeaderCell className='filelist-bitrate'>Bitrate</Table.HeaderCell>
-                        <Table.HeaderCell className='filelist-length'>Length</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>                                
-                <Table.Body>
-                    {files.map((f, i) => 
-                        <Table.Row key={i}>
-                            <Table.Cell className='filelist-selector'>
-                                <Checkbox 
-                                    fitted 
-                                    onChange={(event, data) => onSelectionChange(f, data.checked)}
-                                    checked={f.selected}
-                                    disabled={disabled}
-                                />
-                            </Table.Cell>
-                            <Table.Cell className='filelist-filename'>{getFileName(f.filename)}</Table.Cell>
-                            <Table.Cell className='filelist-size'>{formatBytes(f.size)}</Table.Cell>
-                            <Table.Cell className='filelist-bitrate'>{f.bitRate}</Table.Cell>
-                            <Table.Cell className='filelist-length'>{formatSeconds(f.length)}</Table.Cell>
-                        </Table.Row>
-                    )}
-                </Table.Body>
-            </Table>
-            </List.Item>
-        </List>
+        <Accordion>
+            <Accordion.Title active={true}>
+                <Icon name='dropdown' />
+                Files
+            </Accordion.Title>
+            <Accordion.Content active={true}>
+                <List>
+                    <List.Item>
+                        <Table>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell className='filelist-selector'>
+                                        <Checkbox 
+                                            fitted
+                                            onChange={(event, data) => files.map(f => onSelectionChange(f, data.checked))}
+                                            checked={files.filter(f => !f.selected).length === 0}
+                                            disabled={disabled}
+                                        />
+                                    </Table.HeaderCell>
+                                    <Table.HeaderCell className='filelist-filename'>File</Table.HeaderCell>
+                                    <Table.HeaderCell className='filelist-size'>Size</Table.HeaderCell>
+                                    <Table.HeaderCell className='filelist-bitrate'>Bitrate</Table.HeaderCell>
+                                    <Table.HeaderCell className='filelist-length'>Length</Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>                                
+                            <Table.Body>
+                                {files.map((f, i) => 
+                                    <Table.Row key={i}>
+                                        <Table.Cell className='filelist-selector'>
+                                            <Checkbox 
+                                                fitted 
+                                                onChange={(event, data) => onSelectionChange(f, data.checked)}
+                                                checked={f.selected}
+                                                disabled={disabled}
+                                            />
+                                        </Table.Cell>
+                                        <Table.Cell className='filelist-filename'>{getFileName(f.filename)}</Table.Cell>
+                                        <Table.Cell className='filelist-size'>{formatBytes(f.size)}</Table.Cell>
+                                        <Table.Cell className='filelist-bitrate'>{f.bitRate}</Table.Cell>
+                                        <Table.Cell className='filelist-length'>{formatSeconds(f.length)}</Table.Cell>
+                                    </Table.Row>
+                                )}
+                            </Table.Body>
+                        </Table>
+                    </List.Item>
+                </List>
+            </Accordion.Content>
+        </Accordion>
     </div>
 );
 
