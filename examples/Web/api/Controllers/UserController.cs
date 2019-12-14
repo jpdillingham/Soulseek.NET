55592f9e7c9f1028fe34a85bebb671f17dfbc859
JP Dillingham <jpdillingham@gmail.com>
2019-12-13 22:05:36 -0600

    docs for UserController, move browse

--- examples/Web/api/Controllers/UserController.cs
+++ examples/Web/api/Controllers/UserController.cs
@@ -1,7 +1,8 @@
 ﻿namespace WebAPI.Controllers
 {
-    using System;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Net;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Soulseek;
@@ -17,8 +18,6 @@
     [Consumes("application/json")]
     public class UserController : ControllerBase
     {
-        private ISoulseekClient Client { get; }
-
         /// <summary>
         ///     Initializes a new instance of the <see cref="UserController"/> class.
         /// </summary>
@@ -28,30 +27,90 @@
             Client = client;
         }
 
+        private ISoulseekClient Client { get; }
+
+        /// <summary>
+        ///     Retrieves the address of the specified <paramref name="username"/>.
+        /// </summary>
+        /// <param name="username">The username of the user.</param>
+        /// <returns></returns>
+        [HttpGet("{username}/address")]
+        [ProducesResponseType(typeof((IPAddress, int)), 200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Address([FromRoute, Required]string username)
+        {
+            try
+            {
+                var response = await Client.GetUserAddressAsync(username);
+                return Ok(new { response.IPAddress, response.Port });
+            }
+            catch (UserOfflineException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        /// <summary>
+        ///     Retrieves the files shared by the specified <paramref name="username"/>.
+        /// </summary>
+        /// <param name="username">The username of the user.</param>
+        /// <returns></returns>
+        [HttpGet("{username}/browse")]
+        [ProducesResponseType(typeof(IEnumerable<Directory>), 200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Browse([FromRoute, Required]string username)
+        {
+            try
+            {
+                var result = await Client.BrowseAsync(username);
+                return Ok(result);
+            }
+            catch (UserOfflineException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
         /// <summary>
         ///     Retrieves information about the specified <paramref name="username"/>.
         /// </summary>
-        /// <param name="username">The user from which to download.</param>
+        /// <param name="username">The username of the user.</param>
         /// <returns></returns>
         [HttpGet("{username}/info")]
+        [ProducesResponseType(typeof(UserInfo), 200)]
+        [ProducesResponseType(404)]
         public async Task<IActionResult> Info([FromRoute, Required]string username)
         {
-            UserInfo response;
-
             try
             {
-                response = await Client.GetUserInfoAsync(username);
+                var response = await Client.GetUserInfoAsync(username);
+                return Ok(response);
             }
-            catch (UserOfflineException)
+            catch (UserOfflineException ex)
             {
-                return NotFound();
+                return NotFound(ex.Message);
             }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
+        }
 
-            return Ok(response);
+        /// <summary>
+        ///     Retrieves status for the specified <paramref name="username"/>.
+        /// </summary>
+        /// <param name="username">The username of the user.</param>
+        /// <returns></returns>
+        [HttpGet("{username}/status")]
+        [ProducesResponseType(typeof((UserStatus, bool)), 200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Status([FromRoute, Required]string username)
+        {
+            try
+            {
+                var response = await Client.GetUserStatusAsync(username);
+                return Ok(new { response.Status, response.IsPrivileged });
+            }
+            catch (UserOfflineException ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
     }
 }
