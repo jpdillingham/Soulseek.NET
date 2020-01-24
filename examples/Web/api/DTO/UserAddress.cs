82ab9e9bf699fd951aedea6ae5eb828f7d9eddfa
JP Dillingham <jpdillingham@gmail.com>
2020-01-24 08:09:54 -0600

    docs for UserAddress

--- examples/Web/api/DTO/UserAddress.cs
+++ examples/Web/api/DTO/UserAddress.cs
@@ -1,10 +1,18 @@
 ﻿namespace WebAPI.DTO
 {
-    using System.Net;
-
+    /// <summary>
+    ///     A user's IP address and port.
+    /// </summary>
     public class UserAddress
     {
+        /// <summary>
+        ///     Gets or sets the IP address.
+        /// </summary>
         public string IPAddress { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the port.
+        /// </summary>
         public int Port { get; set; }
     }
 }
