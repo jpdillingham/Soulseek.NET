223becd4c8695cbb5acbcecf1b9b420f398210f2
JP Dillingham <jpdillingham@gmail.com>
2020-02-02 11:34:13 -0600

    remove redundant push of snupkg

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,7 +4,3 @@ set -e
 LATEST=$(find . | sort -dr | grep nupkg | head -1)
 
 dotnet nuget push $LATEST --api-key ${TOKEN_NUGET}
-
-LATEST_SYMBOLS=$(find . | sort -dr | grep snupkg | head -1)
-
-dotnet nuget push $LATEST_SYMBOLS --apk-key ${TOKEN_NUGET}
