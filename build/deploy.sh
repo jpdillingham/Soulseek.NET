ea63cfdbdfba09f93a578057fc8d769e4c2a390c
JP Dillingham <jpdillingham@gmail.com>
2020-02-02 13:33:45 -0600

    tweak deployment

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -e
 
-LATEST=$(find . | sort -dr | grep nupkg | head -1)
+RELEASE_DIR=./src/Soulseek/bin/Release/
+NUGET_API=nuget.org
+TEMP_FILE=${RELEASE_DIR}not_symbols
 
-dotnet nuget push $LATEST --api-key ${TOKEN_NUGET}
+LATEST=$(find $RELEASE_DIR | sort -dr | grep "\.nupkg$" | head -1)
+LATEST_SYMBOLS=$(find $RELEASE_DIR | sort -dr | grep "\.snupkg$" | head -1)
+
+printf "\npreparing to deploy:\n"
+echo "  nupkg: $LATEST"
+printf "  symbols: $LATEST_SYMBOLS\n\n"
+
+printf "pushing nupkg $LATEST\n\n"
+nuget push $LATEST -nosymbols -source ${NUGET_API} -apikey ${TOKEN_NUGET} -skipduplicate
+printf "nupkg done.\n\n"
+
+printf "pushing symbols $LATEST_SYMBOLS\n\n"
+nuget push $LATEST_SYMBOLS -source ${NUGET_API} -apikey ${TOKEN_NUGET} -skipduplicate
+printf "symbols done.\n\n"
+
+printf "deployment complete."
