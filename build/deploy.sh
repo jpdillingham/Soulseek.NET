a7c8f21ae6b116877c94c2a8bf0cb9d3c10502db
JP Dillingham <jpdillingham@gmail.com>
2020-02-02 14:07:05 -0600

    simplify deployment script

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,7 +3,6 @@ set -e
 
 RELEASE_DIR=./src/Soulseek/bin/Release/
 NUGET_API=nuget.org
-TEMP_FILE=${RELEASE_DIR}not_symbols
 
 LATEST=$(find $RELEASE_DIR | sort -dr | grep "\.nupkg$" | head -1)
 LATEST_SYMBOLS=$(find $RELEASE_DIR | sort -dr | grep "\.snupkg$" | head -1)
@@ -14,10 +13,8 @@ printf "  symbols: $LATEST_SYMBOLS\n\n"
 
 printf "pushing nupkg $LATEST\n\n"
 nuget push $LATEST -nosymbols -source ${NUGET_API} -apikey ${TOKEN_NUGET} -skipduplicate
-printf "nupkg done.\n\n"
 
 printf "pushing symbols $LATEST_SYMBOLS\n\n"
 nuget push $LATEST_SYMBOLS -source ${NUGET_API} -apikey ${TOKEN_NUGET} -skipduplicate
-printf "symbols done.\n\n"
 
 printf "deployment complete."
