a6324ab290ed7736233478ac9bfd16ba85177834
JP Dillingham <jpdillingham@gmail.com>
2020-02-02 11:27:46 -0600

    include symbol package, bump version to 0.9.4

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,3 +4,7 @@ set -e
 LATEST=$(find . | sort -dr | grep nupkg | head -1)
 
 dotnet nuget push $LATEST --api-key ${TOKEN_NUGET}
+
+LATEST_SYMBOLS=$(find . | sort -dr | grep snupkg | head -1)
+
+dotnet nuget push $LATEST_SYMBOLS --apk-key ${TOKEN_NUGET}
