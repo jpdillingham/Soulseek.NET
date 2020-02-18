4e7ae6500bb5e8d6f5d5d2a6170edcb7db12022c
JP Dillingham <jpdillingham@gmail.com>
2020-02-17 22:49:03 -0600

    tweak failing SearchAsync tests

--- tests/Soulseek.Tests.Unit/Client/SearchAsyncTests.cs
+++ tests/Soulseek.Tests.Unit/Client/SearchAsyncTests.cs
@@ -318,7 +318,7 @@ namespace Soulseek.Tests.Unit.Client
         [Theory(DisplayName = "SearchAsync adds search to ActiveSearches"), AutoData]
         public async Task SearchInternalAsync_Adds_Search_To_ActiveSearches(string searchText, int token)
         {
-            var options = new SearchOptions(searchTimeout: 1, fileLimit: 1);
+            var options = new SearchOptions(searchTimeout: 5, fileLimit: 1);
 
             using (var search = new SearchInternal(searchText, token, options)
             {
@@ -329,7 +329,7 @@ namespace Soulseek.Tests.Unit.Client
                 conn.Setup(m => m.WriteAsync(It.IsAny<byte[]>(), null))
                     .Returns(Task.CompletedTask);
 
-                using (var cts = new CancellationTokenSource(1000))
+                using (var cts = new CancellationTokenSource(5000))
                 using (var s = new SoulseekClient("127.0.0.1", 1, serverConnection: conn.Object))
                 {
                     s.SetProperty("State", SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
@@ -503,24 +503,29 @@ namespace Soulseek.Tests.Unit.Client
         public async Task SearchAsync_Invokes_ResponseReceived_Delegate(string searchText, int token)
         {
             var fired = false;
-            var options = new SearchOptions(searchTimeout: 1, fileLimit: 1, responseReceived: (e) => fired = true);
+            var options = new SearchOptions(searchTimeout: 5, fileLimit: 1, responseReceived: (e) => fired = true);
             var response = new SearchResponse("username", token, 1, 1, 1, 0, new List<File>() { new File(1, "foo", 1, "bar", 0) });
 
             var conn = new Mock<IMessageConnection>();
             conn.Setup(m => m.WriteAsync(It.IsAny<byte[]>(), null))
                 .Returns(Task.CompletedTask);
 
-            var s = new SoulseekClient("127.0.0.1", 1, serverConnection: conn.Object);
-            s.SetProperty("State", SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
+            using (var cts = new CancellationTokenSource(5000))
+            {
+                var s = new SoulseekClient("127.0.0.1", 1, serverConnection: conn.Object);
+                s.SetProperty("State", SoulseekClientStates.Connected | SoulseekClientStates.LoggedIn);
 
-            var task = s.SearchAsync(SearchQuery.FromText(searchText), SearchScope.Default, token, options, null);
+                var task = s.SearchAsync(SearchQuery.FromText(searchText), SearchScope.Default, token, options, null);
 
-            var search = s.GetProperty<ConcurrentDictionary<int, SearchInternal>>("Searches")[token];
-            search.ResponseReceived.Invoke(response);
+                var searches = s.GetProperty<ConcurrentDictionary<int, SearchInternal>>("Searches").ToList();
 
-            await task;
+                searches.FirstOrDefault(r => r.Key == token).Value.ResponseReceived.Invoke(response);
 
-            Assert.True(fired);
+                cts.Cancel();
+                await Record.ExceptionAsync(async () => await task); // swallow the cancellation exception
+
+                Assert.True(fired);
+            }
         }
 
         [Trait("Category", "SearchAsync")]
