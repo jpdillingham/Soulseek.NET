71dc00e05a688288d31fc7e95b035e0804feb2df
JP Dillingham <jpdillingham@gmail.com>
2020-03-10 21:42:42 -0500

    consolidate buttons, implement removal

--- examples/Web/web/src/TransferList.js
+++ examples/Web/web/src/TransferList.js
@@ -33,6 +33,10 @@ const getColor = (state) => {
     }
 }
 
+const isStateRetryable = (state) => state.includes('Completed') && state !== 'Completed, Succeeded';
+const isStateCancellable = (state) => ['InProgress', 'Requested', 'Queued', 'Initializing'].find(s => s === state);
+const isStateRemovable = (state) => state.includes('Completed');
+
 const downloadOne = (username, file) => {
     return axios.post(`${BASE_URL}/transfers/downloads/${username}/${encodeURI(file.filename)}`);
 }
@@ -41,59 +45,65 @@ const cancel = (direction, username, file) => {
     return axios.delete(`${BASE_URL}/transfers/${direction}s/${username}/${encodeURI(file.filename)}`);
 }
 
-const TransferList = ({ username, directoryName, files, direction }) => (
-    <div>
-        <Header 
-            size='small' 
-            className='filelist-header'
-        >
-            <Icon name='folder'/>{directoryName}
-        </Header>
-        <List>
-            <List.Item>
-            <Table>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell className='transferlist-selector'>
-                            <Checkbox 
-                                fitted 
-                                checked={false}
-                            />
-                        </Table.HeaderCell>
-                        <Table.HeaderCell className='transferlist-filename'>File</Table.HeaderCell>
-                        <Table.HeaderCell className='transferlist-size'>Size</Table.HeaderCell>
-                        <Table.HeaderCell className='transferlist-progress'>Progress</Table.HeaderCell>
-                        {direction === 'download' && <Table.HeaderCell className='transferlist-retry'>Retry</Table.HeaderCell>}
-                        <Table.HeaderCell className='transferlist-cancel'>Cancel</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>                                
-                <Table.Body>
-                    {files.sort((a, b) => getFileName(a.filename).localeCompare(getFileName(b.filename))).map((f, i) => 
-                        <Table.Row key={i}>
-                            <Table.Cell className='transferlist-selector'>
+const remove = (direction, username, file) => {
+    return axios.delete(`${BASE_URL}/transfers/${direction}s/${username}/${encodeURI(file.filename)}?remove=true`);
+}
+
+const TransferList = ({ username, directoryName, files, direction }) => {
+    return (
+        <div>
+            <Header 
+                size='small' 
+                className='filelist-header'
+            >
+                <Icon name='folder'/>{directoryName}
+            </Header>
+            <List>
+                <List.Item>
+                <Table>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell className='transferlist-selector'>
                                 <Checkbox 
                                     fitted 
-                                    checked={f.selected}
+                                    checked={false}
                                 />
-                            </Table.Cell>
-                            <Table.Cell className='transferlist-filename'>{getFileName(f.filename)}</Table.Cell>
-                            <Table.Cell className='transferlist-size'>{formatBytes(f.bytesTransferred).split(' ', 1) + '/' + formatBytes(f.size)}</Table.Cell>
-                            <Table.Cell className='transferlist-progress'>
-                                {f.state === 'InProgress' ? <Progress 
-                                    style={{ margin: 0 }}
-                                    percent={Math.round(f.percentComplete)} 
-                                    progress color={getColor(f.state)}
-                                /> : <Button fluid size='mini' style={{ margin: 0, padding: 8 }} color={getColor(f.state)}>{f.state}</Button>}
-                            </Table.Cell>
-                            {direction === 'download' && <Table.Cell className='transferlist-retry'><Button size='mini' style={{ padding: 8 }} onClick={() => downloadOne(username, f)}>Retry</Button></Table.Cell>}
-                            <Table.Cell className='transferlist-cancel'><Button size='mini' style={{ padding: 8 }} onClick={() => cancel(direction, username, f)}>Cancel</Button></Table.Cell>
+                            </Table.HeaderCell>
+                            <Table.HeaderCell className='transferlist-filename'>File</Table.HeaderCell>
+                            <Table.HeaderCell className='transferlist-size'>Size</Table.HeaderCell>
+                            <Table.HeaderCell className='transferlist-progress'>Progress</Table.HeaderCell>
+                            <Table.HeaderCell className='transferlist-cancel'></Table.HeaderCell>
                         </Table.Row>
-                    )}
-                </Table.Body>
-            </Table>
-            </List.Item>
-        </List>
-    </div>
-);
+                    </Table.Header>                                
+                    <Table.Body>
+                        {files.sort((a, b) => getFileName(a.filename).localeCompare(getFileName(b.filename))).map((f, i) => 
+                            <Table.Row key={i}>
+                                <Table.Cell className='transferlist-selector'>
+                                    <Checkbox 
+                                        fitted 
+                                        checked={f.selected}
+                                    />
+                                </Table.Cell>
+                                <Table.Cell className='transferlist-filename'>{getFileName(f.filename)}</Table.Cell>
+                                <Table.Cell className='transferlist-size'>{formatBytes(f.bytesTransferred).split(' ', 1) + '/' + formatBytes(f.size)}</Table.Cell>
+                                <Table.Cell className='transferlist-progress'>
+                                    {f.state === 'InProgress' ? <Progress 
+                                        style={{ margin: 0 }}
+                                        percent={Math.round(f.percentComplete)} 
+                                        progress color={getColor(f.state)}
+                                    /> : <Button fluid size='mini' style={{ margin: 0, padding: 8 }} color={getColor(f.state)}>{f.state}</Button>}
+                                </Table.Cell>
+                                {direction === 'download' && isStateRetryable(f.state) && <Table.Cell className='transferlist-retry'><Button size='mini' style={{ padding: 8, width: 60 }} onClick={() => downloadOne(username, f)}>Retry</Button></Table.Cell>}
+                                {isStateCancellable(f.state) && <Table.Cell className='transferlist-cancel'><Button size='mini' style={{ padding: 8, width: 60 }} onClick={() => cancel(direction, username, f)}>Cancel</Button></Table.Cell>}
+                                {isStateRemovable(f.state) && <Table.Cell><Button size='mini' style={{ padding: 8, width: 60 }} onClick={() => remove(direction, username, f)}>Remove</Button></Table.Cell>}
+                            </Table.Row>
+                        )}
+                    </Table.Body>
+                </Table>
+                </List.Item>
+            </List>
+        </div>
+    )
+};
 
 export default TransferList;
