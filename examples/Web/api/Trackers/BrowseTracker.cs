f499686c1c580dfa1f34b8de9464099ec173e47e
JP Dillingham <jpdillingham@gmail.com>
2020-03-17 08:24:03 -0500

    add BrowseTracker

---
+++ examples/Web/api/Trackers/BrowseTracker.cs
@@ -0,0 +1,36 @@
+﻿namespace WebAPI.Trackers
+{
+    using System.Collections.Concurrent;
+
+    public class BrowseTracker
+    {
+        /// <summary>
+        ///     Tracked browse operations.
+        /// </summary>
+        public ConcurrentDictionary<string, decimal> Browses { get; } = new ConcurrentDictionary<string, decimal>();
+
+        /// <summary>
+        ///     Adds or updates a tracked browse operation.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="progress"></param>
+        public void AddOrUpdate(string username, decimal progress)
+            => Browses.AddOrUpdate(username, progress, (user, oldprogress) => progress);
+
+        /// <summary>
+        ///     Removes a tracked browse operation for the specified user.
+        /// </summary>
+        /// <param name="username"></param>
+        public void TryRemove(string username)
+            => Browses.TryRemove(username, out _);
+
+        /// <summary>
+        ///     Gets the browse progress for the specified user.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        public bool TryGet(string username, out decimal progress)
+            => Browses.TryGetValue(username, out progress);
+    }
+}
---
+++ examples/Web/api/Trackers/IBrowseTracker.cs
@@ -0,0 +1,36 @@
+﻿namespace WebAPI.Trackers
+{
+    using System.Collections.Concurrent;
+
+    /// <summary>
+    ///     Tracks browse operations.
+    /// </summary>
+    interface IBrowseTracker
+    {
+        /// <summary>
+        ///     Tracked browse operations.
+        /// </summary>
+        ConcurrentDictionary<string, decimal> Browses { get; }
+
+        /// <summary>
+        ///     Adds or updates a tracked browse operation.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="progress"></param>
+        void AddOrUpdate(string username, decimal progress);
+
+        /// <summary>
+        ///     Removes a tracked browse operation for the specified user.
+        /// </summary>
+        /// <param name="username"></param>
+        void TryRemove(string username);
+
+        /// <summary>
+        ///     Gets the browse progress for the specified user.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        bool TryGet(string username, out decimal progress);
+    }
+}
