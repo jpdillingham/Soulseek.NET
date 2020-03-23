79767c744c10a92e0102663de32389b88b0c5c6b
JP Dillingham <jpdillingham@gmail.com>
2020-03-22 21:37:08 -0500

    tweak

--- examples/Web/web/src/config.js
+++ examples/Web/web/src/config.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NODE_ENV ? "http://localhost:5000/api/v1" : "/api/v1";
+const baseUrl = process.env.NODE_ENV ? "http://localhost:5000/api/v1" : "api/v1";
 
 export {
     baseUrl,
