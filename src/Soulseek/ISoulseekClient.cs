c270957e4a399614f0fc58ef7b4dec7a28230c66
JP Dillingham <jpdillingham@gmail.com>
2020-03-29 10:49:07 -0500

    remove DuplicateTokenException

--- src/Soulseek/ISoulseekClient.cs
+++ src/Soulseek/ISoulseekClient.cs
@@ -350,7 +350,6 @@ namespace Soulseek
         ///     Thrown when the <paramref name="username"/> or <paramref name="directoryName"/> is null, empty, or consists only
         ///     of whitespace.
         /// </exception>
-        /// <exception cref="DuplicateTokenException">Thrown when the specified or generated token is already in use.</exception>
         /// <exception cref="InvalidOperationException">Thrown when the client is not connected or logged in.</exception>
         /// <exception cref="TimeoutException">Thrown when the operation has timed out.</exception>
         /// <exception cref="OperationCanceledException">Thrown when the operation has been cancelled.</exception>
