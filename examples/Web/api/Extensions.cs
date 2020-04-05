6ea01d0f363e32b1faca605e544299a219e2f4a1
JP Dillingham <jpdillingham@gmail.com>
2020-04-05 14:39:46 -0500

    add DirectoryName extension, fix display issue on linux

--- examples/Web/api/Extensions.cs
+++ examples/Web/api/Extensions.cs
@@ -1,12 +1,20 @@
 ﻿namespace WebAPI
 {
     using System.IO;
+    using System.Linq;
 
     public static class Extensions
     {
         public static string ToLocalOSPath(this string path)
         {
             return path.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar);
+        }
+
+        public static string DirectoryName(this string path)
+        {
+            var separator = path.Contains('\\') ? '\\' : '/';
+            var parts = path.Split(separator);
+            return string.Join(separator, parts.Take(parts.Length - 1));
         }
     }
 }
