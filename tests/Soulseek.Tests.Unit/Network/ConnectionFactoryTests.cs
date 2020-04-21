24370e743e01deafdbd6d3e01e22b66078e67f96
JP Dillingham <jpdillingham@gmail.com>
2020-04-21 08:32:49 -0500

    add test for GetServerConnection

--- tests/Soulseek.Tests.Unit/Network/ConnectionFactoryTests.cs
+++ tests/Soulseek.Tests.Unit/Network/ConnectionFactoryTests.cs
@@ -12,21 +12,56 @@
 
 namespace Soulseek.Tests.Unit
 {
+    using System;
     using System.Net;
     using AutoFixture.Xunit2;
     using Soulseek.Network;
+    using Soulseek.Network.Tcp;
     using Xunit;
 
     public class ConnectionFactoryTests
     {
-        [Trait("Category", "GetConnection")]
-        [Theory(DisplayName = "GetConnection returns the expected connection"), AutoData]
-        internal void GetConneciton_Returns_The_Expected_Connection(IPEndPoint endpoint, ConnectionOptions options)
+        [Trait("Category", "GetTransferConnection")]
+        [Theory(DisplayName = "GetTransferConnection returns the expected connection"), AutoData]
+        internal void GetTransferConneciton_Returns_The_Expected_Connection(IPEndPoint endpoint, ConnectionOptions options)
         {
             var c = new ConnectionFactory().GetTransferConnection(endpoint, options);
 
             Assert.Equal(endpoint.Address, c.IPEndPoint.Address);
             Assert.Equal(endpoint.Port, c.IPEndPoint.Port);
+
+            Assert.Equal(options.ReadBufferSize, c.Options.ReadBufferSize);
+            Assert.Equal(options.WriteBufferSize, c.Options.WriteBufferSize);
+            Assert.Equal(options.ConnectTimeout, c.Options.ConnectTimeout);
+            Assert.Equal(-1, c.Options.InactivityTimeout);
+        }
+
+        [Trait("Category", "GetServerConnection")]
+        [Theory(DisplayName = "GetServerConnection returns the expected connection"), AutoData]
+        internal void GetServerConneciton_Returns_The_Expected_Connection(IPEndPoint endpoint, ConnectionOptions options)
+        {
+            bool connect = false;
+            EventHandler connected = (s, a) => { connect = true;  };
+
+            bool disconnect = false;
+            EventHandler<ConnectionDisconnectedEventArgs> disconnected = (s, a) => { disconnect = true; };
+
+            bool read = false;
+            EventHandler<MessageReadEventArgs> messageRead = (s, a) => { read = true; };
+
+            var c = new ConnectionFactory().GetServerConnection(endpoint, connected, disconnected, messageRead, options);
+
+            Assert.Equal(endpoint.Address, c.IPEndPoint.Address);
+            Assert.Equal(endpoint.Port, c.IPEndPoint.Port);
+
+            c.RaiseEvent(typeof(Connection), "Connected", EventArgs.Empty);
+            Assert.True(connect);
+
+            c.RaiseEvent(typeof(Connection), "Disconnected", new ConnectionDisconnectedEventArgs("foo"));
+            Assert.True(disconnect);
+
+            c.RaiseEvent("MessageRead", new MessageReadEventArgs(Array.Empty<byte>()));
+            Assert.True(read);
 
             Assert.Equal(options.ReadBufferSize, c.Options.ReadBufferSize);
             Assert.Equal(options.WriteBufferSize, c.Options.WriteBufferSize);
