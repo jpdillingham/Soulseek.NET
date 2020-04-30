e4a5986631abc7cfac64ea5b61550af4c55c3ab2
JP Dillingham <jpdillingham@gmail.com>
2020-04-30 08:35:01 -0500

    add tokenKey and tokenPassthroughValue to config

--- examples/Web/web/src/config.js
+++ examples/Web/web/src/config.js
@@ -1,5 +1,9 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? 'api/v1' : 'http://localhost:5000/api/v1'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'api/v1' : 'http://localhost:5000/api/v1';
+const tokenKey = 'soulseek-example-token';
+const tokenPassthroughValue = JSON.stringify('n/a');
 
 export {
     baseUrl,
+    tokenKey,
+    tokenPassthroughValue
 }
