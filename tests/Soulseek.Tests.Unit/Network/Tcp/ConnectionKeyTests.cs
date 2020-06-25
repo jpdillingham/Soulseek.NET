3a69ce9d2a8d2f7f4cfc44af7bf84577a527a06c
JP Dillingham <jpdillingham@gmail.com>
2020-06-25 09:07:06 -0500

    improve test coverage for ConnectionKey

--- tests/Soulseek.Tests.Unit/Network/Tcp/ConnectionKeyTests.cs
+++ tests/Soulseek.Tests.Unit/Network/Tcp/ConnectionKeyTests.cs
@@ -76,7 +76,57 @@ namespace Soulseek.Tests.Unit.Network.Tcp
         public void GetHashCode_Does_Not_Match_If_Key_Differs()
         {
             var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("b", new IPEndPoint(new IPAddress(0x1), 2));
+
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Trait("Category", "Hash Code")]
+        [Fact(DisplayName = "GetHashCode does not match if Username differs")]
+        public void GetHashCode_Does_Not_Match_If_Username_Differs()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
             var b = new ConnectionKey("b", new IPEndPoint(new IPAddress(0x0), 1));
+
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Trait("Category", "Hash Code")]
+        [Fact(DisplayName = "GetHashCode does not match if IPAddress differs")]
+        public void GetHashCode_Does_Not_Match_If_IPAddress_Differs()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x1), 1));
+
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Trait("Category", "Hash Code")]
+        [Fact(DisplayName = "GetHashCode does not match if Port differs")]
+        public void GetHashCode_Does_Not_Match_If_Port_Differs()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 2));
+
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Trait("Category", "Hash Code")]
+        [Fact(DisplayName = "GetHashCode does not match if nulls differ")]
+        public void GetHashCode_Does_Not_Match_If_Nulls_Differ()
+        {
+            var a = new ConnectionKey(null, new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("a", null);
+
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+
+            a = new ConnectionKey(null, null);
+            b = new ConnectionKey("a", null);
+
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+
+            a = new ConnectionKey(null, new IPEndPoint(new IPAddress(0x0), 1));
+            b = new ConnectionKey(null, null);
 
             Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
         }
@@ -93,11 +143,44 @@ namespace Soulseek.Tests.Unit.Network.Tcp
         }
 
         [Trait("Category", "Equals")]
-        [Fact(DisplayName = "Equals returns false when not equal")]
-        public void Equals_Returns_False_When_Not_Equal()
+        [Fact(DisplayName = "Equals returns false when IPAddress differs")]
+        public void Equals_Returns_False_When_IPAddress_Differs()
         {
             var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
             var b = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x1), 1));
+
+            Assert.False(a.Equals(b));
+            Assert.False(b.Equals(a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals returns false when IPAddress and Port differ")]
+        public void Equals_Returns_False_When_IPAddress_And_Port_Differ()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x1), 2));
+
+            Assert.False(a.Equals(b));
+            Assert.False(b.Equals(a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals returns false when Username, IPAddress and Port differ")]
+        public void Equals_Returns_False_When_Username_IPAddress_And_Port_Differ()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("b", new IPEndPoint(new IPAddress(0x1), 2));
+
+            Assert.False(a.Equals(b));
+            Assert.False(b.Equals(a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals returns false when Port differs")]
+        public void Equals_Returns_False_When_Port_Differs()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 2));
 
             Assert.False(a.Equals(b));
             Assert.False(b.Equals(a));
@@ -123,6 +206,91 @@ namespace Soulseek.Tests.Unit.Network.Tcp
 
             Assert.True(a.Equals((object)b));
             Assert.True(b.Equals((object)a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals handles null")]
+        public void Equals_Handles_Null()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            ConnectionKey b = null;
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals handles null IPEndPoint")]
+        public void Equals_Handles_Null_IPEndPoint()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("a", null);
+
+            Assert.False(a.Equals(b));
+            Assert.False(b.Equals(a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals handles null Username")]
+        public void Equals_Handles_Null_Username()
+        {
+            var a = new ConnectionKey("a", new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey(null, new IPEndPoint(new IPAddress(0x0), 1));
+
+            Assert.False(a.Equals(b));
+            Assert.False(b.Equals(a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals handles both null IPEndPoints")]
+        public void Equals_Handles_Both_Null_IPEndPoints()
+        {
+            var a = new ConnectionKey("a", null);
+            var b = new ConnectionKey("a", null);
+
+            Assert.True(a.Equals(b));
+            Assert.True(b.Equals(a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals handles both null Usernames")]
+        public void Equals_Handles_Both_Null_Usernames()
+        {
+            var a = new ConnectionKey(null, new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey(null, new IPEndPoint(new IPAddress(0x0), 1));
+
+            Assert.True(a.Equals(b));
+            Assert.True(b.Equals(a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals handles null Username and IPEndPoint")]
+        public void Equals_Handles_Null_Username_And_IPEndPoint()
+        {
+            var a = new ConnectionKey(null, new IPEndPoint(new IPAddress(0x0), 1));
+            var b = new ConnectionKey("a", null);
+
+            Assert.False(a.Equals(b));
+            Assert.False(b.Equals(a));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals handles null and null IPEndPoint")]
+        public void Equals_Handles_Null_And_Null_IPEndPoint()
+        {
+            var a = new ConnectionKey(null, new IPEndPoint(new IPAddress(0x0), 1));
+            ConnectionKey b = null;
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Trait("Category", "Equals")]
+        [Fact(DisplayName = "Equals handles null and null Username")]
+        public void Equals_Handles_Null_And_Null_Username()
+        {
+            var a = new ConnectionKey("a", null);
+            ConnectionKey b = null;
+
+            Assert.False(a.Equals(b));
         }
     }
 }
