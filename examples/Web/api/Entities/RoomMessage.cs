3646bfb74aec88a8ae741ddced734455b810f751
JP Dillingham <jpdillingham@gmail.com>
2020-09-26 15:46:28 -0500

    add Private/Room message entities

---
+++ examples/Web/api/Entities/RoomMessage.cs
@@ -0,0 +1,25 @@
+﻿namespace WebAPI.Entities
+{
+    using System;
+
+    /// <summary>
+    ///     A message sent to a room.
+    /// </summary>
+    public class RoomMessage
+    {
+        /// <summary>
+        ///     The timestamp of the message.
+        /// </summary>
+        DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        ///     The username of the user who sent the message.
+        /// </summary>
+        string Username { get; set; }
+
+        /// <summary>
+        ///     The message.
+        /// </summary>
+        string Message { get; set; }
+    }
+}
