f072def882495b0a10750c157278a2884e98f14c
JP Dillingham <jpdillingham@gmail.com>
2020-09-26 15:46:49 -0500

    add I/ConversationTracker

--- examples/Web/api/Trackers/IConversationTracker.cs
+++ examples/Web/api/Trackers/IConversationTracker.cs
@@ -1,25 +1,25 @@
-﻿using Soulseek;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-
-namespace WebAPI.Trackers
+﻿namespace WebAPI.Trackers
 {
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using WebAPI.Entities;
+
     /// <summary>
-    ///     Tracks private messages.
+    ///     Tracks private message conversations.
     /// </summary>
-    public interface IPrivateMessageTracker
+    public interface IConversationTracker
     {
         /// <summary>
         ///     Tracked private message conversations.
         /// </summary>
-        ConcurrentDictionary<string, IList<PrivateMessageReceivedEventArgs>> PrivateMessages { get; }
+        ConcurrentDictionary<string, IList<PrivateMessage>> Conversations { get; }
 
         /// <summary>
         ///     Adds a private message conversation and appends the specified <paramref name="message"/>, or just appends the message if the conversation exists.
         /// </summary>
         /// <param name="username"></param>
         /// <param name="message"></param>
-        void AddOrUpdate(string username, PrivateMessageReceivedEventArgs message);
+        void AddOrUpdate(string username, PrivateMessage message);
 
         /// <summary>
         ///     Removes a tracked private message conversation for the specified user.
