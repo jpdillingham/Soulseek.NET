e479b20c27885fd4cabb30890e33c451cbbb2b11
JP Dillingham <jpdillingham@gmail.com>
2020-10-06 07:57:23 -0500

    add ConversationMenu

---
+++ examples/Web/web/src/Chat/ConversationMenu.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Chat.css';
+
+import {
+  Icon, Button, Label, Menu
+} from 'semantic-ui-react';
+
+const ConversationMenu = ({ conversations, active, onConversationChange }) => {
+  const names = [...Object.keys(conversations), 'asfasf aaaaaaaaaaaaaaaaaaaaaasdfas asdf', '1111 asdf1521'];
+
+  const unread = Object.entries(conversations).reduce((acc, [name, messages]) => {
+    acc[name] = messages.filter(message => !message.acknowledged)
+    return acc;
+  }, {});
+
+  const isActive = (name) => active === name;
+
+  return (
+    <Menu className='conversation-menu' stackable size='huge'>
+      {names.map(name => 
+        <Menu.Item
+          style={{fontWeight: isActive(name) ? 'bold' : ''}}
+          name={name}
+          active={isActive(name)}
+          onClick={() => onConversationChange(name)}
+        >
+          {name}
+          {(unread[name] || []).length === 0 ? 
+            '' :
+            <Label color='red'>{(unread[name] || []).length}</Label>
+          }
+        </Menu.Item>
+      )}
+      <Menu.Menu position='right'>
+        <Menu.Item>
+          <Button icon primary><Icon name='plus'/></Button>
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  )
+}
+
+export default ConversationMenu;
