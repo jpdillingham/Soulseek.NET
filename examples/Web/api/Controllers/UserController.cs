e388c18deff078e1f50eeddecc3a8ededc4b6853
JP Dillingham <jpdillingham@gmail.com>
2020-10-10 15:07:33 -0500

    remove Exceptions

--- examples/Web/api/Controllers/UserController.cs
+++ examples/Web/api/Controllers/UserController.cs
@@ -6,7 +6,6 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Soulseek;
-    using Soulseek.Exceptions;
     using WebAPI.DTO;
     using WebAPI.Trackers;
 
