13f314b710a078bb343f2cc12babc27d3a6bb058
JP Dillingham <jpdillingham@gmail.com>
2020-10-11 15:06:14 -0500

    tweak handling of users

--- examples/Web/api/Trackers/RoomTracker.cs
+++ examples/Web/api/Trackers/RoomTracker.cs
@@ -1,13 +1,21 @@
 ﻿namespace WebAPI.Trackers
 {
+    using Soulseek;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
     using WebAPI.Entities;
 
+    /// <summary>
+    ///     Tracks rooms.
+    /// </summary>
     public class RoomTracker : IRoomTracker
     {
-        public RoomTracker(int messageLimit = 100)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RoomTracker"/> class.
+        /// </summary>
+        /// <param name="messageLimit"></param>
+        public RoomTracker(int messageLimit = 25)
         {
             MessageLimit = messageLimit;
         }
@@ -15,7 +23,7 @@
         /// <summary>
         ///     Tracked rooms.
         /// </summary>
-        public ConcurrentDictionary<string, IList<RoomMessage>> Rooms { get; } = new ConcurrentDictionary<string, IList<RoomMessage>>();
+        public ConcurrentDictionary<string, Room> Rooms { get; } = new ConcurrentDictionary<string, Room>();
 
         private int MessageLimit { get; }
 
@@ -24,26 +32,46 @@
         /// </summary>
         /// <param name="roomName"></param>
         /// <param name="message"></param>
-        public void AddOrUpdate(string roomName, RoomMessage message)
+        public void AddOrUpdateMessage(string roomName, RoomMessage message)
         {
-            Rooms.AddOrUpdate(roomName, new List<RoomMessage>() { message }, (_, messageList) =>
+            Rooms.AddOrUpdate(roomName, new Room() { Messages = new List<RoomMessage>() { message } }, (_, room) =>
             {
-                if (messageList.Count >= MessageLimit)
+                if (room.Messages.Count >= MessageLimit)
                 {
-                    messageList = messageList.TakeLast(MessageLimit - 1).ToList();
+                    room.Messages = room.Messages.TakeLast(MessageLimit - 1).ToList();
                 }
 
-                messageList.Add(message);
-                return messageList;
+                room.Messages.Add(message);
+                return room;
             });
+        }
+
+        /// <summary>
+        ///     Adds the specified room to the tracker
+        /// </summary>
+        /// <param name="roomName"></param>
+        /// <param name="room"></param>
+        public void TryAdd(string roomName, Room room) => Rooms.TryAdd(roomName, room);
+
+        /// <summary>
+        ///     Adds the specified <paramref name="userData"/> to the specified room.
+        /// </summary>
+        /// <param name="roomName"></param>
+        /// <param name="userData"></param>
+        public void TryAddUser(string roomName, UserData userData)
+        {
+            if (Rooms.TryGetValue(roomName, out var room))
+            {
+                room.Users.Add(userData);
+            }
         }
 
         /// <summary>
         ///     Removes a tracked room.
         /// </summary>
         /// <param name="roomName"></param>
-        /// <param name="messages"></param>
-        public bool TryGet(string roomName, out IList<RoomMessage> messages) => Rooms.TryGetValue(roomName, out messages);
+        /// <param name="room"></param>
+        public bool TryGet(string roomName, out Room room) => Rooms.TryGetValue(roomName, out room);
 
         /// <summary>
         ///     Returns the list of messages for the specified <paramref name="roomName"/>, if it is tracked.
@@ -51,5 +79,18 @@
         /// <param name="roomName"></param>
         /// <returns></returns>
         public void TryRemove(string roomName) => Rooms.TryRemove(roomName, out _);
+
+        /// <summary>
+        ///     Removes the specified <paramref name="username"/> from the specified room.
+        /// </summary>
+        /// <param name="roomName"></param>
+        /// <param name="username"></param>
+        public void TryRemoveUser(string roomName, string username)
+        {
+            if (Rooms.TryGetValue(roomName, out var room))
+            {
+                room.Users = room.Users.Where(u => u.Username != username).ToList();
+            }
+        }
     }
 }
