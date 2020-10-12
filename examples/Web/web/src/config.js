644e8ded0705f648394a6f9d9a97785ea141f992
JP Dillingham <jpdillingham@gmail.com>
2020-10-11 22:05:10 -0500

    add activeRoomKey

--- examples/Web/web/src/config.js
+++ examples/Web/web/src/config.js
@@ -2,10 +2,12 @@ const baseUrl = process.env.NODE_ENV === 'production' ? 'api/v1' : 'http://local
 const tokenKey = 'soulseek-example-token';
 const tokenPassthroughValue = 'n/a';
 const activeChatKey = 'soulseek-example-active-chat';
+const activeRoomKey = 'soulseek-example-active-room';
 
 export {
     baseUrl,
     tokenKey,
     tokenPassthroughValue,
     activeChatKey,
+    activeRoomKey
 };
