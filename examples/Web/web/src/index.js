1fc33a915de27b96508f2ccb208edd6bc5dbdb2e
JP Dillingham <jpdillingham@gmail.com>
2020-10-17 10:37:28 -0500

    import semantic css before app

--- examples/Web/web/src/index.js
+++ examples/Web/web/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import App from './App';
 import 'semantic-ui-css/semantic.min.css';
+import App from './App';
 
 ReactDOM.render(
     <Router>
