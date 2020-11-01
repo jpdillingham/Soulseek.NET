bc38f55f97dd58254cdbec81722751982536f1f0
JP Dillingham <jpdillingham@gmail.com>
2020-11-01 17:25:06 -0600

    add SizeSuffix()

--- examples/Web/api/Extensions.cs
+++ examples/Web/api/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace WebAPI
 {
+    using System;
     using System.IO;
     using System.Linq;
 
@@ -28,6 +29,34 @@
             var separator = path.Contains('\\') ? '\\' : '/';
             var parts = path.Split(separator);
             return string.Join(separator, parts.Take(parts.Length - 1));
+        }
+
+        static readonly string[] SizeSuffixes =
+                   { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+        public static string SizeSuffix(this double value, int decimalPlaces = 1)
+        {
+            if (value < 0) { return "-" + SizeSuffix(-value); }
+            if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
+
+            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+            int mag = (int)Math.Log(value, 1024);
+
+            // 1L << (mag * 10) == 2 ^ (10 * mag) 
+            // [i.e. the number of bytes in the unit corresponding to mag]
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+            // make adjustment when the value is large enough that
+            // it would round up to 1000 or more
+            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
+            {
+                mag += 1;
+                adjustedSize /= 1024;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                SizeSuffixes[mag]);
         }
     }
 }
