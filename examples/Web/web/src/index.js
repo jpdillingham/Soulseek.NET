36b5763faa25bf896bbf325d8072876d6ddff3e0
JP Dillingham <jpdillingham@gmail.com>
2020-11-12 07:43:21 -0600

    tweak import

--- examples/Web/web/src/index.js
+++ examples/Web/web/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css';
-import App from './Components/App';
+import App from './components/App';
 
 ReactDOM.render(
     <Router>
