1c1833e262fbefd4e4a4f8c59ad819bc19f7814c
JP Dillingham <jpdillingham@gmail.com>
2021-03-04 09:06:07 -0600

    resolve address, add docs

--- src/Options/ProxyOptions.cs
+++ src/Options/ProxyOptions.cs
@@ -19,9 +19,20 @@ namespace Soulseek
 {
     using System;
     using System.Net;
+    using System.Net.Sockets;
 
+    /// <summary>
+    ///     Socks 5 proxy configuration options.
+    /// </summary>
     public class ProxyOptions
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ProxyOptions"/> class.
+        /// </summary>
+        /// <param name="address">The address of the proxy server to which to connect.</param>
+        /// <param name="port">The port to which to connect.</param>
+        /// <param name="username">The username for the proxy, if applicable.</param>
+        /// <param name="password">The password for the proxy, if applicable.</param>
         public ProxyOptions(string address, int port, string username = null, string password = null)
         {
             if (string.IsNullOrWhiteSpace(address))
@@ -49,16 +60,54 @@ namespace Soulseek
                 throw new ArgumentOutOfRangeException(nameof(password), "The password must be less than or equal to 255 characters");
             }
 
+            if (!IPAddress.TryParse(address, out IPAddress ipAddress))
+            {
+                try
+                {
+                    ipAddress = Dns.GetHostEntry(address).AddressList[0];
+                }
+                catch (SocketException ex)
+                {
+                    throw new AddressException($"Failed to resolve address '{Address}': {ex.Message}", ex);
+                }
+            }
+
             Address = address;
+            IPAddress = ipAddress;
             Port = port;
+            IPEndPoint = new IPEndPoint(IPAddress, Port);
             Username = username;
             Password = password;
         }
 
+        /// <summary>
+        ///     Gets the address of the proxy server to which to connect.
+        /// </summary>
         public string Address { get; }
-        public int Port { get; }
-        public bool HasCredentuals => !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
-        public string Username { get; }
+
+        /// <summary>
+        ///     Gets the resolved proxy server address.
+        /// </summary>
+        public IPAddress IPAddress { get; }
+
+        /// <summary>
+        ///     Gets the resolved proxy server endpoint.
+        /// </summary>
+        public IPEndPoint IPEndPoint { get; }
+
+        /// <summary>
+        ///     Gets the password for the proxy, if applicable.
+        /// </summary>
         public string Password { get; }
+
+        /// <summary>
+        ///     Gets the port to which to connect.
+        /// </summary>
+        public int Port { get; }
+
+        /// <summary>
+        ///     Gets the username for the proxy, if applicable.
+        /// </summary>
+        public string Username { get; }
     }
 }
