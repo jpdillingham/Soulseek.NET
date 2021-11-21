82263c1417e2228416102370ea8167ba63a09578
JP Dillingham <jpdillingham@gmail.com>
2021-11-21 10:27:30 -0600

    add new overload for WriteAsync accepting a ReadOnlyMemory<byte>

--- src/Network/Tcp/INetworkStream.cs
+++ src/Network/Tcp/INetworkStream.cs
@@ -39,24 +39,6 @@ namespace Soulseek.Network.Tcp
         /// <param name="buffer">An array of type <see cref="byte"/> into which the read data will be written.</param>
         /// <param name="offset">The location in <paramref name="buffer"/> from which to start reading data.</param>
         /// <param name="size">The number of bytes to read.</param>
-        /// <returns>The number of bytes read.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="buffer"/> is null.</exception>
-        /// <exception cref="ArgumentOutOfRangeException">
-        ///     Thrown when the <paramref name="offset"/> is less than zero or greater than the length of
-        ///     <paramref name="buffer"/>, or <paramref name="size"/> is less than zero or greater than the length of
-        ///     <paramref name="buffer"/> minus the value of the <paramref name="offset"/> parameter.
-        /// </exception>
-        /// <exception cref="InvalidOperationException">Thrown when the stream is write only.</exception>
-        /// <exception cref="IOException">Thrown when an error occurs while reading from the network.</exception>
-        /// <exception cref="ObjectDisposedException">Thrown when the <see cref="NetworkStream"/> is closed.</exception>
-        Task<int> ReadAsync(byte[] buffer, int offset, int size);
-
-        /// <summary>
-        ///     Asynchronously reads data from the <see cref="NetworkStream"/>.
-        /// </summary>
-        /// <param name="buffer">An array of type <see cref="byte"/> into which the read data will be written.</param>
-        /// <param name="offset">The location in <paramref name="buffer"/> from which to start reading data.</param>
-        /// <param name="size">The number of bytes to read.</param>
         /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
         /// <returns>The number of bytes read.</returns>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="buffer"/> is null.</exception>
@@ -69,25 +51,6 @@ namespace Soulseek.Network.Tcp
         /// <exception cref="IOException">Thrown when an error occurs while reading from the network.</exception>
         /// <exception cref="ObjectDisposedException">Thrown when the <see cref="NetworkStream"/> is closed.</exception>
         Task<int> ReadAsync(byte[] buffer, int offset, int size, CancellationToken cancellationToken);
-
-        /// <summary>
-        ///     Asynchronously writes data to the <see cref="NetworkStream"/>.
-        /// </summary>
-        /// <param name="buffer">An array of type <see cref="byte"/> that contains the data to write to the <see cref="NetworkStream"/>.</param>
-        /// <param name="offset">The location in <paramref name="buffer"/> from which to start writing data.</param>
-        /// <param name="size">The number of bytes to write to the <see cref="NetworkStream"/>.</param>
-        /// <returns>A Task representing the asynchronous operation.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="buffer"/> is null.</exception>
-        /// <exception cref="ArgumentOutOfRangeException">
-        ///     Thrown when the <paramref name="offset"/> is less than zero or greater than the length of
-        ///     <paramref name="buffer"/>, or <paramref name="size"/> is less than zero or greater than the length of
-        ///     <paramref name="buffer"/> minus the value of the <paramref name="offset"/> parameter.
-        /// </exception>
-        /// <exception cref="IOException">Thrown when an error occurs while writing to the network.</exception>
-        /// <exception cref="ObjectDisposedException">
-        ///     Thrown when the <see cref="NetworkStream"/> is closed or when there was a failure reading from the network.
-        /// </exception>
-        Task WriteAsync(byte[] buffer, int offset, int size);
 
         /// <summary>
         ///     Asynchronously writes data to the <see cref="NetworkStream"/>.
@@ -108,5 +71,20 @@ namespace Soulseek.Network.Tcp
         ///     Thrown when the <see cref="NetworkStream"/> is closed or when there was a failure reading from the network.
         /// </exception>
         Task WriteAsync(byte[] buffer, int offset, int size, CancellationToken cancellationToken);
+
+#if NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        ///     Asynchronously writes data to the <see cref="NetworkStream"/>.
+        /// </summary>
+        /// <param name="buffer">A ReadOnlyMemory span of type <see cref="byte"/> that contains the data to write to the <see cref="NetworkStream"/>.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="buffer"/> is null.</exception>
+        /// <exception cref="IOException">Thrown when an error occurs while writing to the network.</exception>
+        /// <exception cref="ObjectDisposedException">
+        ///     Thrown when the <see cref="NetworkStream"/> is closed or when there was a failure reading from the network.
+        /// </exception>
+        ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken);
+#endif
     }
 }
