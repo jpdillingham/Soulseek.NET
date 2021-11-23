11d636e6d8262729a4dc0503222cc67feade276d
JP Dillingham <jpdillingham@gmail.com>
2021-11-23 08:00:39 -0600

    add WriteTimeout and KeepAlive options, add WithKeepAlive()

--- src/Options/ConnectionOptions.cs
+++ src/Options/ConnectionOptions.cs
@@ -29,21 +29,28 @@ namespace Soulseek
         /// <param name="writeBufferSize">The write buffer size for underlying TCP connections.</param>
         /// <param name="writeQueueSize">The size of the write queue for double buffered writes.</param>
         /// <param name="connectTimeout">The connection timeout, in milliseconds, for client and peer TCP connections.</param>
+        /// <param name="writeTimeout">The timeout, in milliseconds, for write operations.</param>
         /// <param name="inactivityTimeout">The inactivity timeout, in milliseconds, for peer TCP connections.</param>
+        /// <param name="keepAlive">A value indicating whether the connection should use TCP KeepAlives.</param>
         /// <param name="proxyOptions">Optional SOCKS 5 proxy configuration options.</param>
         public ConnectionOptions(
             int readBufferSize = 16384,
             int writeBufferSize = 16384,
             int writeQueueSize = 250,
             int connectTimeout = 10000,
+            int writeTimeout = 5000,
             int inactivityTimeout = 15000,
+            bool keepAlive = false,
             ProxyOptions proxyOptions = null)
         {
             ReadBufferSize = readBufferSize;
             WriteBufferSize = writeBufferSize;
             WriteQueueSize = writeQueueSize;
+
             ConnectTimeout = connectTimeout;
+            WriteTimeout = writeTimeout;
             InactivityTimeout = inactivityTimeout;
+            KeepAlive = keepAlive;
 
             ProxyOptions = proxyOptions;
         }
@@ -61,6 +68,11 @@ namespace Soulseek
         ///     be forcibly disconnected.
         /// </remarks>
         public int InactivityTimeout { get; }
+
+        /// <summary>
+        ///     Gets a value indicating whether the connection should use TCP KeepAlives.  (Default = false).
+        /// </summary>
+        public bool KeepAlive { get; }
 
         /// <summary>
         ///     Gets the optional SOCKS 5 proxy configuration options.
@@ -83,12 +95,26 @@ namespace Soulseek
         public int WriteQueueSize { get; }
 
         /// <summary>
+        ///     Gets the timeout, in milliseconds, for write operations.  (Default = 5000).
+        /// </summary>
+        public int WriteTimeout { get; }
+
+        /// <summary>
         ///     Returns a new instance with <see cref="InactivityTimeout"/> fixed to -1, disabling it.
         /// </summary>
         /// <returns>A new instance with InactivityTimeout disabled.</returns>
         public ConnectionOptions WithoutInactivityTimeout()
         {
-            return new ConnectionOptions(ReadBufferSize, WriteBufferSize, WriteQueueSize, ConnectTimeout, inactivityTimeout: -1, ProxyOptions);
+            return new ConnectionOptions(ReadBufferSize, WriteBufferSize, WriteQueueSize, ConnectTimeout, WriteTimeout, inactivityTimeout: -1, KeepAlive, ProxyOptions);
+        }
+
+        /// <summary>
+        ///     Returns a new instance with <see cref="KeepAlive"/> fixed to true, enabling it.
+        /// </summary>
+        /// <returns>A new instance with KeepAlive enabled.</returns>
+        public ConnectionOptions WithKeepAlive()
+        {
+            return new ConnectionOptions(ReadBufferSize, WriteBufferSize, WriteQueueSize, ConnectTimeout, WriteTimeout, InactivityTimeout, keepAlive: true, ProxyOptions);
         }
     }
 }
