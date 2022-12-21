1cf9f3d4864de853d85466aa343b3a702bcaf057
JP Dillingham <jp@dillingham.me>
2022-12-21 14:38:46 -0600

    remove Encoding constants

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -60,22 +60,6 @@ namespace Soulseek
         }
 
         /// <summary>
-        ///     Character encodings.
-        /// </summary>
-        internal static class Encoding
-        {
-            /// <summary>
-            ///     UTF-8.
-            /// </summary>
-            public const string UTF8 = "UTF-8";
-
-            /// <summary>
-            ///     ISO-8859-1.
-            /// </summary>
-            public const string ISO88591 = "ISO-8859-1";
-        }
-
-        /// <summary>
         ///     Wait keys.
         /// </summary>
         internal static class WaitKey
