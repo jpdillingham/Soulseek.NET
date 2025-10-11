1483701481473da843c58d3fce4645deefa76134
JP Dillingham <jp@dillingham.me>
2025-10-10 22:30:39 -0500

    tweak message, add debug log

--- src/Network/ListenerHandler.cs
+++ src/Network/ListenerHandler.cs
@@ -98,7 +98,8 @@ namespace Soulseek.Network
                         {
                             // either a random client connected and tried to download something without being told it could,
                             // or a client tried to initiate a transfer as a last-ditch effort to "save" an upload
-                            transferConnection.Disconnect($"Unknown transfer connection for token {peerInit.Token} from {peerInit.Username} ({connection.IPEndPoint.Address}:{SoulseekClient.Listener.Port}) (id: {connection.Id})");
+                            Diagnostic.Debug($"Unexpected transfer connection for token {peerInit.Token} from {peerInit.Username} ({connection.IPEndPoint.Address}:{SoulseekClient.Listener.Port}) (id: {connection.Id})");
+                            transferConnection.Disconnect($"Transfer connection rejected: unknown token");
                         }
                     }
                     else if (peerInit.ConnectionType == Constants.ConnectionType.Distributed)
