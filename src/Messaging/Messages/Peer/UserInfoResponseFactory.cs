b98eeac206841836bf640a749a23a5950c635ed4
JP Dillingham <jp@dillingham.me>
2026-09-02 08:48:19 -0500

    add logging of unread data

--- src/Messaging/Messages/Peer/UserInfoResponseFactory.cs
+++ src/Messaging/Messages/Peer/UserInfoResponseFactory.cs
@@ -57,6 +57,11 @@ namespace Soulseek.Messaging.Messages
             var queueLength = reader.ReadInteger();
             var hasFreeUploadSlot = reader.ReadByte() > 0;
 
+            if (SoulseekClient.ReportUnreadMessageData && reader.HasMoreData)
+            {
+                Diagnostics.GlobalDiagnostic.Warning($"Message reader for {nameof(MessageCode.Peer.InfoResponse)} finalized with {reader.Remaining} unread bytes");
+            }
+
             return new UserInfo(description, uploadSlots, queueLength, hasFreeUploadSlot, picture);
         }
 
